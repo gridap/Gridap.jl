b5e221fa9810280fe8adbe35a339add6b2108a6e
sbadia <sbadia@cimne.upc.edu>
2019-03-22 16:16:47 +0100

    Improving documentation

--- src/CellFields.jl
+++ src/CellFields.jl
@@ -3,8 +3,8 @@ export evaluate
 
 """
 Abstract type that represents a cell-wise field, where
-T stands for the type that represents the field at a point
-(e.g., scalar, vector, tensor) and D stands for the space
+`T` stands for the type that represents the field at a point
+(e.g., scalar, vector, tensor) and `D` stands for the space
 dimension
 """
 const CellField{D,T} = EvaluableCellArray{D,T,1} where {D,T}
@@ -13,6 +13,6 @@ evaluate(::CellField{D,T} where {D,T},::CellPoints{D} where D)::CellFieldValues{
 
 """
 Returns another CellField object that represents the gradient.
-TG has a rank one order greater than the one of T
+`TG` has a rank one order greater than the one of `T`
 """
 gradient(::CellField{D,T} where {D,T})::CellField{D,TG} = @abstractmethod
