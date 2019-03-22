5963b427e5f00c597f7640cb9cd6fa52cb50bf6d
sbadia <sbadia@cimne.upc.edu>
2019-03-22 16:36:19 +0100

    Improving documentation

--- src/CellFields.jl
+++ src/CellFields.jl
@@ -18,12 +18,12 @@ Returns another CellField object that represents the gradient.
 gradient(::CellField{D,T} where {D,T})::CellField{D,TG} = @abstractmethod
 
 """
-Composes a lambda function with a `CellField`
-`f` has to be overloaded with 2 methods one that returns the type of the
-result, and another that returns the result
+Composes a lambda function `f` with a `CellField`
+`g` to provide `f ∘ g`. It has to be overloaded with 2 methods,
+one that returns the type of the result, and another one that
+returns the result
 """
 function compose(f,g::CellField{D,S}) where {D,S}
   T = f(S)
   CellFieldFromComposeWithLambda{D,S,T}(f,g)
 end
-
