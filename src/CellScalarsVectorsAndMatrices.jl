73a4d23813645c86ea3ad4054620921ac35e7fc9
Francesc Verdugo <fverdugo@cimne.upc.edu>
2019-03-22 08:18:01 +0100

    Added inner(::CellBasisValues,::CellFieldValues)

--- src/CellScalarsVectorsAndMatrices.jl
+++ src/CellScalarsVectorsAndMatrices.jl
@@ -70,4 +70,65 @@ function inner_scalar_kernel!(testvals,trialvals,values)
   end
 end
 
+#TODO this involves a lot of code repetition
+# Surely there is a better way to do it in julia
+# We want DRY (don't repeat yourself) code
+
+struct CellVectorsFromInner{T} <: CellVectors{T}
+  test::CellBasisValues{T}
+  trial::CellFieldValues{T}
+end
+
+function Base.length(self::CellVectorsFromInner)
+  @assert length(self.test) == length(self.trial)
+  length(self.test)
+end
+
+function maxsize(self::CellVectorsFromInner)
+  @assert maxsize(self.test,2) == maxsize(self.trial,1)
+  maxsize(self.test)
+end
+
+function Base.iterate(self::CellVectorsFromInner{T}) where {T}
+  maxdofs = maxsize(self.test,1)
+  maxqpoints = maxsize(self.test,2)
+  values = Array{T,2}(undef,(maxdofs,maxqpoints))
+  testnext = iterate(self.test)
+  trialnext = iterate(self.trial)
+  state = (values,testnext,trialnext)
+  iterate(self,state)
+end
+
+function Base.iterate(self::CellVectorsFromInner,state)
+  (values,testnext,trialnext) = state
+  if testnext == nothing || trialnext == nothing
+    nothing
+  else
+    (testvals,teststate) = testnext
+    (trialvals,trialstate) = trialnext
+    inner_vector_kernel!(testvals,trialvals,values)
+    ndofs = size(testvals,1)
+    npoints = size(testvals,2)
+    vals = @view values[1:ndofs,1:npoints]
+    testnext = iterate(self.test,teststate)
+    trialnext = iterate(self.trial,trialstate)
+    state = (values,testnext,trialnext)
+    (vals, state)
+  end
+end
+
+function inner_vector_kernel!(testvals,trialvals,values)
+  @assert size(testvals,2) <= length(values)
+  @assert length(trialvals) <= length(values)
+  ndofs = size(testvals,1)
+  npoin = size(testvals,2)
+  @inbounds for i = 1:npoin
+    @inbounds for j = 1:ndofs
+      values[j,i] = inner(testvals[j,i],trialvals[i])
+    end
+  end
+end
+
 inner(test::CellFieldValues,trial::CellFieldValues) = CellScalarsFromInner(test,trial)
+
+inner(test::CellBasisValues,trial::CellFieldValues) = CellVectorsFromInner(test,trial)
--- test/CellScalarsVectorsAndMatricesTests.jl
+++ test/CellScalarsVectorsAndMatricesTests.jl
@@ -1,18 +1,45 @@
 
-l = 10
-a = [1.1,2.1,2.4]
-b = [3.1,2.3,2.4]
-test = ConstantCellArray{Float64,1}(a,l)
-trial = ConstantCellArray{Float64,1}(b,l)
+let
 
-values = inner(test,trial)
-
-@test length(values) == l
-@test maxsize(values) == (3,)
-
-for vals in values
-  for (i,v) in enumerate(vals)
-    @test v == a[i]*b[i]
+  l = 10
+  a = [1.1,2.1,2.4]
+  b = [3.1,2.3,2.4]
+  test = ConstantCellArray{Float64,1}(a,l)
+  trial = ConstantCellArray{Float64,1}(b,l)
+  
+  values = inner(test,trial)
+  
+  @test length(values) == l
+  @test maxsize(values) == (3,)
+  
+  for vals in values
+    for (i,v) in enumerate(vals)
+      @test v == a[i]*b[i]
+    end
   end
+
+end
+
+let
+
+  l = 10
+  a = [1.1 2.1 2.4; 2.3 1.2 3.1; 3.2 4.3 6.5; 2.3 1.2 3.1]
+  b = [3.1,2.3,2.4]
+  test = ConstantCellArray{Float64,2}(a,l)
+  trial = ConstantCellArray{Float64,1}(b,l)
+  
+  values = inner(test,trial)
+  
+  @test length(values) == l
+  @test maxsize(values) == (4,3)
+  
+  for vals in values
+    for i in 1:size(a,2)
+      for j in 1:size(a,1)
+        @test vals[j,i] == a[j,i]*b[i]
+      end
+    end
+  end
+
 end
 
