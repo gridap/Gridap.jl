29d12d93ad9de8b6edc3e3dbee5162835c20e58f
sbadia <sbadia@cimne.upc.edu>
2019-03-25 16:56:37 +0100

    Added Mutable versions

--- src/Points.jl
+++ src/Points.jl
@@ -1,28 +1,42 @@
-
 export VectorValue, MVectorValue
 export Point, MPoint
+export FieldValue
+export ScalarValue, VectorValue, TensorValue
+export MVectorValue, MTensorValue
 using StaticArrays
 
 """
-Type representing a vector value of D components
+Type representing a scalar value
+"""
+const ScalarValue = Float64
+
+"""
+Type representing a vector value of dimension `D`
 """
 const VectorValue{D} = SVector{D,Float64} where D
 
 """
-Type representing a tensor value of D components
+Type representing a tensor value of dimension `D`
 """
 const TensorValue{D,DD} = SMatrix{D,D,Float64,DD} where D
-
-const ScalarValue = Float64
-
-const FieldValue = Union{ScalarValue, VectorValue, TensorValue}
-
-# @santiagobadia : Mutable version...
+# @santiagobadia : Any way to eliminate DD?
 
 """
-Mutable version of VectorValue
+Mutable version of `VectorValue{D}`
 """
 const MVectorValue{D} = MVector{D,Float64} where D
+
+"""
+Mutable version of `TensorValue{D,DD}`
+"""
+const MTensorValue{D,DD} = MMatrix{D,D,Float64,DD} where D
+# @santiagobadia : Any way to eliminate DD?
+
+"""
+Type representing all possible field value types
+"""
+const FieldValue = Union{ScalarValue, VectorValue, TensorValue, MVectorValue, MTensorValue}
+# @santiagobadia : Should we make a difference between SFieldValue and MFieldValue?
 
 """
 Type representing a point of D dimensions
