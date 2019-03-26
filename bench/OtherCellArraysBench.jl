ca6f2b875c5bf27560cbd69978efb3aa7527a38c
Francesc Verdugo <fverdugo@cimne.upc.edu>
2019-03-26 10:20:57 +0100

    Added benchmark for OtherCellArrayFromUnaryOp

--- bench/OtherCellArraysBench.jl
+++ bench/OtherCellArraysBench.jl
@@ -16,4 +16,34 @@ let
   print("OtherConstantCellArray ->"); @time doloop(a)
   print("OtherConstantCellArray ->"); @time doloop(a)
 
+  eval(quote
+
+    struct DummyCellArray <: Numa.OtherCellArrayFromUnaryOp{Float64,2}
+      a::Numa.OtherCellArray{Float64,1}
+    end
+    
+    Numa.inputcellarray(self::DummyCellArray) = self.a
+    
+    Numa.computesize(self::DummyCellArray,asize) = (2,asize[1])
+    
+    function Numa.computevals!(self::DummyCellArray,a,v)
+      @inbounds for i in 1:size(a,1)
+        v[1,i] = a[i]
+        v[2,i] = a[i]
+      end
+    end
+
+  end)
+
+  b = DummyCellArray(a)
+
+  #(v,state) = iterate(b)
+  #@code_warntype iterate(b)
+  #v, astate = state
+  #anext = iterate(Numa.inputcellarray(b),astate)
+  #@code_warntype Numa.iteratekernel(b,anext,v)
+
+  print("OtherCellArrayFromUnaryOp ->"); @time doloop(b)
+  print("OtherCellArrayFromUnaryOp ->"); @time doloop(b)
+
 end
