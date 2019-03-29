5e5bda47a42f4fd075280784724dd1b599906590
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-03-29 16:38:59 +0000

    build based on 4ee6620

--- dev/search_index.js
+++ dev/search_index.js
@@ -9,46 +9,6 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "#Numa.gradshfsps-Union{Tuple{D}, Tuple{LagrangianRefFE{D},Array{SArray{Tuple{D},Float64,1,D},N} where N}} where D",
-    "page": "Numa.jl",
-    "title": "Numa.gradshfsps",
-    "category": "method",
-    "text": "Compute shape functions gradients (to be re-implemented)\n\n\n\n\n\n"
-},
-
-{
-    "location": "#Numa.shfsps-Union{Tuple{D}, Tuple{LagrangianRefFE{D},Any}} where D",
-    "page": "Numa.jl",
-    "title": "Numa.shfsps",
-    "category": "method",
-    "text": "Compute shape functions in a set of points\n\n\n\n\n\n"
-},
-
-{
-    "location": "#Numa.FESpace",
-    "page": "Numa.jl",
-    "title": "Numa.FESpace",
-    "category": "type",
-    "text": "FE Space structure, where only one RefFE is possible in the whole mesh (to be improved in the future)\n\n\n\n\n\n"
-},
-
-{
-    "location": "#Numa.LagrangianRefFE",
-    "page": "Numa.jl",
-    "title": "Numa.LagrangianRefFE",
-    "category": "type",
-    "text": "Reference Finite Element a la Ciarlet, i.e., it relies on a local function (polynomial) space, an array of nodes (DOFs), and a polytope (cell topology). The rank of the approximating field can be arbitrary. The current implementation relies on the prebasis (e.g., monomial basis of polynomials) and a change-of-basis (using the node array) to generate the canonical basis, i.e., the shape functions.\n\n\n\n\n\n"
-},
-
-{
-    "location": "#Numa.RefFE",
-    "page": "Numa.jl",
-    "title": "Numa.RefFE",
-    "category": "type",
-    "text": "Abstract Reference Finite Element\n\n\n\n\n\n"
-},
-
-{
     "location": "#Numa.jl-1",
     "page": "Numa.jl",
     "title": "Numa.jl",
