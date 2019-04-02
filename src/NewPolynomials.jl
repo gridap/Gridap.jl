87c0002f954c2301f8cec2da9e890a4ff7436ae6
sbadia <sbadia@cimne.upc.edu>
2019-04-02 13:07:28 +0200

    Univariate monomials with new flavour

--- src/NewPolynomials.jl
+++ src/NewPolynomials.jl
@@ -4,4 +4,78 @@ using Numa #@fverdugo to be eliminated
 using Numa.Helpers
 using Numa.FieldValues
 
+"""
+Abstract type representing a multivariate polynomial basis
+with value of type T in a coordinate space of D dimensions
+"""
+abstract type MultivariatePolynomialBasis{D,T} end
+
+Base.length(::MultivariatePolynomialBasis)::Int = @abstractmethod
+
+"""
+First axis of v for dofs, second for points
+"""
+evaluate!(::MultivariatePolynomialBasis{D,T},::AbstractVector{Point{D}},v::AbstractArray{T,2}) where {D,T} = @abstractmethod
+
+"""
+Same as evaluate! but allocates output
+"""
+function evaluate(self::MultivariatePolynomialBasis{D,T},points::AbstractArray{Point{D},1}) where {D,T}
+  vals = Array{T,2}(undef,(length(self),length(points)))
+  evaluate!(self,points,vals)
+  vals
+end
+
+"""
+Returns a MultivariatePolynomialBasis{TG,D} where TG
+is a type whose rank is one unit grater than the one of T
+"""
+gradient(::MultivariatePolynomialBasis{D,T} where{D,T})::MultivariatePolynomialBasis{D,TG} = @abstractmethod
+
+const ∇ = gradient
+
+Base.:*(::typeof(∇),f) = ∇(f)
+
+"""
+Abstract type representing a univariate polynomial basis in dimension one
+"""
+abstract type UnivariatePolynomialBasis <: MultivariatePolynomialBasis{1,ScalarValue} end
+
+"""
+Univariate monomial basis of a given `order`
+"""
+struct UnivariateMonomialBasis <: UnivariatePolynomialBasis
+  order::Int64
+end
+
+Base.length(this::UnivariateMonomialBasis)::Int = this.order+1
+
+"""
+Alocate and evaluate an array with all the elements of the
+a `UnivariateMonomialBasis` evaluated at a set of 1D points. The array axis are
+first the basis polynomial index and next the point label
+"""
+function (this::UnivariateMonomialBasis)(points::Vector{Point{1}})
+  dbas = length(this)
+  v = Array{Float64,2}(undef, dbas, length(points))
+  evaluate!(this, points, v)
+  return v
+end
+
+function evaluate(self::UnivariateMonomialBasis,
+  points::AbstractVector{Point{1}}) self(points) end
+
+"""
+Auxiliary function that does the same as evaluate but using a pre-allocated
+array
+"""
+function evaluate!(this::UnivariateMonomialBasis,
+  points::AbstractVector{Point{1}},v::AbstractArray{ScalarValue,2})
+  for (j,p) in enumerate(points)
+    for i in 1:length(this)
+      v[i,j] = p[1]^(i-1)
+    end
+  end
+end
+
 end # module NewPolynomials
---
+++ test/NewPolynomialsTests.jl
@@ -0,0 +1,32 @@
+##
+using Test
+using Numa
+using Numa.NewPolynomials
+using Numa.FieldValues
+
+using Numa.NewPolynomials: MultivariatePolynomialBasis
+using Numa.NewPolynomials: UnivariatePolynomialBasis
+using Numa.NewPolynomials: UnivariateMonomialBasis
+##
+
+@test UnivariatePolynomialBasis <: MultivariatePolynomialBasis{1,ScalarValue}
+
+a = UnivariateMonomialBasis(2)
+@test typeof(a) <: MultivariatePolynomialBasis{1,ScalarValue}
+@test length(a) == 3
+#evaluate UnivariateMonomialBasis
+
+points = [Point{1}(1.0), Point{1}(2.0), Point{1}(3.0)]
+values = a(points)
+@test values[3,3] == 9
+##
+
+
+# end
+
+
+
+# l=10000000
+# println("+++ CellFieldsBench ( length = $l ) +++")
+# print("Array of 1D points ->"); @time dopointmon(l)
+# print("Array of Ints ->"); @time doarraymon(l)
