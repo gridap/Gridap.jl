7016af920387d2fc56aed7e90ad23e981c438b86
Francesc Verdugo <fverdugo@cimne.upc.edu>
2019-04-04 09:24:58 +0200

    Saving unfinished changes

--- src/CellValues/CellValues.jl
+++ src/CellValues/CellValues.jl
@@ -9,8 +9,13 @@ import Base: size
 import Base: getindex
 import Base: IndexStyle
 import Base: +, -, *, /
+import Base: ==
 import LinearAlgebra: inv, det
 
+import Numa.FieldValues: inner, outer
+
 include("Interfaces.jl")
+include("ConstantCellValues.jl")
+include("Operations.jl")
 
 end # module CellValues
