d60c5fd806b4b3cd57d9e05382c5880e49d3ada6
fverdugo <fverdugo@cimne.upc.edu>
2019-04-05 16:03:13 +0200

    Removed CellArrays

--- src/Numa.jl
+++ src/Numa.jl
@@ -12,7 +12,6 @@ include("Quadratures.jl")
 include("Polynomials.jl")
 # include("NewPolynomials.jl")
 include("CellValues/CellValues.jl")
-#include("CellArrays/CellArrays.jl") # @fverdugo to be replaced by CellValues
 include("CellFunctions/CellFunctions.jl")
 include("CellQuadratures.jl")
 include("IntegrationMeshes.jl")
