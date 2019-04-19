34665efabf5e713d41cc93b951dbf92699b7ed32
sbadia <sbadia@cimne.upc.edu>
2019-04-19 15:43:52 +0200

    Fixing the templates

--- src/Fields/CellMaps.jl
+++ src/Fields/CellMaps.jl
@@ -1,30 +1,31 @@
 module CellMaps
 
+using Numa.Maps
 using Numa.Helpers
 
 # Iterable cell Maps
 
-abstract type IterCellMap{T} end
+abstract type IterCellMap{S,M,T,N} end
 
-function iterate(::IterCellMap{T})::Union{Nothing,Tuple{T,Any}} where T
+function iterate(::IterCellMap{S,M,T,N})::Union{Nothing,Tuple{Map{S,M,T,N},Any}} where {S,M,T,N}
   @abstractmethod
 end
 
-function iterate(::IterCellMap{T},state)::Union{Nothing,Tuple{T,Any}} where T
+function iterate(::IterCellMap{S,M,T,N},state)::Union{Nothing,Tuple{Map{S,M,T,N},Any}} where {S,M,T,N}
   @abstractmethod
 end
 
 length(::IterCellMap)::Int = @abstractmethod
 
-eltype(::Type{C}) where C <: IterCellMap{T} where T = T
+eltype(::Type{C}) where C <: IterCellMap{S,M,T,N} where {S,M,T,N} = Map{S,M,T,N}
 
 # Indexable cell Maps
 
-abstract type IndexCellMap{T,N} <: AbstractArray{T,N} end
+abstract type IndexCellMap{S,M,T,N,R<:Map{S,M,T,N}} <: AbstractVector{R} end
 
 # Cell Maps
 
-const CellMap{T} = Union{IterCellMap{T},IndexCellMap{T}}
+const CellMap{S,M,T,N} = Union{IterCellMap{S,M,T,N},IndexCellMap{S,M,T,N,R} where R<:Map{S,M,T,N}}
 
 cellsize(::CellMap) = ()
 # @santiagobadia : What should I put here?
