d5999f75a83240eb1b103acc005e2a1ec213d691
sbadia <sbadia@cimne.upc.edu>
2019-04-24 10:38:24 +0200

    Temporary inactive tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,10 +10,10 @@ using Test
   # @time @testset "CellValues" begin include("CellValuesTests/CellValuesTests.jl") end
   # @time @testset "CellQuadratures" begin include("CellQuadraturesTests.jl") end
   # @time @testset "CellFunctions" begin include("CellFunctionsTests.jl") end
-  @time @testset "CellMaps" begin include("CellMapsTests.jl") end
+  # @time @testset "CellMaps" begin include("CellMapsTests.jl") end
   # @time @testset "CellIntegration" begin include("CellIntegrationTests.jl") end
   # @time @testset "Polytopes" begin include("PolytopesTests.jl") end
-  @time @testset "Geometry" begin include("GeometryTests.jl") end
+  # @time @testset "Geometry" begin include("GeometryTests.jl") end
   # @time @testset "RefFEs" begin include("RefFEsTests.jl") end
   # @time @testset "Vtkio" begin include("VtkioTests.jl") end
   # @time @testset "FESpaces" begin include("FESpacesTests.jl") end
