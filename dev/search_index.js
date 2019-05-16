1d162e71055ccd76a5b11102e27b67288bce94f9
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-05-16 20:38:23 +0000

    build based on 13a7163

--- dev/search_index.js
+++ dev/search_index.js
@@ -2,18 +2,18 @@ var documenterSearchIndex = {"docs": [
 
 {
     "location": "#",
-    "page": "Numa.jl",
-    "title": "Numa.jl",
+    "page": "Gridap.jl",
+    "title": "Gridap.jl",
     "category": "page",
     "text": ""
 },
 
 {
-    "location": "#Numa.jl-1",
-    "page": "Numa.jl",
-    "title": "Numa.jl",
+    "location": "#Gridap.jl-1",
+    "page": "Gridap.jl",
+    "title": "Gridap.jl",
     "category": "section",
-    "text": "Documentation for the Numa libraryModules = [Numa,]\nOrder   = [:function, :type]"
+    "text": "Documentation for the Gridap libraryModules = [Gridap,]\nOrder   = [:function, :type]"
 },
 
 ]}
