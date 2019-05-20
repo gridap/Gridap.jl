a986cfb27251671df692c64e79f8a0878fbbaed9
fverdugo <fverdugo@cimne.upc.edu>
2019-05-20 17:45:32 +0200

    Advancing with the refactoring of FE spaces

--- test/FESpacesTests/FESpaces.jl
+++ test/FESpacesTests/FESpaces.jl
@@ -5,6 +5,7 @@ using Gridap.Helpers
 using Gridap.RefFEs
 using Gridap.Polytopes
 using Gridap.CellValues
+using Gridap.CellMaps
 using Gridap.CellValues.ConstantCellValues
 using Gridap.CellValues.Wrappers
 using Gridap.CellValues.Append
@@ -13,22 +14,42 @@ using Gridap.FieldValues
 
 export FESpace
 export num_free_dofs
-export num_fixed_dofs
+export num_diri_dofs
 export apply_constraints
 export apply_constraints_rows
 export apply_constraints_cols
+export interpolated_values
 
 export ConformingFESpace
 
 """
+Abstract type representing a FE Function
+A FE function is a member of a FESpace
+since a FE space is the direct sum of free + dirichlet spaces
+a FE function is uniquely represented by two vectors of components aka
+`free_dofs` and `diri_dofs`
+E = eltype(T)
+"""
+abstract type FEFunction{Z,T,C,R} <: IndexCellField{Z,T,C,R} end
+
+function free_dofs(::FEFunction)::Vector{E} where E
+  @abstractmethod
+end
+
+function diri_dofs(::FEFunction)::Vector{E} where E
+  @abstractmethod
+end
+
+"""
 Abstract FE Space parameterized with respec to the environment dimension `D`,
 the cell dimension `Z`, and the field type `T` (rank)
+A FE space has to be understood as the direct sum of two spaces free + dirichlet
 """
 abstract type FESpace{D,Z,T} end
 
 num_free_dofs(::FESpace)::Int = @abstractmethod
 
-num_fixed_dofs(::FESpace)::Int = @abstractmethod
+num_diri_dofs(::FESpace)::Int = @abstractmethod
 
 function apply_constraints(
   ::FESpace, cellvec::CellVector)::Tuple{CellVector,CellVector{Int}}
@@ -46,14 +67,34 @@ function apply_constraints_cols(
 end
 
 """
+Interpolates a function and returns a vector of free values and another of dirichlet ones
+E = eltype(T)
+"""
+function interpolated_values(::FESpace,::Function)::Tuple{Vector{E},Vector{E}} where E
+  @abstractmethod
+end
+
+"""
+Returns the FE function represented be the  free and dirichlet values
+E = eltype(T)
+"""
+function FEFunction(::FESpace,free_dofs::Vector{E},diri_dofs::Vector{E})::FEFunction where E
+  @abstractmethod
+end
+
+function interpolate(::FESpace{D,Z,T},::Function)::CellField{Z,T} where {D,Z,T}
+  @abstractmethod
+end
+
+"""
 Conforming FE Space, where only one RefFE is possible in the whole mesh
 """
 struct ConformingFESpace{D,Z,T} <: FESpace{D,Z,T}
   dim_to_nface_eqclass::Vector{<:IndexCellArray{Int}}
   cell_eqclass::IndexCellArray{Int}
   num_free_dofs::Int
-  num_fixed_dofs::Int
-  dir_tags::Vector{Int}
+  num_diri_dofs::Int
+  diri_tags::Vector{Int}
   _reffes::LagrangianRefFE{D,T}
   _triangulation::Triangulation{D,Z}
   _gridgraph::FullGridGraph
@@ -73,28 +114,14 @@ function ConformingFESpace(
   trian::Triangulation{D,Z},
   graph::FullGridGraph,
   labels::FaceLabels,
-  dir_tags::Vector{Int}) where {D,Z,T}
-
-  dim_to_nface_eqclass, nfree, nfixed  = _generate_dim_to_nface_eqclass(
-    reffe, graph, labels, dir_tags)
-
-  cellvefs_dim = [connections(graph,D,i) for i in 0:D]
-  offset = length.(dim_to_nface_eqclass)
-  for i in 2:length(offset)
-    offset[i] += offset[i-1]
-  end
-  offset = tuple(offset...)
-  cellvefs = IndexCellValueByLocalAppendWithOffset(offset, cellvefs_dim...)
-  dofs_all = IndexCellValueByGlobalAppend(dim_to_nface_eqclass...)
-  cell_eqclass = CellVectorByComposition(cellvefs, dofs_all)
-  ConformingFESpace{D,Z,T}(
-    dim_to_nface_eqclass, cell_eqclass, nfree, nfixed,
-    dir_tags, reffe, trian, graph, labels)
+  diri_tags::Vector{Int}) where {D,Z,T}
+  args = _setup_conforming_fe_fields(reffe,trian,graph,labels,diri_tags,D)
+  ConformingFESpace{D,Z,T}(args...)
 end
 
 num_free_dofs(this::ConformingFESpace) = this.num_free_dofs
 
-num_fixed_dofs(this::ConformingFESpace) = this.num_fixed_dofs
+num_diri_dofs(this::ConformingFESpace) = this.num_diri_dofs
 
 function apply_constraints(
   this::ConformingFESpace, cellvec::CellVector)
@@ -109,6 +136,66 @@ end
 function apply_constraints_cols(
   this::ConformingFESpace, cellmat::CellMatrix)
   return cellmat, this.cell_eqclass
+end
+
+function interpolated_values(this::ConformingFESpace,f::Function)
+  _interpolated_values(this,f)
+end
+
+function interpolate(this::ConformingFESpace,f::Function)
+  _interpolate(this,f)
+end
+
+# Helpers
+
+function _interpolate(this::ConformingFESpace,fun::Function)
+  free_dofs, diri_dofs = interpolated_values(this,fun)
+  cdofs = CellVectorFromLocalToGlobalPosAndNeg(celldofs, free_dofs, diri_dofs)
+  shb = ConstantCellValue(reffe.shfbasis, ncells(trian))
+  intu = CellFieldFromExpand(shb, cdofs)
+  return ConformingFEFunction(fesp, intu)
+end
+
+function _interpolated_values(fesp::ConformingFESpace{D,Z,T},fun::Function) where {D,Z,T}
+  reffe = fesp._reffes
+  dofb = reffe.dofbasis
+  trian = fesp._triangulation
+  phi = geomap(trian)
+  uphys = fun ∘ phi
+  celldofs = fesp.cell_eqclass
+  nfdofs = fesp.dim_to_nface_eqclass
+  maxs = max([length(nfdofs[i]) for i=1:D+1]...)
+  E = eltype(T)
+  free_dofs = zeros(E, num_free_dofs(fesp))
+  diri_dofs = zeros(E, num_diri_dofs(fesp))
+  aux = zeros(E, maxs)
+  for (imap,l2g) in zip(uphys,celldofs)
+    evaluate!(dofb,imap,aux)
+    for (i,gdof) in enumerate(l2g)
+      if (gdof > 0)
+        free_dofs[gdof] = aux[i]
+      else
+        diri_dofs[-gdof] = aux[i]
+      end
+    end
+  end
+  return free_dofs, diri_dofs
+end
+
+function _setup_conforming_fe_fields(reffe,trian,graph,labels,diri_tags,D)
+  dim_to_nface_eqclass, nfree, ndiri  = _generate_dim_to_nface_eqclass(
+    reffe, graph, labels, diri_tags)
+  cellvefs_dim = [connections(graph,D,i) for i in 0:D]
+  offset = length.(dim_to_nface_eqclass)
+  for i in 2:length(offset)
+    offset[i] += offset[i-1]
+  end
+  offset = tuple(offset...)
+  cellvefs = IndexCellValueByLocalAppendWithOffset(offset, cellvefs_dim...)
+  dofs_all = IndexCellValueByGlobalAppend(dim_to_nface_eqclass...)
+  cell_eqclass = CellVectorByComposition(cellvefs, dofs_all)
+  return dim_to_nface_eqclass, cell_eqclass, nfree, ndiri, diri_tags,
+    reffe, trian, graph, labels
 end
 
 function _generate_dim_to_nface_eqclass(
@@ -135,7 +222,7 @@ function _generate_dim_to_nface_eqclass(
       lid_vef = reffe.polytope.nf_dim[end][vef_dim+1][1]+lid_vef_dim-1
       # @santiagobadia : Better a method for nfs of a particular type...
       num_nf_dofs = length(reffe.nfacedofs[lid_vef])
-      if ( _is_fixed(vef_labels[ignf],diri_tags,labels) )
+      if ( _is_diri(vef_labels[ignf],diri_tags,labels) )
         nfdofs_l = Int[nfdofs_l..., c_n:-1:c_n-num_nf_dofs+1... ]
         c_n -= num_nf_dofs
       else
@@ -150,7 +237,7 @@ function _generate_dim_to_nface_eqclass(
   return [ dim_to_nface_eqclass , c-1, -c_n-1 ]
 end
 
-@inline function _is_fixed(v,dt,labels)
+@inline function _is_diri(v,dt,labels)
   for tag in dt
     labs = labels_on_tag(labels,tag)
     for label in labs
--- test/FESpacesTests/FESpacesTests.jl
+++ test/FESpacesTests/FESpacesTests.jl
@@ -31,7 +31,7 @@ fe = LagrangianRefFE{D,Float64}(polytope, orders)
 fespace = ConformingFESpace(fe,trian,graph,labels,tags)
 
 @test num_free_dofs(fespace) == 5
-@test num_fixed_dofs(fespace) == 4
+@test num_diri_dofs(fespace) == 4
 
 r = [[-1, 1, 2, 3], [1, -2, 3, 4], [2, 3, -3, 5], [3, 4, 5, -4]]
 
@@ -46,11 +46,25 @@ tags = [1,2,3,4,6,5]
 fespace = ConformingFESpace(fe,trian,graph,labels,tags)
 
 @test num_free_dofs(fespace) == 15
-@test num_fixed_dofs(fespace) == 10
+@test num_diri_dofs(fespace) == 10
 
 r = [[-1, -2, 1, 2, -7, 4, 5, 6, 12], [-2, -3, 2, 3, -8, 7, 6, 8, 13],
      [1, 2, -4, -5, 4, -9, 9, 10, 14], [2, 3, -5, -6, 7, -10, 10, 11, 15]]
 
 @test r == collect(fespace.cell_eqclass)
+
+fun(x) = sin(x[1])*cos(x[2])
+
+free_vals, diri_vals = interpolated_values(fespace,fun)
+
+rf = [0.0, 0.420735, 0.598194, 0.078012, 0.0, 0.420735,
+      0.214936, 0.598194, -0.0, -0.199511, -0.283662,
+      0.420735, 0.73846, -0.199511, -0.350175]
+
+rd = [0.0, 0.259035, 0.368291, 0.420735, 0.73846, 0.151174,
+      0.239713, 0.660448, 0.151174, 0.265335]
+
+@test isapprox(free_vals,rf,rtol=1.0e-5)
+@test isapprox(diri_vals,rd,rtol=1.0e-5)
 
 end # module FESpacesTests
