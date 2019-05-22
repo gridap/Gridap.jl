936f1111b5b308829a3ea33db4fd84a527833507
fverdugo <fverdugo@cimne.upc.edu>
2019-05-22 17:51:38 +0200

    Starting to add a real non-linear problem

--- test/FESpacesTests/NonLinearFEOperatorsTests.jl
+++ test/FESpacesTests/NonLinearFEOperatorsTests.jl
@@ -20,7 +20,10 @@ import Gridap: gradient
 ufun(x) = x[1] + x[2]
 ufun_grad(x) = VectorValue(1.0,1.0)
 gradient(::typeof(ufun)) = ufun_grad
-bfun(x) = 0.0
+#bfun(x) = -(3*x[1]+x[2]+1.0)
+#νfun(x,u) = (u+1.0)*x[1]
+bfun(x) = -1.0
+νfun(x,u) = x[1]
 
 # Construct the discrete model
 model = CartesianDiscreteModel(domain=(0.0,1.0,0.0,1.0), partition=(4,4))
@@ -39,14 +42,17 @@ U = TrialFESpace(fespace,ufun)
 trian = Triangulation(model)
 quad = CellQuadrature(trian,order=2)
 
-# Define cell field describing the source term
+# Define cell field describing
+# a solution dependent source term
 bfield = CellField(trian,bfun)
 
+# Define a solution dependent material parameter
+ν(u) = CellField(trian,νfun,u)
+
 # Define residual and jacobian
-a(v,u) = varinner(∇(v), ∇(u))
-b(v) = varinner(v,bfield)
-res(u,v) = a(v,u) - b(v)
-jac(u,v,du) = a(v,du)
+a(u,v,du) = varinner( ∇(v), ν(u)*∇(du))
+res(u,v) = a(u,v,u) - varinner(v,bfield)
+jac(u,v,du) = a(u,v,du)
 
 # Define Assembler
 assem = SparseMatrixAssembler(V,U)
@@ -55,8 +61,8 @@ assem = SparseMatrixAssembler(V,U)
 op = NonLinearFEOperator(res,jac,V,U,assem,trian,quad)
 
 # Define the FESolver
-tol = 1.e-8
-maxiters = 10
+tol = 1.e-10
+maxiters = 20
 solver = NonLinearFESolver(tol,maxiters)
 
 # Solve!
@@ -68,14 +74,17 @@ e = u - uh
 
 # Define norms to measure the error
 l2(u) = varinner(u,u)
-h1(u) = a(u,u) + l2(u)
+sh1(u) = varinner(∇(u),∇(u))
+h1(u) = sh1(u) + l2(u)
 
 # Compute errors
 el2 = sqrt(sum( integrate(l2(e),trian,quad) ))
 eh1 = sqrt(sum( integrate(h1(e),trian,quad) ))
+ul2 = sqrt(sum( integrate(l2(u),trian,quad) ))
+uh1 = sqrt(sum( integrate(h1(u),trian,quad) ))
 
-@test el2 < 1.e-8
-@test eh1 < 1.e-8
+@test el2/ul2 < 1.e-8
+@test eh1/uh1 < 1.e-7
 
 #writevtk(trian,"trian",nref=4,cellfields=["uh"=>uh,"u"=>u,"e"=>e])
 
