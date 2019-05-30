e17133e6064f2cab4c81e39ed39018d90a5daf1e
fverdugo <fverdugo@cimne.upc.edu>
2019-05-30 17:34:24 +0200

    Starting to add MultiFEOperators

---
+++ src/MultiField/MultiFEOperators.jl
@@ -0,0 +1,12 @@
+include("MultiAssemblers.jl")
+
+module MultiFEOperators
+
+using Gridap
+using Gridap.Helpers
+using Gridap.FESpaces
+using ..MultiFESpaces
+using ..MultiAssemblers
+using ..MultiFEFunctions
+
+end # module MultiFEOperators
