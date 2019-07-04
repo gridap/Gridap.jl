f6e65543568365786d1548c11ef22843c1dc58b3
Francesc Verdugo <fverdugo@cimne.upc.edu>
2019-07-04 14:52:53 +0200

    Removed commented text

--- src/FESpaces/Assemblers.jl
+++ src/FESpaces/Assemblers.jl
@@ -11,10 +11,6 @@ export SparseMatrixAssembler
 export assemble
 export assemble!
 
-#@fverdugo for the moment the abstract interface of Assembler
-# (and therefore its concrete implementations)
-# assumes single field, and single term
-
 """
 Abstract assembly operator
 Parametrized by the type of returned matrix and vector
@@ -121,34 +117,5 @@ function assemble!(
   m = assemble(this,vals)
   mat.nzval .= m.nzval
 end
-
-# Draft of multi field assembler
-#function _assemble_sparse_matrix_values(mf_vals,mf_rows,mf_cols,I,E)
-#  aux_row = I[]; aux_col = I[]; aux_val = E[]
-#  for (mf_rows_c, mf_cols_c, mf_vals_c) in zip(mf_rows,mf_cols,mf_vals)
-#    for (vals_c, (ifield, jfield)) in eachblock(mf_vals_c)
-#      rows_c = mf_rows_c[ifield]
-#      cols_c = mf_cols_c[jfield]
-#      row_offset = row_offsets[ifield]
-#      col_offset = col_offsets[jfield]
-#      _asseble_cell_values!(aux_row,aux_col,aux_val,rows_c,cols_c,vals_c,col_offset,row_offset)
-#    end
-#  end
-#  (aux_row, aux_col, aux_val)
-#end
-#
-#function _asseble_cell_values!(aux_row,aux_col,aux_val,rows_c,cols_c,vals_c,col_offset,row_offset)
-# for (j,gidcol) in enumerate(cols_c)
-#   if gidcol > 0
-#     for (i,gidrow) in enumerate(rows_c)
-#       if gidrow > 0
-#         push!(aux_row, gidrow+row_offset)
-#         push!(aux_col, gidcol+col_offset)
-#         push!(aux_val, vals_c[i,j])
-#       end
-#     end
-#   end
-# end
-#end
 
 end # module Assemblers
