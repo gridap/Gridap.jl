35abb001936217312d7070e3d0138bf32b812f8b
fverdugo <fverdugo@cimne.upc.edu>
2019-07-07 09:31:03 +0200

    Testing integration on surfaces

--- test/IntegrationTests/BoundaryCellFieldsTests.jl
+++ test/IntegrationTests/BoundaryCellFieldsTests.jl
@@ -15,7 +15,7 @@ u = CellField(trian,ufun)
 
 bu = restrict(u,btrian)
 
-quad = CellQuadrature(btrian,order=8)
+quad = CellQuadrature(btrian,order=0)
 q = coordinates(quad)
 
 bphi = CellGeomap(btrian)
@@ -34,5 +34,22 @@ x2 = evaluate(bphi2,q)
 #writevtk(btrian,"btrian",cellfields=["u"=>bu])
 
 #writevtk(x,"x",pointdata=["u"=>buq])
+
+cn = integrate(bu,btrian,quad)
+@test isa(cn,CellNumber)
+_ = collect(cn)
+
+v = CellBasis(trian)
+bv = restrict(v,btrian)
+cm = varinner(bv,bu)
+
+ca = integrate(cm,btrian,quad)
+@test isa(ca,CellArray{Float64,1})
+_ = collect(ca)
+
+cm = varinner(bv,bv)
+ca = integrate(cm,btrian,quad)
+@test isa(ca,CellArray{Float64,2})
+_ = collect(ca)
 
 end # module
