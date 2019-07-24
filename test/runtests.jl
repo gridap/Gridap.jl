455c7c69e272dbbe4febce13098deae32696f40f
fverdugo <fverdugo@cimne.upc.edu>
2019-07-24 10:20:52 +0200

    Reactivating all project

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,16 +10,16 @@ using Test
 
 @time @testset "RefFEs" begin include("RefFEsTests/runtests.jl") end
 
-#@time @testset "Integration" begin include("IntegrationTests/runtests.jl") end
-#
+@time @testset "Integration" begin include("IntegrationTests/runtests.jl") end
+
 @time @testset "Geometry" begin include("GeometryTests/runtests.jl") end
 
-#@time @testset "Algebra" begin include("AlgebraTests/runtests.jl") end
-#
-#@time @testset "FESpaces" begin include("FESpacesTests/runtests.jl") end
-#
-#@time @testset "MultiField" begin include("MultiFieldTests/runtests.jl") end
-#
-#@time @testset "Visualization" begin include("VisualizationTests/runtests.jl") end
+@time @testset "Algebra" begin include("AlgebraTests/runtests.jl") end
+
+@time @testset "FESpaces" begin include("FESpacesTests/runtests.jl") end
+
+@time @testset "MultiField" begin include("MultiFieldTests/runtests.jl") end
+
+@time @testset "Visualization" begin include("VisualizationTests/runtests.jl") end
 
 end # module
