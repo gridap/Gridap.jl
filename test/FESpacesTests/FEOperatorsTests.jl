5ffde696f691c2b1396e9c1e3c2862569097da42
fverdugo <fverdugo@cimne.upc.edu>
2019-08-02 16:39:56 +0200

    Stressing simulations with triangles and tets

--- test/FESpacesTests/FEOperatorsTests.jl
+++ test/FESpacesTests/FEOperatorsTests.jl
@@ -16,120 +16,10 @@ bfun(x) = 0.0
 # Construct the discrete model
 model = CartesianDiscreteModel(domain=(0.0,1.0,0.0,1.0), partition=(4,4))
 
-# Construct the FEspace
-order = 1
-diritag = "boundary"
-fespace = ConformingFESpace(Float64,model,order,diritag)
+include("FEOperatorsTestsMixin.jl")
 
-# Define test and trial
-V = TestFESpace(fespace)
-U = TrialFESpace(fespace,ufun)
-
-# Define integration mesh and quadrature
-trian = Triangulation(model)
-quad = CellQuadrature(trian,order=2)
-
-# Define cell field describing the source term
-bfield = CellField(trian,bfun)
-
-# Define forms
-a(v,u) = inner(∇(v), ∇(u))
-b(v) = inner(v,bfield)
-
-# Define Assembler
-assem = SparseMatrixAssembler(V,U)
-
-# Define the FEOperator
-op = LinearFEOperator(a,b,V,U,assem,trian,quad)
-
-# Define the FESolver
-ls = LUSolver()
-solver = LinearFESolver(ls)
-
-# Solve!
-uh = solve(solver,op)
-
-# Define exact solution and error
-u = CellField(trian,ufun)
-e = u - uh
-
-# Define norms to measure the error
-l2(u) = inner(u,u)
-h1(u) = a(u,u) + l2(u)
-
-# Compute errors
-el2 = sqrt(sum( integrate(l2(e),trian,quad) ))
-eh1 = sqrt(sum( integrate(h1(e),trian,quad) ))
-
-@test el2 < 1.e-8
-@test eh1 < 1.e-8
-
-#writevtk(trian,"trian",nref=4,cellfields=["uh"=>uh,"u"=>u,"e"=>e])
-
-# Further tests
-
-@test TrialFESpace(op) === U
-@test TestFESpace(op) === V
-
-zh = zero(U)
-res = apply(op,zh)
-r2 = similar(res)
-apply!(r2,op,zh)
-@test res ≈ r2
-
-cache = solve!(zh,solver,op)
-@test free_dofs(zh) ≈ free_dofs(uh)
-
-zh = zero(U)
-solve!(zh,solver,op,cache)
-@test free_dofs(zh) ≈ free_dofs(uh)
-
-# With Neumann BCs
-
-# Manufactured Neumann function
-gfun(x) = 1.0
-
-# Construct the FEspace
-order = 1
-diritags = [1,2,3,4,5,6,7]
-fespace = ConformingFESpace(Float64,model,order,diritags)
-
-# Define test and trial
-V = TestFESpace(fespace)
-U = TrialFESpace(fespace,ufun)
-
-# Setup integration on Neumann boundary
-neumanntags = [8,]
-btrian = BoundaryTriangulation(model,neumanntags)
-bquad = CellQuadrature(btrian,order=2)
-
-# Object describing Neumann function
-gfield = CellField(btrian,gfun)
-
-# Integrand of the Neumann BC
-g(v) = inner(v,gfield)
-
-# Define weak form terms
-t_Ω = AffineFETerm(a,b,trian,quad)
-t_ΓN = FESource(g,btrian,bquad)
-
-# Define Assembler
-assem = SparseMatrixAssembler(V,U)
-
-# Define FE problem
-op = LinearFEOperator(V,U,assem,t_Ω,t_ΓN)
-
-# Solve!
-uh = solve(solver,op)
-
-# Define exact solution and error
-e = u - uh
-
-# Compute errors
-el2 = sqrt(sum( integrate(l2(e),trian,quad) ))
-eh1 = sqrt(sum( integrate(h1(e),trian,quad) ))
-
-#writevtk(trian,"trian",nref=4,cellfields=["uh"=>uh,"u"=>u,"e"=>e])
+# TODO Following test not yet working for triangles
+# since it involves jumps and they do not work for non-oriented facets
 
 @test el2 < 1.e-8
 @test eh1 < 1.e-8
@@ -193,5 +83,11 @@ eh1 = sqrt(sum( integrate(h1(e),trian,quad) ))
 
 @test el2 < 1.e-8
 @test eh1 < 1.e-8
+
+# For triangles
+
+model = simplexify(model)
+
+include("FEOperatorsTestsMixin.jl")
 
 end # module
