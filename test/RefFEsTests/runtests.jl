b1aabfbc1173184d73b803dd4bcd72393e536e95
Santiago Badia <santiago.badia@monash.edu>
2019-09-14 15:40:41 +1000

    Added RT test in runtests

--- test/RefFEsTests/runtests.jl
+++ test/RefFEsTests/runtests.jl
@@ -12,4 +12,6 @@ using Test
 
 @testset "PDiscRefFEs" begin include("PDiscRefFEsTests.jl") end
 
+@testset "RaviartThomasRefFEsTests" begin include("RaviartThomasRefFEsTests.jl") end
+
 end # module
