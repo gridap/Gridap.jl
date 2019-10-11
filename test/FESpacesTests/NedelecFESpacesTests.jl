f1b313a32802398df68f8a9cadba429ff92ec6a4
Santiago Badia <santiago.badia@monash.edu>
2019-10-11 19:48:14 +1100

    Nedelec working for whatever order and 2/3 dim!!!

--- test/FESpacesTests/NedelecFESpacesTests.jl
+++ test/FESpacesTests/NedelecFESpacesTests.jl
@@ -9,8 +9,8 @@ using LinearAlgebra
 D = 2
 order = 1
 
-dom = fill(4,D)
-model = CartesianDiscreteModel(partition=tuple(dom...))
+dom = fill(1,D)
+model = CartesianDiscreteModel(domain=(0.0,1.0,0.0,1.0), partition=(1,1))
 
 p = Polytope(fill(HEX_AXIS,D)...)
 reffe = NedelecRefFE(p,order)
@@ -18,11 +18,14 @@ reffe = NedelecRefFE(p,order)
 grid = Grid(model,D)
 trian = Triangulation(grid)
 graph = GridGraph(model)
-graph.data[2,1]
+
+
 
 labels = FaceLabels(model)
-tags = [5,6,7,8]
+# tags = [5,6,7,8]
+tags = Int[]
 fun(x) = VectorValue(x[1],x[2])
+# fun(x) = VectorValue(1.0,1.0)
 # tags = Int[]
 
 fesp = ConformingFESpace(reffe,trian,graph,labels,tags)
@@ -36,7 +39,19 @@ uh = interpolate(dfesp,fun)
 uh.free_dofs
 uh.diri_dofs
 
+uh2 = FEFunction(uh.fespace,0.5*uh.free_dofs,uh.diri_dofs)
+
 e = fun - uh
+uh.free_dofs
+uh.diri_dofs
+
+ps = ConstantCellVector(1,1)
+isa(ps,CellValue)
+evaluate(uh,ps)
+length(uh)
+length(ps)
+
+
 
 el2 = sqrt(sum(integrate(inner(e,e),trian,quad)))
 @test el2 < 1.0e-10
