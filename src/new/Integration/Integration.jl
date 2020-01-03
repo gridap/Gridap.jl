3161dc1d8e981bee3dfe84cf089026856e2b45b4
Francesc Verdugo <fverdugo@cimne.upc.edu>
2020-01-03 10:53:36 +0100

    Added TensorProductQuadrature

--- src/new/Integration/Integration.jl
+++ src/new/Integration/Integration.jl
@@ -7,19 +7,27 @@ module Integration
 
 using DocStringExtensions
 using Test
+using QuadGK: gauss
 using Gridap.Helpers
 using Gridap.Fields
+using Gridap.TensorValues
 
 import Gridap.ReferenceFEs: num_dims
 import Gridap.ReferenceFEs: num_point_dims
 
 export Quadrature
 export GenericQuadrature
+export TensorProductQuadrature
+export DuffyQuadrature
 export num_points
 export get_coordinates
 export get_weights
 export test_quadrature
 
 include("Quadratures.jl")
+
+include("TensorProductQuadratures.jl")
+
+include("DuffyQuadratures.jl")
 
 end # module
--- test/new/IntegrationTests/runtests.jl
+++ test/new/IntegrationTests/runtests.jl
@@ -4,4 +4,8 @@ using Test
 
 @testset "Quadratures" begin include("QuadraturesTests.jl") end
 
+@testset "TensorProductQuadratures" begin include("TensorProductQuadraturesTests.jl") end
+
+@testset "DuffyQuadratures" begin include("DuffyQuadraturesTests.jl") end
+
 end # module
