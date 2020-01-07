55c72390a13ed6568ed542ddf62251f1031241f3
victorsndvg <victorsv@gmail.com>
2020-01-07 11:26:25 +0100

    Support BSON files management for storing julia objects from dicts

--- src/new/Io/Io.jl
+++ src/new/Io/Io.jl
@@ -9,17 +9,22 @@ using DocStringExtensions
 
 using Gridap.Helpers
 import JSON
+using BSON
 
 export to_dict
 export from_dict
 
 export to_json
 export to_json_file
+export to_bson_file
 export from_json
 export from_json_file
+export from_bson_file
 
 include("IoInterfaces.jl")
 
 include("Json.jl")
+
+include("Bson.jl")
 
 end
