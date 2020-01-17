6aca8c8d2cc893885708ae84034530bac0c6cd2d
victorsndvg <victorsv@gmail.com>
2020-01-17 12:14:32 +0100

    Test show method on GridapTypes

--- test/new/HelpersTests/runtests.jl
+++ test/new/HelpersTests/runtests.jl
@@ -7,5 +7,13 @@ for D = 0:5
   @test tfill(2,Val(D)) == tuple(fill(2,D)...)
 end
 
+struct Foo{A} <: GridapType
+  bar::A
+end
+
+object = Foo(nothing)
+
+@test sprint(show,"text/plain",object) == "$(nameof(typeof(object)))()"
+
 end # module
 
