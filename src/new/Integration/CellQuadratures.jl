57ea8424ccddf7494d2e062304c372df1e675b94
Francesc Verdugo <fverdugo@cimne.upc.edu>
2020-01-17 18:37:49 +0100

    Introducing struct CellQuadrature

--- src/new/Integration/CellQuadratures.jl
+++ src/new/Integration/CellQuadratures.jl
@@ -16,19 +16,12 @@ function Quadrature(p::Polytope{D},degree) where D
 end
 
 """
-    CellQuadrature(polytopes::Vector{<:Polytope}, cell_types::AbstractVector)
 """
-function CellQuadrature(degree,polytopes::Vector{<:Polytope}, cell_types::AbstractVector)
-  f = (p) -> Quadrature(p,degree)
-  quads = map(f,polytopes)
-  CompressedArray(quads,cell_types)
-end
-
-function CellQuadrature(degree,polytopes::Vector{<:Polytope}, cell_types::Fill)
-  ctype = cell_types.value
-  p = polytopes[ctype]
-  quad = Quadrature(p,degree)
-  Fill(quad,length(cell_types))
+struct CellQuadrature <: GridapType
+  array
+  function CellQuadrature(array::AbstractArray{<:Quadrature})
+    new(array)
+  end
 end
 
 """
@@ -41,35 +34,65 @@ function CellQuadrature(trian::Triangulation, degree)
 end
 
 """
-    get_coordinates(q::AbstractArray{<:Quadrature})
+    CellQuadrature(polytopes::Vector{<:Polytope}, cell_types::AbstractVector)
 """
-function get_coordinates(q::AbstractArray{<:Quadrature})
+function CellQuadrature(degree,polytopes::Vector{<:Polytope}, cell_types::AbstractVector)
+  f = (p) -> Quadrature(p,degree)
+  quads = map(f,polytopes)
+  array = CompressedArray(quads,cell_types)
+  CellQuadrature(array)
+end
+
+function CellQuadrature(degree,polytopes::Vector{<:Polytope}, cell_types::Fill)
+  ctype = cell_types.value
+  p = polytopes[ctype]
+  quad = Quadrature(p,degree)
+  array = Fill(quad,length(cell_types))
+  CellQuadrature(array)
+end
+
+"""
+"""
+get_array(quad::CellQuadrature) = quad.array
+
+"""
+    get_coordinates(q::CellQuadrature)
+"""
+function get_coordinates(quad::CellQuadrature)
+  _get_coordinates(quad.array)
+end
+
+"""
+    get_weights(q::CellQuadrature)
+"""
+function get_weights(quad::CellQuadrature)
+  _get_weights(quad.array)
+end
+
+function _get_coordinates(q::AbstractArray{<:Quadrature})
   @notimplemented "Not implemented, since we dont need it"
 end
 
-function get_coordinates(q::CompressedArray{<:Quadrature})
+function _get_coordinates(q::CompressedArray{<:Quadrature})
   coords = map(get_coordinates,q.values)
   CompressedArray(coords,q.ptrs)
 end
 
-function get_coordinates(q::Fill{<:Quadrature})
+function _get_coordinates(q::Fill{<:Quadrature})
   coords = get_coordinates(q.value)
   Fill(coords,length(q))
 end
 
-"""
-    get_weights(q::AbstractArray{<:Quadrature})
-"""
-function get_weights(q::AbstractArray{<:Quadrature})
+function _get_weights(q::AbstractArray{<:Quadrature})
   @notimplemented "Not implemented, since we dont need it"
 end
 
-function get_weights(q::CompressedArray{<:Quadrature})
+function _get_weights(q::CompressedArray{<:Quadrature})
   w = map(get_weights,q.values)
   CompressedArray(w,q.ptrs)
 end
 
-function get_weights(q::Fill{<:Quadrature})
+function _get_weights(q::Fill{<:Quadrature})
   w = get_weights(q.value)
   Fill(w,length(q))
 end
@@ -77,7 +100,7 @@ end
 """
 the `cell_field` is aligned with the cells in `trian`
 """
-function integrate(cell_field,trian::Triangulation,quad::AbstractArray{<:Quadrature})
+function integrate(cell_field,trian::Triangulation,quad::CellQuadrature)
   cell_map = get_cell_map(trian)
   q = get_coordinates(quad)
   w = get_weights(quad)
