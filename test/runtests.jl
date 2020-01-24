047adc7a0d4812efca68e69dfff72cb84a72ec0a
Francesc Verdugo <fverdugo@cimne.upc.edu>
2020-01-24 12:14:23 +0100

    Moved runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,28 +1,35 @@
+module GridapTests
 
-include("new/runtests.jl")
+using Test
 
-#module GridapTests
-#
-#using Test
-#
-#@time @testset "Utils" begin include("UtilsTests/runtests.jl") end
-#
-#@time @testset "CellValues" begin include("CellValuesTests/runtests.jl") end
-#
-#@time @testset "Fields" begin include("FieldsTests/runtests.jl") end
-#
-#@time @testset "RefFEs" begin include("RefFEsTests/runtests.jl") end
-#
-#@time @testset "Integration" begin include("IntegrationTests/runtests.jl") end
-#
-#@time @testset "Geometry" begin include("GeometryTests/runtests.jl") end
-#
-#@time @testset "Algebra" begin include("AlgebraTests/runtests.jl") end
-#
-#@time @testset "FESpaces" begin include("FESpacesTests/runtests.jl") end
-#
-#@time @testset "MultiField" begin include("MultiFieldTests/runtests.jl") end
-#
-#@time @testset "Visualization" begin include("VisualizationTests/runtests.jl") end
-#
-#end # module
+@time @testset "Helpers" begin include("HelpersTests/runtests.jl") end
+
+@time @testset "Inference" begin include("InferenceTests/runtests.jl") end
+
+@time @testset "Io" begin include("IoTests/runtests.jl") end
+
+@time @testset "Algebra" begin include("AlgebraTests/runtests.jl") end
+
+@time @testset "TensorValues" begin include("TensorValuesTests/runtests.jl") end
+
+@time @testset "Arrays" begin include("ArraysTests/runtests.jl") end
+
+@time @testset "Fields" begin include("FieldsTests/runtests.jl") end
+
+@time @testset "Polynomials" begin include("PolynomialsTests/runtests.jl") end
+
+@time @testset "ReferenceFEs" begin include("ReferenceFEsTests/runtests.jl") end
+
+@time @testset "Geometry" begin include("GeometryTests/runtests.jl") end
+
+@time @testset "Integration" begin include("IntegrationTests/runtests.jl") end
+
+@time @testset "FESpaces" begin include("FESpacesTests/runtests.jl") end
+
+@time @testset "Visualization" begin include("VisualizationTests/runtests.jl") end
+
+@time @testset "Gridaps" begin include("GridapTests/runtests.jl") end
+
+include("../../bench/runbenchs.jl")
+
+end # module
