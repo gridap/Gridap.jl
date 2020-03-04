5d1764c4f69a5285f01335ec014b1fa4678e9b4b
Santiago Badia <santiago.badia@monash.edu>
2020-03-05 06:16:18 +1100

    Current version

--- test/FESpacesTests/PhysicalBasesTests.jl
+++ test/FESpacesTests/PhysicalBasesTests.jl
@@ -55,26 +55,52 @@ change_basis(cell_prebasis[1],cell_matrix_inv[1])
 
 evaluate(cell_prebasis,q)
 g_cpb = gradient(cell_prebasis)
-evaluate(g_cpb,q)
+# Juno.@enter gradient(cell_prebasis)
+# Juno.@enter evaluate(g_cpb,q)
 
 shapefuns =  map(get_shapefuns,reffes)
 refshapefuns = CompressedArray(shapefuns,cell_to_ctype)
 cell_shapefuns = attachmap(refshapefuns,cell_map)
 g_csf = gradient(cell_shapefuns)
-evaluate(g_csf,q)
-@which evaluate(g_csf,q)
-@which Gridap.Fields.apply(g_csf,q)
-@which Gridap.Arrays._fill_to_compressed(g_csf,q)
-Gridap.Arrays._fill_to_compressed(g_csf,q)
-f = Gridap.Arrays._fill_to_compressed(g_csf,q)
-Gridap.Arrays._apply_compressed(g_csf,f...)
+# Juno.@enter gradient(cell_shapefuns)
+# Juno.@enter evaluate(g_csf,q)
+typeof(g_csf)
+# Juno.@enter evaluate(g_csf,q)
 
 cell_physshapefuns = apply(change_basis,cell_prebasis,cell_matrix_inv)
 # I don't think I need this step, just to check whether it solved the problem below
 cell_physshapefuns = attachmap(cell_physshapefuns,cell_map)
 g_cpsf = gradient(cell_physshapefuns)
-# evaluate(g_cpsf,q)
+# Juno.@enter evaluate(g_cpsf,q)
+evaluate(g_cpsf,q)
 
+# # # k = g_cpsf.g.value
+# # # f = g_cpsf.f
+# # # g = apply(k,f...)
+# # # h = testitem(g)
+# # # b = h.field
+# # # x = testitems(q...)
+# # # Gridap.Polynomials.gradient_cache(b,x)
+# #
+# # bs1 = g_csf
+# #
+# # k1 = bs1.g.value
+# # f1 = bs1.f
+# # g1 = apply(k1,f1...)
+# # h1 = testitem(g1)
+# # b1 = h1.field
+# # x1 = testitems(q...)
+# # Gridap.Polynomials.gradient_cache(b1,x1)
+#
+# bs2 = g_cpsf
+#
+# k2 = bs2.g.value
+# f2 = bs2.f
+# g2 = apply(k2,f2...)
+# h2 = testitem(g2)
+# b2 = h2.field
+# x2 = testitems(q...)
+# Gridap.Polynomials.gradient_cache(b2,x2)
 ##
 
 function compute_cell_space_physical_space(reffes, cell_to_ctype, cell_map)
