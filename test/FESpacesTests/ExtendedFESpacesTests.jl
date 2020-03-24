bb2813bac708bf2685db1ac45d4976fe8d883069
Francesc Verdugo <fverdugo@cimne.upc.edu>
2020-03-24 09:32:36 +0100

    Bugfix in gradient of functions / bases on ExtendedFESpace

--- test/FESpacesTests/ExtendedFESpacesTests.jl
+++ test/FESpacesTests/ExtendedFESpacesTests.jl
@@ -2,6 +2,7 @@ module ExtendedFESpacesTests
 
 using Test
 using Gridap.Arrays
+using Gridap.TensorValues
 using Gridap.Algebra
 using Gridap.ReferenceFEs
 using Gridap.Geometry
@@ -61,7 +62,7 @@ quad_in = CellQuadrature(trian_in,2*order)
 
 quad_Γ = CellQuadrature(trian_Γ,2*order)
 
-reffes = [LagrangianRefFE(Float64,get_polytope(p),order) for p in get_reffes(trian_in)]
+reffes = [LagrangianRefFE(VectorValue{2,Float64},get_polytope(p),order) for p in get_reffes(trian_in)]
 
 V_in = DiscontinuousFESpace(reffes,trian_in)
 
@@ -72,7 +73,7 @@ test_single_field_fe_space(V)
 U = TrialFESpace(V)
 test_single_field_fe_space(U)
 
-u(x) = x[1]+x[2]
+u(x) = VectorValue(x[1]+x[2], x[1])
 
 uh = interpolate(U,u)
 
@@ -81,17 +82,17 @@ uh_in = restrict(uh,trian_in)
 
 uh_Γ = restrict(uh,trian_Γ)
 
-t_in = AffineFETerm( (u,v) -> v*u, (v) -> v*4, trian_in, quad_in)
+t_in = AffineFETerm( (u,v) -> v*u, (v) -> v*u, trian_in, quad_in)
 op_in = AffineFEOperator(U,V,t_in)
 
 quad = CellQuadrature(trian,2*order)
 
-t_Ω = AffineFETerm( (u,v) -> v*u, (v) -> v*4, trian, quad)
+t_Ω = AffineFETerm( (u,v) -> v*u, (v) -> v*u, trian, quad)
 op_Ω = AffineFEOperator(U,V,t_Ω)
 
 @test get_vector(op_in) ≈ get_vector(op_Ω)
 
-t_Γ = AffineFETerm( (u,v) -> jump(v)*jump(u), (v) -> jump(v)*4, trian_Γ, quad_Γ)
+t_Γ = AffineFETerm( (u,v) -> jump(v)*jump(u) + inner(jump(ε(v)),jump(ε(u))), (v) -> jump(v)*u, trian_Γ, quad_Γ)
 op_Γ = AffineFEOperator(U,V,t_Γ)
 
 q_in = get_coordinates(quad_in)
@@ -102,6 +103,8 @@ collect(evaluate(uh,q))
 
 q_Γ = get_coordinates(quad_Γ)
 collect(evaluate(jump(uh_Γ),q_Γ))
+collect(evaluate(jump(∇(uh_Γ)),q_Γ))
+collect(evaluate(jump(ε(uh_Γ)),q_Γ))
 
 V = TestFESpace(model=model_in,valuetype=Float64,reffe=:Lagrangian,order=2,conformity=:H1)
 @test isa(V,ExtendedFESpace)
