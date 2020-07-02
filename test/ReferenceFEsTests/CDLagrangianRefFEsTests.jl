e7123a4974af7c0f6c2e24f2a369a01d455c66e8
Santiago Badia <santiago.badia@monash.edu>
2020-07-02 15:44:16 +1000

    Tests

--- test/ReferenceFEsTests/CDLagrangianRefFEsTests.jl
+++ test/ReferenceFEsTests/CDLagrangianRefFEsTests.jl
@@ -1,4 +1,4 @@
-module CDLagrangianRefFEsTests
+# module CDLagrangianRefFEsTests
 
 using Gridap
 using Gridap.TensorValues
@@ -8,15 +8,15 @@ T = Float64
 reffe = CDLagrangianRefFE(T,SEGMENT,(2,),(DISC,))
 test_reference_fe(reffe)
 
-reffe = CDLagrangianRefFE(T,QUAD,(2,2),(CONT,DISC))
+T = VectorValue{2,Float64}
+T = Float64
+reffe = CDLagrangianRefFE(T,QUAD,(2,0),(CONT,DISC))
 test_reference_fe(reffe)
+
+reffe.face_own_dofs
 
 T = VectorValue{3,Float64}
 reffe = CDLagrangianRefFE(T,HEX,(2,2,2),(CONT,CONT,DISC))
 test_reference_fe(reffe)
 
-T = Float64
-reffe = LagrangianRefFE(T,QUAD,2)
-reffe.face_own_nodes
-
-end # module
+# end # module
