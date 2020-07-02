a95065bbfeadef35b4312280940c787d916efa1f
Santiago Badia <santiago.badia@monash.edu>
2020-07-02 15:44:58 +1000

    tests

--- test/ReferenceFEsTests/CDLagrangianRefFEsTests.jl
+++ test/ReferenceFEsTests/CDLagrangianRefFEsTests.jl
@@ -9,11 +9,8 @@ reffe = CDLagrangianRefFE(T,SEGMENT,(2,),(DISC,))
 test_reference_fe(reffe)
 
 T = VectorValue{2,Float64}
-T = Float64
 reffe = CDLagrangianRefFE(T,QUAD,(2,0),(CONT,DISC))
 test_reference_fe(reffe)
-
-reffe.face_own_dofs
 
 T = VectorValue{3,Float64}
 reffe = CDLagrangianRefFE(T,HEX,(2,2,2),(CONT,CONT,DISC))
