0f8bd26f7a672cf8725197e8a93c56db06bfef5f
Santiago Badia <santiago.badia@monash.edu>
2020-07-04 17:14:32 +1000

    tests reffe

--- test/ReferenceFEsTests/CDLagrangianRefFEsTests.jl
+++ test/ReferenceFEsTests/CDLagrangianRefFEsTests.jl
@@ -1,4 +1,4 @@
-module CDLagrangianRefFEsTests
+# module CDLagrangianRefFEsTests
 
 using Gridap
 using Gridap.TensorValues
@@ -30,4 +30,10 @@ T = VectorValue{3,Float64}
 reffe = CDLagrangianRefFE(T,HEX,2,(CONT,CONT,DISC))
 test_reference_fe(reffe)
 
-end # module
+T = VectorValue{2,Float64}
+reffe = LagrangianRefFE(T,QUAD,(2,0))
+test_reference_fe(reffe)
+
+reffe = LagrangianRefFE(T,QUAD,(2,2))
+
+# end # module
