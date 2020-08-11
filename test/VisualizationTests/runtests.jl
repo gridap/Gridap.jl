c8e62ea97f21ae245faaedc3c46ffc4d4b8f0cf7
Jan Weidner <jw3126@gmail.com>
2020-08-11 13:36:47 +0200

    fix VisualizationTests

--- test/VisualizationTests/runtests.jl
+++ test/VisualizationTests/runtests.jl
@@ -2,10 +2,10 @@ module VisualizationTests
 
 using Test
 
+@testset "visualization_data" begin include("VisualizationDataTests.jl") end
+
 @testset "PrintOpTrees" begin include("PrintOpTreesTests.jl") end
 
 @testset "Vtk" begin include("VtkTests.jl") end
-
-@testset "visualization_data" begin include("VisualizationData.jl") end
 
 end # module
