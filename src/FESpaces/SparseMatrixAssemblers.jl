770cb8d0d6c7def6f052974810766fef79bf271a
Francesc Verdugo <fverdugo@cimne.upc.edu>
2020-08-20 15:11:25 +0200

    some code relocation

--- src/FESpaces/SparseMatrixAssemblers.jl
+++ src/FESpaces/SparseMatrixAssemblers.jl
@@ -1,3 +1,141 @@
+# This is an extended interface that only makes sense for assemblers that build (sequential) sparse matrices
+# (e.g. not for matrix free assemblers or for distributed assemblers)
+
+"""
+"""
+abstract type SparseMatrixAssembler <: Assembler end
+
+"""
+"""
+function get_matrix_type(a::SparseMatrixAssembler)
+  @abstractmethod
+end
+
+"""
+"""
+function get_vector_type(a::SparseMatrixAssembler)
+  @abstractmethod
+end
+
+function allocate_vector(a::SparseMatrixAssembler,vecdata)
+  n = num_free_dofs(get_test(a))
+  allocate_vector(get_vector_type(a),n)
+end
+
+function assemble_vector!(b,a::SparseMatrixAssembler,vecdata)
+  fill_entries!(b,zero(eltype(b)))
+  assemble_vector_add!(b,a,vecdata)
+end
+
+"""
+"""
+function count_matrix_nnz_coo(a::SparseMatrixAssembler,matdata)
+  @abstractmethod
+end
+
+"""
+"""
+function count_matrix_and_vector_nnz_coo(a::SparseMatrixAssembler,data)
+  @abstractmethod
+end
+
+"""
+"""
+function fill_matrix_coo_symbolic!(I,J,a::SparseMatrixAssembler,matdata,n=0)
+  @abstractmethod
+end
+
+function fill_matrix_and_vector_coo_symbolic!(I,J,a::SparseMatrixAssembler,data,n=0)
+  @abstractmethod
+end
+
+function allocate_matrix(a::SparseMatrixAssembler,matdata)
+  n = count_matrix_nnz_coo(a,matdata)
+  I,J,V = allocate_coo_vectors(get_matrix_type(a),n)
+  fill_matrix_coo_symbolic!(I,J,a,matdata)
+  m = num_free_dofs(get_test(a))
+  n = num_free_dofs(get_trial(a))
+  finalize_coo!(get_matrix_type(a),I,J,V,m,n)
+  sparse_from_coo(get_matrix_type(a),I,J,V,m,n)
+end
+
+function assemble_matrix!(mat,a::SparseMatrixAssembler,matdata)
+  z = zero(eltype(mat))
+  fill_entries!(mat,z)
+  assemble_matrix_add!(mat,a,matdata)
+end
+
+"""
+"""
+function fill_matrix_coo_numeric!(I,J,V,a::SparseMatrixAssembler,matdata,n=0)
+  @abstractmethod
+end
+
+function assemble_matrix(a::SparseMatrixAssembler,matdata)
+
+  n = count_matrix_nnz_coo(a,matdata)
+  I,J,V = allocate_coo_vectors(get_matrix_type(a),n)
+
+  fill_matrix_coo_numeric!(I,J,V,a,matdata)
+
+  m = num_free_dofs(get_test(a))
+  n = num_free_dofs(get_trial(a))
+  finalize_coo!(get_matrix_type(a),I,J,V,m,n)
+  sparse_from_coo(get_matrix_type(a),I,J,V,m,n)
+end
+
+
+function allocate_matrix_and_vector(a::SparseMatrixAssembler,data)
+
+  n = count_matrix_and_vector_nnz_coo(a,data)
+
+  I,J,V = allocate_coo_vectors(get_matrix_type(a),n)
+  fill_matrix_and_vector_coo_symbolic!(I,J,a,data)
+  m = num_free_dofs(get_test(a))
+  n = num_free_dofs(get_trial(a))
+  finalize_coo!(get_matrix_type(a),I,J,V,m,n)
+  A = sparse_from_coo(get_matrix_type(a),I,J,V,m,n)
+
+  b = allocate_vector(get_vector_type(a),m)
+
+  A,b
+end
+
+function assemble_matrix_and_vector!(A,b,a::SparseMatrixAssembler, data)
+  fill_entries!(A,zero(eltype(A)))
+  fill_entries!(b,zero(eltype(b)))
+  assemble_matrix_and_vector_add!(A,b,a,data)
+  A, b
+end
+
+"""
+"""
+function fill_matrix_and_vector_coo_numeric!(I,J,V,b,a::SparseMatrixAssembler,data,n=0)
+  @abstractmethod
+end
+
+function assemble_matrix_and_vector(a::SparseMatrixAssembler, data)
+
+  n = count_matrix_and_vector_nnz_coo(a,data)
+  I,J,V = allocate_coo_vectors(get_matrix_type(a),n)
+  n = num_free_dofs(get_test(a))
+  b = allocate_vector(get_vector_type(a),n)
+
+  fill_matrix_and_vector_coo_numeric!(I,J,V,b,a,data)
+
+  m = num_free_dofs(get_test(a))
+  n = num_free_dofs(get_trial(a))
+  finalize_coo!(get_matrix_type(a),I,J,V,m,n)
+  A = sparse_from_coo(get_matrix_type(a),I,J,V,m,n)
+
+  A, b
+end
+
+function test_sparse_matrix_assembler(a::SparseMatrixAssembler,matdata,vecdata,data)
+  test_assembler(a,matdata,vecdata,data)
+  _ = get_matrix_type(a)
+  _ = get_vector_type(a)
+end
 
 struct GenericSparseMatrixAssembler{M,V} <: SparseMatrixAssembler
   matrix_type::Type{M}
