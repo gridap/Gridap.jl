73680c3a4828e6cb77456098b6101fa8e16a01ac
Francesc Verdugo <fverdugo@cimne.upc.edu>
2020-09-01 17:37:34 +0200

    Saving unfinished changes

--- src/CellData/CellFields.jl
+++ src/CellData/CellFields.jl
@@ -16,10 +16,324 @@ function get_array(cf::CellField)
 end
 
 """
-    get_cell_map(cf::CellField)
+    get_cell_axes(cf::CellField)
 """
-function get_cell_map(cf::CellField)
+function get_cell_axes(cf::CellField)
   @abstractmethod
+end
+
+"""
+    get_memo(cf::CellField)
+"""
+function get_memo(cf::CellField)
+  @abstractmethod
+end
+
+"""
+This trait provides information about the field size at a single
+evaluation point.
+
+For physical fields `MetaSizeStyle(T) == Val( () )`
+For test bases `MetaSizeStyle(T) == Val( (:,) )`
+For trial bases `MetaSizeStyle(T) = Val( (1,:) )`
+For fields representing elemental matrices `MetaSizeStyle(T) = Val( (:,:) )`
+"""
+MetaSizeStyle(::Type{<:CellField}) = @abstractmethod
+MetaSizeStyle(::T) where T = MetaSizeStyle(T)
+get_metasize(::Type{T}) where T = get_val_parameter(MetaSizeStyle(T))
+get_metasize(::T) where T = get_val_parameter(MetaSizeStyle(T))
+is_test(::Type{T}) where T = get_metasize(T) == (:,)
+is_test(::T) where T = is_test(T)
+is_trial(::Type{T}) where T = get_metasize(T) == (1,:)
+is_trial(::T) where T = is_trial(T)
+is_basis(::Type{T}) where T =  is_test(T) || is_trial(T)
+is_basis(::T) where T = is_basis(T)
+
+# Tester
+
+"""
+    test_cell_field(
+      cf::CellField,
+      x::AbstractArray,
+      b::AbstractArray,
+      pred=(==);
+      grad=nothing)
+"""
+function test_cell_field(cf::CellField,x::AbstractArray,b::AbstractArray,pred=(==);grad=nothing)
+  a = evaluate(cf,x)
+  test_array(a,b,pred)
+  if grad != nothing
+    g = evaluate(gradient(cf),x)
+    test_array(g,grad,pred)
+  end
+  @test get_metasize(_cf) == get_metasize(cf)
+  @test get_cell_axes(_cf) == get_cell_axes(cf)
+  @test isa(get_cell_axes(cf),AbstractArray{<:Tuple})
+  @test isa(get_metasize(cf),Tuple)
+  @test isa(MetaSizeStyle(cf),Val)
+  @test isa(get_memo(cf),Dict)
+end
+
+# Concrete implementation
+
+"""
+struct GenericCellField{S} <: CellField
+  # Private fields
+end
+
+"""
+struct GenericCellField{R,S} <: CellField
+  array::AbstractArray
+  cell_axes::AbstractArray
+  metasize::Val{S}
+  memo::Dict
+  function GenericCellField(
+    array::AbstractArray,
+    cell_axes::AbstractArray,
+    metasize::Val{S}) where {S}
+    memo = Dict()
+    new{S}(array,cell_axes,metasize,memo)
+  end
+end
+
+function GenericCellField(array::AbstractArray)
+  cell_axes = Fill((),length(array))
+  metasize = Val(())
+  GenericCellField(array,cell_axes,metasize)
+end
+
+function get_array(cf::GenericCellField)
+  cf.array
+end
+
+function get_cell_axes(cf::GenericCellField)
+  cf.cell_axes
+end
+
+function get_memo(cf::GenericCellField)
+  cf.memo
+end
+
+function MetaSizeStyle(::Type{<:GenericCellField{S}}) where {S}
+  Val{S}()
+end
+
+# The rest of the file is some default API using the CellField interface
+
+"""
+    Base.:(==)(a::CellField,b::CellField)
+
+Check if two `CellField` objects are the same. Uses `===` by default. 
+"""
+function Base.:(==)(a::CellField,b::CellField)
+  a === b
+end
+
+"""
+    length(cf::CellField)
+"""
+function Base.length(cf::CellField)
+  length(get_cell_axes(cf))
+end
+
+"""
+    evaluate(f::CellField,x::AbstractArray)
+
+It is assumed that `x` is in the domain of the array of fields
+in `get_array(f)`.
+"""
+function evaluate(f::CellField,x::AbstractArray)
+  key = (:evaluate,objectid(x))
+  memo = get_memo(cf)
+  if !haskey(memo,key)
+    memo[key] = evaluate_array_of_fields(get_array(f),x)
+  end
+  memo[key]
+end
+
+"""
+    (f::CellField)(x)
+
+Functor like-evaluation `f(x)` for `CellField` objects. Syntactic sugar for `evaluate(f,x)`.
+"""
+function (f::CellField)(x)
+  evaluate(f,x)
+end
+
+"""
+    Base.:∘(f::CellField,ϕ::CellField)
+
+Composition of cell fields `(f∘ϕ)(q) == f(ϕ(q)) `
+"""
+function Base.:∘(f::CellField,ϕ::CellField)
+  @notimplementedif length(f) == length(g)
+  array = compose_field_arrays(get_array(f),get_array(ϕ))
+  GenericCellField(array,get_cell_axes(f),MetaSizeStyle(f))
+end
+
+"""
+    reindex(f::CellField,a::AbstractVector)
+"""
+function Arrays.reindex(f::CellField,a::AbstractVector)
+  array = reindex(get_array(f),a)
+  cell_axes = reindex(get_cell_axes(f),a)
+  GenericCellField(array,cell_axes,MetaSizeStyle(f))
+end
+
+"""
+    gradient(cf::CellField)
+"""
+function gradient(cf::CellField)
+  key = :gradient
+  memo = get_memo(cf)
+  if !haskey(memo,key)
+    a = get_array(cf)
+    ag = field_array_gradient(a)
+    memo[key] = GenericCellField(ag,get_cell_axes(cf),MetaSizeStyle(cf))
+  end
+  memo[key]
+end
+
+"""
+    inverse_map(ϕ::CellField)
+
+Return another `CellField` representing the inverse map of the given map `ϕ`.
+By defalut, it returns an instance of `InverseCellMap`, which keeps the direct cell map `ϕ` as metadata.
+"""
+inverse_map(ϕ::CellField) = InverseCellMap(ϕ)
+
+inverse_map(ϕ::AbstractArray) = @notimplemented
+
+"""
+    struct InverseCellMap <: CellField
+      direct_cell_map::CellField
+      # + private fields
+    end
+
+An inverse cell map that keeps the direct cell map as metadata.
+"""
+struct InverseCellMap <: CellField
+  direct_cell_map::CellField
+  inverse_cell_map::CellField
+  function InverseCellMap(direct_cell_map::CellField)
+    array = inverse_map(get_array(direct_cell_map))
+    @notimplementedif length(array) != length(direct_cell_map)
+    inverse_cell_map = GenericCellField(
+      array,get_cell_axes(direct_cell_map),MetaSizeStyle(direct_cell_map))
+    new(direct_cell_map,inverse_cell_map)
+  end
+end
+
+get_array(a::InverseCellMap) = get_array(a.inverse_cell_map)
+get_cell_axes(a::InverseCellMap) = get_cell_axes(a.inverse_cell_map)
+get_memo(a::InverseCellMap) = get_memo(a.inverse_cell_map)
+MetaSizeStyle(::Type{InverseCellMap}) = Val(())
+
+function Arrays.reindex(f::InverseCellMap,a::AbstractVector)
+  InverseCellMap(reindex(f.direct_cell_map,a),reindex(f.inverse_cell_map))
+end
+
+"""
+    struct CellFieldComposedWithInverseMap{T<:CellField} <: CellField
+      f::T
+      map::InverseCellMap
+    end
+struct representing `f∘inverse_map(ϕ)`.  This struct allows a number of optimizations.
+In particular, the computation of `f∘inverse_map(ϕ)∘ϕ`, which is simply computed as`f`.
+It also computes `∇(f∘inverse_map(ϕ))` as `(inv(ϕ)*∇(f))∘inverse_map(ϕ)` which is in turn
+stored in an instance of `CellFieldComposedWithInverseMap`.
+"""
+struct CellFieldComposedWithInverseMap{T<:CellField} <: CellField
+  f::T
+  map::InverseCellMap
+end
+
+# TODO
+get_array(a::CellFieldComposedWithInverseMap) = @notimplemented
+get_cell_axes(a::CellFieldComposedWithInverseMap) = @notimplemented
+get_memo(a::CellFieldComposedWithInverseMap) = @notimplemented
+MetaSizeStyle(::Type{CellFieldComposedWithInverseMap}) = @notimplemented
+evaluate(a::CellFieldComposedWithInverseMap,x::AbstractArray) = @notimplemented
+
+function Base.:∘(f::CellFieldComposedWithInverseMap,ϕ::CellField)
+  if f.map.direct_cell_map == ϕ
+    return f.f
+  else
+    @notimplemented
+    return (f.f∘f.map.inverse_map)∘ϕ
+  end
+end
+
+function Arrays.reindex(f::CellFieldComposedWithInverseMap,a::AbstractVector)
+  CellFieldComposedWithInverseMap(reindex(f.f,a),reindex(a.map))
+end
+
+# TODO
+function gradient(cf::CellFieldComposedWithInverseMap)
+  @notimplemented
+end
+
+"""
+    struct CellFieldFromOperation{S} <: CellField
+      op
+      args::Tuple
+    end
+
+Struct representing a `CellField` defined as an operation between other `CellField` objects.
+This struct allows to compute, e.g., `(a*b)∘ϕ` as `(a∘ϕ)*(b∘ϕ)`, which is the trick needed to 
+implement operations involving `CellField` objects defined on different geometrical objects.
+"""
+struct CellFieldFromOperation{S} <: CellField
+  op
+  args::Tuple
+end
+
+# They are not needed in practice. So, we don't implement them for the moment.
+get_array(a::CellFieldFromOperation) = @notimplemented
+get_cell_axes(a::CellFieldFromOperation) = @notimplemented
+get_memo(a::CellFieldFromOperation) = @notimplemented
+MetaSizeStyle(::Type{CellFieldFromOperation}) = @notimplemented
+evaluate(a::CellFieldFromOperation,x::AbstractArray) = @notimplemented
+
+
+
+
+
+
+
+
+
+
+
+get_array(cf,::Val{false}) = @abstractmethod
+
+function get_array(f,::Val{true})
+  ϕ = get_cell_map(f)
+  get_array((f∘ϕ)∘inverse_map(ϕ))
+end
+
+inverse_map(ϕ::CellField) = inverse_map(get_array(ϕ))
+inverse_map(ϕ::AbstractArray) = @notimplemented
+
+function Base.:∘(f::CellField,ϕ::CellField)
+  _compose(f,RefStyle(f),ϕ)
+end
+
+function _compose(f,::Val{false},ϕ)
+  a = compose_field_arrays(get_array(f),get_array(ϕ))
+  similar_object(f,a) # TODO
+end
+
+function _compose(f,::Val{true},ϕ)
+  if get_cell_map(f) == ϕ 
+    similar_object(f,get_ref_array(f)) # TODO
+  else
+    _compose(f,Val(false),ϕ)
+  end
+end
+
+function Base.:(==)(a::CellField,b::CellField)
+  a === b
 end
 
 """
@@ -34,14 +348,30 @@ function get_memo(cf::CellField)
 end
 
 """
-This trait returns `Val{true}()` when the `CellField` is defined in a
-reference finite element space, and `Val{false}()` when it is defined in the
-physical space
+This trait returns `Val(true)` when the concept of reference space
+is defined for the `CellField`, and `Val(false)` otherwise.
+All `CellField` types such that this trait is `Val(true)` should define a function
+[`get_cell_map(cf::CellField)`](@ref) which returns the cell map defining the reference space.
+A cell map is a `CellField` object whose domain is the reference space.
+Moreover, types with this trait need to implement
+`get_ref_array(cf::CellField)` that returns an array of fields defined in the reference space.
+In this case, `get_array(cf::CellField)` returns an array of fields in the physical space.
 """
 RefStyle(::Type{<:CellField}) = @notimplemented
 
+"""
+    get_cell_map(cf::CellField)
+"""
+function get_cell_map(cf::CellField)
+  @abstractmethod
+end
+
+function get_ref_array(cf::CellField)
+  @abstractmethod
+end
+
 # We use duck typing here for all types marked with the RefStyle
-RefStyle(::Type) = @notimplemented
+RefStyle(::Type) = @abstractmethod
 RefStyle(a) = RefStyle(typeof(a))
 is_in_ref_space(::Type{T}) where T = get_val_parameter(RefStyle(T))
 is_in_ref_space(::T) where T = is_in_ref_space(T)
@@ -71,101 +401,7 @@ is_trial(::T) where T = is_trial(T)
 is_basis(::Type{T}) where T =  is_test(T) || is_trial(T)
 is_basis(::T) where T = is_basis(T)
 
-# Tester
 
-"""
-    test_cell_field(
-      cf::CellField,
-      x::AbstractArray,
-      b::AbstractArray,
-      pred=(==);
-      grad=nothing)
-"""
-function test_cell_field(cf::CellField,x::AbstractArray,b::AbstractArray,pred=(==);grad=nothing)
-  cell_map = get_cell_map(cf)
-  @test isa(cell_map,AbstractArray)
-  a = evaluate(cf,x)
-  test_array(a,b,pred)
-  if grad != nothing
-    g = evaluate(gradient(cf),x)
-    test_array(g,grad,pred)
-  end
-  rs = RefStyle(cf)
-  @test isa(get_val_parameter(rs),Bool)
-  _cf = change_ref_style(cf)
-  @test get_array(_cf) === get_array(cf)
-  @test is_in_ref_space(cf) == !is_in_ref_space(_cf)
-  @test is_in_physical_space(cf) == !is_in_physical_space(_cf)
-  @test get_metasize(_cf) == get_metasize(cf)
-  @test get_cell_axes(_cf) == get_cell_axes(cf)
-  @test isa(get_cell_axes(cf),AbstractArray{<:Tuple})
-  @test isa(get_metasize(cf),Tuple)
-  @test isa(MetaSizeStyle(cf),Val)
-  @test isa(get_memo(cf),Dict)
-end
-
-# Concrete implementation
-
-"""
-struct GenericCellField{R,S} <: CellField
-  # Private fields
-end
-
-"""
-struct GenericCellField{R,S} <: CellField
-  array::AbstractArray
-  cell_map::AbstractArray
-  ref_trait::Val{R}
-  cell_axes::AbstractArray
-  metasize::Val{S}
-  memo::Dict
-  function GenericCellField(
-    array::AbstractArray,
-    cell_map::AbstractArray,
-    ref_trait::Val{R},
-    cell_axes::AbstractArray,
-    metasize::Val{S}) where {R,S}
-    memo = Dict()
-    new{R,S}(array,cell_map,ref_trait,cell_axes,metasize,memo)
-  end
-end
-
-function GenericCellField(array::AbstractArray,cell_map::AbstractArray)
-  GenericCellField(array,cell_map,Val{true}(),Fill((),length(cell_map)))
-end
-
-function GenericCellField(array::AbstractArray,cell_map::AbstractArray,ref_trait::Val)
-  GenericCellField(array,cell_map,ref_trait,Fill((),length(cell_map)))
-end
-
-function GenericCellField(
-  array::AbstractArray,cell_map::AbstractArray,ref_trait::Val,cell_axes::AbstractArray)
-  GenericCellField(array,cell_map,ref_trait,cell_axes,Val(()))
-end
-
-function get_array(cf::GenericCellField)
-  cf.array
-end
-
-function get_cell_map(cf::GenericCellField)
-  cf.cell_map
-end
-
-function get_memo(cf::GenericCellField)
-  cf.memo
-end
-
-function get_cell_axes(cf::GenericCellField)
-  cf.cell_axes
-end
-
-function RefStyle(::Type{<:GenericCellField{R}}) where {R}
-  Val{R}()
-end
-
-function MetaSizeStyle(::Type{<:GenericCellField{R,S}}) where {R,S}
-  Val{S}()
-end
 
 # The rest of the file is some default API using the CellField interface
 
