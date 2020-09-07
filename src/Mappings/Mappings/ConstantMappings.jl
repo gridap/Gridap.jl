6edf324d8c6c600aa1fab99596c8ee05d14d6ffe
Santiago Badia <santiago.badia@monash.edu>
2020-09-07 12:35:18 +1000

    More tests

---
+++ src/Mappings/Mappings/ConstantMappings.jl
@@ -0,0 +1,68 @@
+struct ConstantMapping{T} <: Mapping
+  v::T
+  function ConstantMapping{T}(v::Union{Number,AbstractArray{<:Number}}) where {T}
+    new{typeof(v)}(v)
+  end
+end
+
+ConstantMapping(v::Union{Number,AbstractArray{<:Number}}) = ConstantMapping{typeof(v)}(v)
+
+constant_mapping(v::Union{Number,AbstractArray{<:Number}}) =  ConstantMapping(v)
+
+# Number
+
+# function return_cache(f::ConstantMapping,x...)
+  # nx = length(x)
+  # c = zeros(typeof(f.v),nx)
+  # CachedArray(c)
+# end
+
+function return_cache(f::ConstantMapping,x::AbstractArray)
+  nx = length(x)
+  sv = size(f.v)
+  s = (nx,sv...)
+  c = zeros(eltype(f.v),s)
+  cis = CartesianIndices(f.v)
+  for i in eachindex(x)
+    for ci in cis
+      @inbounds c[i,ci] = f.v[ci]
+    end
+  end
+  c
+end
+
+function evaluate!(c,f::ConstantMapping,x...)
+  return f.v
+end
+
+function evaluate!(c,f::ConstantMapping,x::AbstractArray)
+  return c
+end
+
+gradient(f::ConstantMapping) = GradientConstantMapping(f)
+
+# @santiagobadia: Think about recursive struct for higher derivatives
+struct GradientConstantMapping <: Mapping
+  m::ConstantMapping
+end
+
+function return_cache(f::GradientConstantMapping,x::AbstractArray)
+  nx = length(x)
+  if nx > 1
+    sx = size(x[1]) # must be identical for all entries
+    sv = size(f.m.v)
+    s = (nx,sx...,sv...)
+    c = zeros(eltype(f.m.v),s)
+  else
+    c = nothing
+  end
+  return c
+end
+
+function evaluate!(c,f::GradientConstantMapping,x...)
+  return c
+end
+
+function evaluate!(c,f::GradientConstantMapping,x::AbstractArray)
+  return c
+end
---
+++ src/Mappings/Mappings/Tests/CompositionMappingsTests.jl
@@ -0,0 +1,98 @@
+module CompositionMappingsTests
+
+using Test
+# using Gridap.Arrays
+using Gridap.Mappings
+using FillArrays
+using Gridap.TensorValues
+
+using Gridap.NewFields
+using Gridap.NewFields: MockField, MockBasis, OtherMockBasis
+
+k = FunctionMapping(-)
+
+v = 3.0
+d = 2
+f = MockField{d}(v)
+g = composition(k,f)
+
+p = 4
+p1 = Point(1,2)
+p2 = Point(2,1)
+p3 = Point(4,3)
+p4 = Point(6,1)
+x = [p1,p2,p3,p4]
+
+fx = evaluate(f,x)
+∇fx = evaluate(∇(f),x)
+gx = evaluate(k,fx)
+∇gx = evaluate(k,∇fx)
+# santiagobadia :  I don't like the different notation for
+# mapping and kernel, i.e., tuple vs non-tuple, to be fixed
+test_mapping(g,(x,),gx) #,grad=∇gx)
+
+# @santiagobadia : Create the gradient method for composition
+# using chain rule
+∇g = composition(k,∇(f))
+test_mapping(∇g,(x,),∇gx)
+
+fi = 3.0
+gi = 4.5
+d = 2
+f = MockField{d}(fi)
+g = MockField{d}(gi)
+
+# @santiagobadia : I like the tuple, k ∘ (f,g)
+h = composition(k,(f,g))
+
+hx = evaluate(k,evaluate(f,x),evaluate(g,x))
+∇hx = evaluate(k,evaluate(∇(f),x),evaluate(∇(g),x))
+test_mapping(h,(x,),hx) #,grad=∇hx)
+
+∇h = composition(k,(∇(f),∇(g)))
+test_mapping(∇h,(x,),∇hx)
+
+fi = 3.0
+gi = VectorValue(4,5)
+d = 2
+f = MockField{d}(fi)
+g = ConstantField(gi)
+h = composition(k,(f,g))
+
+hx = evaluate(k,evaluate(f,x),evaluate(g,x))
+∇hx = evaluate(k,evaluate(∇(f),x),evaluate(∇(g),x))
+test_mapping(h,(x,),hx)
+
+∇h = composition(k,(∇(f),∇(g)))
+test_mapping(∇h,(x,),∇hx)
+
+
+k = BroadcastMapping(-)
+fi = 3.0
+gi = [1,2,3,4,5]
+d = 2
+f = MockField{d}(fi)
+g = ConstantMapping(gi)
+h = composition(k,(f,g))
+
+# @santiagobadia : A field can be paralelised wrt array of points
+# A mapping on the contrary can take multiple arguments...
+# We must create two evalutes, one for arrays of tuples and
+# one for one tuple
+# @santiagobadia : The previous version of gradient is wrong
+# @santiagobadia : We could also have derivative (transpose of gradient),
+# i.e., a Jacobian for f : Rn -> Rm
+
+hx = evaluate(k,evaluate(f,x),evaluate(g,x))
+test_mapping(h,(x,),hx) #,grad=∇hx)
+
+∇h = composition(k,(∇(f),∇(g)))
+∇hx = evaluate(k,evaluate(∇(f),x),evaluate(∇(g),x))
+test_mapping(∇h,(x,),∇hx)
+
+
+f1(1,2,3,4)
+
+f1(x...) = x
+
+end # module
