3ca57ab734bc4e300fdb336253162ca842d95adf
Francesc Verdugo <fverdugo@cimne.upc.edu>
2020-10-25 17:56:21 +0100

    Trying to fix tests for julia 1.3 (again)

--- src/ReferenceFEs/Dofs.jl
+++ src/ReferenceFEs/Dofs.jl
@@ -70,24 +70,24 @@ abstract type Dof <: Map end
 # Testers
 
 """
-    test_dof(dof,field,v,comp::Function=(==))
+    test_dof(dof,field,v;cmp::Function=(==))
 
 Test that the `Dof` interface is properly implemented
 for object `dof`. It also checks if the object `dof`
 when evaluated at the field `field` returns the same
 value as `v`. Comparison is made with the `comp` function.
 """
-function test_dof(dof::Dof,field,v,comp::Function=(==))
-  _test_dof(dof,field,v,comp)
+function test_dof(dof::Dof,field,v;cmp::Function=(==))
+  _test_dof(dof,field,v,cmp)
 end
 
-function test_dof_array(dof::AbstractArray{<:Dof},field,v,comp::Function=(==))
-  _test_dof(dof,field,v,comp)
+function test_dof_array(dof::AbstractArray{<:Dof},field,v;cmp::Function=(==))
+  _test_dof(dof,field,v,cmp)
 end
 
-function _test_dof(dof,field,v,cmp::Function=(==))
+function _test_dof(dof,field,v,cmp)
   if isa(dof,Dof)
-    test_mapping(v,dof,field;cmp)
+    test_mapping(v,dof,field;cmp=cmp)
   end
   r = evaluate(dof,field)
   @test cmp(r,v)
