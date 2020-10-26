60e5503998330a9b281d14aadbd192515bc4846a
Francesc Verdugo <fverdugo@cimne.upc.edu>
2020-10-26 14:57:40 +0100

    Reactivated TriangulationPortion

--- src/Geometry/TriangulationPortions.jl
+++ src/Geometry/TriangulationPortions.jl
@@ -1,53 +1,115 @@
 
-function reindex(trian::Triangulation,indices)
-  TriangulationPortion(trian,collect(Int,indices))
-end
-
-function reindex(trian::Triangulation,indices::Vector{Int})
-  TriangulationPortion(trian,indices)
-end
-
-function reindex(trian::Triangulation,indices::IdentityVector)
-  trian
-end
-
 """
 """
-struct TriangulationPortion{Dc,Dp,G} <: Triangulation{Dc,Dp}
-  oldtrian::G
-  cell_to_oldcell::Vector{Int}
+struct TriangulationPortion{Dc,Dp,G,A} <: Triangulation{Dc,Dp}
+  parent_trian::G
+  cell_to_parent_cell::A
   @doc """
   """
-  function TriangulationPortion(oldtrian::Triangulation{Dc,Dp},cell_to_oldcell::Vector{Int}) where {Dc,Dp}
-    new{Dc,Dp,typeof(oldtrian)}(oldtrian,cell_to_oldcell)
+  function TriangulationPortion(
+    parent_trian::Triangulation,
+    cell_to_parent_cell::AbstractVector{<:Integer})
+
+    Dc = num_cell_dims(parent_trian)
+    Dp = num_point_dims(parent_trian)
+    G = typeof(parent_trian)
+    A = typeof(cell_to_parent_cell)
+    new{Dc,Dp,G,A}(parent_trian,cell_to_parent_cell)
   end
 end
 
-function get_reffes(trian::TriangulationPortion)
-  get_reffes(trian.oldtrian)
+TriangulationStyle(::Type{<:TriangulationPortion}) = SubTriangulation()
+
+get_background_triangulation(trian::TriangulationPortion) = get_background_triangulation(trian.parent_trian)
+
+get_node_coordinates(trian::TriangulationPortion) = get_node_coordinates(trian.parent_trian)
+
+get_reffes(trian::TriangulationPortion) = get_reffes(trian.parent_trian)
+
+function get_cell_coordinates(trian::TriangulationPortion)
+  parent_cell_data = get_cell_coordinates(trian.parent_trian) 
+  lazy_map(Reindex(parent_cell_data),trian.cell_to_parent_cell)
 end
 
 function get_cell_type(trian::TriangulationPortion)
-  reindex(get_cell_type(trian.oldtrian),trian.cell_to_oldcell)
+  parent_cell_data = get_cell_type(trian.parent_trian) 
+  lazy_map(Reindex(parent_cell_data),trian.cell_to_parent_cell)
 end
 
-function get_cell_coordinates(trian::TriangulationPortion)
-  reindex(get_cell_coordinates(trian.oldtrian),trian.cell_to_oldcell)
+function get_cell_nodes(trian::TriangulationPortion)
+  parent_cell_data = get_cell_nodes(trian.parent_trian) 
+  lazy_map(Reindex(parent_cell_data),trian.cell_to_parent_cell)
 end
 
 function get_cell_map(trian::TriangulationPortion)
-  cell_map = get_cell_map(trian.oldtrian)
-  reindex(cell_map,trian.cell_to_oldcell)
+  parent_cell_data = get_cell_map(trian.parent_trian) 
+  lazy_map(Reindex(parent_cell_data),trian.cell_to_parent_cell)
 end
 
 function get_cell_id(trian::TriangulationPortion)
-  reindex(get_cell_id(trian.oldtrian),trian.cell_to_oldcell)
+  parent_cell_data = get_cell_id(trian.parent_trian) 
+  lazy_map(Reindex(parent_cell_data),trian.cell_to_parent_cell)
 end
 
-function restrict(f::AbstractArray,trian::TriangulationPortion)
-  reindex(restrict(f,trian.oldtrian),trian.cell_to_oldcell)
+function get_facet_normal(trian::TriangulationPortion)
+  parent_cell_data = get_facet_normal(trian.parent_trian) 
+  lazy_map(Reindex(parent_cell_data),trian.cell_to_parent_cell)
 end
 
-function get_normal_vector(trian::TriangulationPortion)
-  reindex(get_normal_vector(trian.oldtrian),trian.cell_to_oldcell)
+function get_cell_ref_map(trian::TriangulationPortion)
+  parent_cell_data = get_cell_ref_map(trian.parent_trian) 
+  lazy_map(Reindex(parent_cell_data),trian.cell_to_parent_cell)
 end
+
+#function reindex(trian::Triangulation,indices)
+#  TriangulationPortion(trian,collect(Int,indices))
+#end
+#
+#function reindex(trian::Triangulation,indices::Vector{Int})
+#  TriangulationPortion(trian,indices)
+#end
+#
+#function reindex(trian::Triangulation,indices::IdentityVector)
+#  trian
+#end
+#
+#"""
+#"""
+#struct TriangulationPortion{Dc,Dp,G} <: Triangulation{Dc,Dp}
+#  oldtrian::G
+#  cell_to_oldcell::Vector{Int}
+#  @doc """
+#  """
+#  function TriangulationPortion(oldtrian::Triangulation{Dc,Dp},cell_to_oldcell::Vector{Int}) where {Dc,Dp}
+#    new{Dc,Dp,typeof(oldtrian)}(oldtrian,cell_to_oldcell)
+#  end
+#end
+#
+#function get_reffes(trian::TriangulationPortion)
+#  get_reffes(trian.oldtrian)
+#end
+#
+#function get_cell_type(trian::TriangulationPortion)
+#  reindex(get_cell_type(trian.oldtrian),trian.cell_to_oldcell)
+#end
+#
+#function get_cell_coordinates(trian::TriangulationPortion)
+#  reindex(get_cell_coordinates(trian.oldtrian),trian.cell_to_oldcell)
+#end
+#
+#function get_cell_map(trian::TriangulationPortion)
+#  cell_map = get_cell_map(trian.oldtrian)
+#  reindex(cell_map,trian.cell_to_oldcell)
+#end
+#
+#function get_cell_id(trian::TriangulationPortion)
+#  reindex(get_cell_id(trian.oldtrian),trian.cell_to_oldcell)
+#end
+#
+#function restrict(f::AbstractArray,trian::TriangulationPortion)
+#  reindex(restrict(f,trian.oldtrian),trian.cell_to_oldcell)
+#end
+#
+#function get_normal_vector(trian::TriangulationPortion)
+#  reindex(get_normal_vector(trian.oldtrian),trian.cell_to_oldcell)
+#end
--- test/GeometryTests/TriangulationPortionsTests.jl
+++ test/GeometryTests/TriangulationPortionsTests.jl
@@ -13,29 +13,11 @@ cell_to_oldcell = collect(1:34)
 trian = TriangulationPortion(oldtrian,cell_to_oldcell)
 test_triangulation(trian)
 
-oldbtrian = BoundaryTriangulation(oldmodel)
-
-bface_to_oldbface = collect(1:32)
-btrian = TriangulationPortion(oldbtrian,bface_to_oldbface)
-test_triangulation(btrian)
-
-nb = get_normal_vector(btrian)
-@test length(nb) == num_cells(btrian)
-
-sface_to_oldsface = collect(10:45)
-oldstrian = SkeletonTriangulation(oldmodel)
-strian = TriangulationPortion(oldstrian,sface_to_oldsface)
-test_triangulation(strian)
-
-ns = get_normal_vector(strian)
-@test length(ns) == num_cells(strian)
-
-v = [i for i=1:num_cells(trian)]
 cell_to_oldcell = [2,9,7]
 trian_portion = TriangulationPortion(oldtrian,cell_to_oldcell)
 trian_portion_portion = TriangulationPortion(trian_portion,[3,1])
+test_triangulation(trian_portion_portion)
 @test get_cell_id(trian_portion_portion)==[7,2]
-@test restrict(v, trian_portion_portion)==[7,2]
 
 #using Gridap.Visualization
 #writevtk(oldtrian,"oldtrian")
