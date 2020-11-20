2eb4d26f4ba2aef8a670b9fe18ba1f840bbfa54f
Francesc Verdugo <fverdugo@cimne.upc.edu>
2020-11-20 09:35:13 +0100

    PLaplacianTests also checks autodiff

--- test/GridapTests/PLaplacianTests.jl
+++ test/GridapTests/PLaplacianTests.jl
@@ -3,10 +3,11 @@ module PLaplacianTests
 using Test
 using Gridap
 import Gridap: ∇
-using Gridap.Geometry: DiscreteModelMock
 using LinearAlgebra
 
-model = DiscreteModelMock()
+domain = (0,1,0,1)
+cells = (4,4)
+model = CartesianDiscreteModel(domain,cells)
 
 u(x) = x[1] + x[2]
 ∇u(x) = VectorValue(1,1)
@@ -35,6 +36,27 @@ op = FEOperator(res,jac,U,V)
 
 nls = NLSolver(show_trace=false, method=:newton)
 solver = FESolver(nls)
+
+x = rand(T,num_free_dofs(U))
+uh0 = FEFunction(U,x)
+uh, = solve!(uh0,solver,op)
+
+e = u - uh
+
+l2(u) = sqrt(sum( ∫( u⊙u )*dΩ ))
+h1(u) = sqrt(sum( ∫( u⊙u + ∇(u)⊙∇(u) )*dΩ ))
+
+el2 = l2(e)
+eh1 = h1(e)
+ul2 = l2(uh)
+uh1 = h1(uh)
+
+@test el2/ul2 < 1.e-8
+@test eh1/uh1 < 1.e-7
+
+# now with autodiff
+
+op = FEOperator(res,U,V)
 
 x = rand(T,num_free_dofs(U))
 uh0 = FEFunction(U,x)
