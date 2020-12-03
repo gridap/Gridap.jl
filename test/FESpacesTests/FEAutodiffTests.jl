4622bd01340e92810bd12166bc35d5537bacd9b6
Francesc Verdugo <fverdugo@cimne.upc.edu>
2020-12-03 17:52:33 +0100

    Using \approx in FEAutodiffTests

--- test/FESpacesTests/FEAutodiffTests.jl
+++ test/FESpacesTests/FEAutodiffTests.jl
@@ -36,9 +36,9 @@ cell_r_auto = get_array(gradient(ener,uh))
 cell_j_auto = get_array(jacobian(res,uh))
 cell_h_auto = get_array(hessian(ener,uh))
 
-test_array(cell_r_auto,cell_r)
-test_array(cell_j_auto,cell_j)
-test_array(cell_h_auto,cell_h)
+test_array(cell_r_auto,cell_r,≈)
+test_array(cell_j_auto,cell_j,≈)
+test_array(cell_h_auto,cell_h,≈)
 
 Γ = BoundaryTriangulation(model)
 dΓ = LebesgueMeasure(Γ,2)
@@ -55,9 +55,9 @@ cell_r_auto = get_array(gradient(ener,uh))
 cell_j_auto = get_array(jacobian(res,uh))
 cell_h_auto = get_array(hessian(ener,uh))
 
-test_array(cell_r_auto,cell_r)
-test_array(cell_j_auto,cell_j)
-test_array(cell_h_auto,cell_h)
+test_array(cell_r_auto,cell_r,≈)
+test_array(cell_j_auto,cell_j,≈)
+test_array(cell_h_auto,cell_h,≈)
 
 ener(uh) = ∫( 0.5*∇(uh)⋅∇(uh) )*dΓ + ∫( 0.5*∇(uh)⋅∇(uh) )*dΩ
 res(uh) = ∫( ∇(uh)⋅∇(dv) )*dΓ + ∫(∇(uh)⋅∇(dv))*dΩ
@@ -71,13 +71,13 @@ cell_r_auto = gradient(ener,uh)
 cell_j_auto = jacobian(res,uh)
 cell_h_auto = hessian(ener,uh)
 
-test_array(cell_r_auto[Ω],cell_r[Ω])
-test_array(cell_j_auto[Ω],cell_j[Ω])
-test_array(cell_h_auto[Ω],cell_h[Ω])
+test_array(cell_r_auto[Ω],cell_r[Ω],≈)
+test_array(cell_j_auto[Ω],cell_j[Ω],≈)
+test_array(cell_h_auto[Ω],cell_h[Ω],≈)
 
-test_array(cell_r_auto[Γ],cell_r[Γ])
-test_array(cell_j_auto[Γ],cell_j[Γ])
-test_array(cell_h_auto[Γ],cell_h[Γ])
+test_array(cell_r_auto[Γ],cell_r[Γ],≈)
+test_array(cell_j_auto[Γ],cell_j[Γ],≈)
+test_array(cell_h_auto[Γ],cell_h[Γ],≈)
 
 const p = 3
 j(∇u) = norm(∇u)^(p-2) * ∇u
@@ -90,6 +90,6 @@ jac(u,du,v) = ∫( ∇(v)⋅(dj∘(∇(du),∇(u))) )*dΩ
 cell_j = get_array(jac(uh,du,dv))
 cell_j_auto = get_array(jacobian(u->res(u,dv),uh))
 
-test_array(cell_j_auto,cell_j)
+test_array(cell_j_auto,cell_j,≈)
 
 end # module
