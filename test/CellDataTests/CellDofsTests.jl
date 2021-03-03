fbd8898af65638e9ef1ad9a758572aa0c3dcb74c
Francesc Verdugo <fverdugo@cimne.upc.edu>
2021-03-03 11:54:18 +0100

    Implemented get_cell_points for CellDof

--- test/CellDataTests/CellDofsTests.jl
+++ test/CellDataTests/CellDofsTests.jl
@@ -30,6 +30,10 @@ a = s(v)
 r = fill(Matrix(I,4,4),num_cells(trian))
 test_array(a,r)
 
+x = get_cell_points(s)
+vx = v(x)
+test_array(vx,r)
+
 acell_to_cell = [1,3,5,3]
 atrian = Triangulation(trian,acell_to_cell)
 s_a = change_domain(s,atrian,DomainStyle(s))
@@ -38,5 +42,9 @@ v_a = change_domain(v,atrian,DomainStyle(v))
 a = s_a(v_a)
 r = fill(Matrix(I,4,4),num_cells(atrian))
 test_array(a,r)
+
+x_a = get_cell_points(s_a)
+vx_a = v(x_a)
+test_array(vx_a,r)
 
 end # module
