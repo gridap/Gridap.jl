d6187d1edf6583048c5eb2a60dcbe6c717fcfd0a
Erik Schnetter <schnetter@gmail.com>
2021-03-03 23:04:18 -0500

    Commit current state

--- src/MultiField/MultiField.jl
+++ src/MultiField/MultiField.jl
@@ -18,7 +18,9 @@ using Gridap.Fields
 
 using Gridap.FESpaces: FEBasis, TestBasis, TrialBasis, get_cell_dof_values
 using Gridap.Arrays: BlockArrayCooMap
-using Gridap.ReferenceFEs: HEX_AXIS, TET_AXIS, get_node_coordinates
+using Gridap.ReferenceFEs: HEX_AXIS, TET_AXIS,
+                           get_faces, get_grid_topology, get_node_coordinates,
+                           num_cell_dims
 
 using FillArrays
 using SparseArrays
--- src/MultiField/MultiFieldCellFields.jl
+++ src/MultiField/MultiFieldCellFields.jl
@@ -50,16 +50,15 @@ function return_cache(f::CellField,x::Point)
   node_coordinates = get_node_coordinates(trian)
   node_coordinates = reshape(node_coordinates, length(node_coordinates))
   kdtree = KDTree(map(nc -> SVector(Tuple(nc)), node_coordinates))
+  # TODO: This does not work for SingleFieldFEFunction
   cell_f = get_array(f)
   c1 = array_cache(cell_f)
   f = testitem(cell_f)
-  @show typeof(f) typeof(x) typeof(cell_f)
   c2 = return_cache(f,x)
   return kdtree, c1, c2, cell_f, f
 end
 
-# function evaluate!(cache,f::CellField,x::Point)
-function evaluate!(cache,f::SingleFieldFEFunction,x::Point)
+function evaluate!(cache,f::CellField,x::Point)
   kdtree, c1, c2, cell_g, g = cache
   if f === g
     cell_f = cell_g
@@ -69,47 +68,115 @@ function evaluate!(cache,f::SingleFieldFEFunction,x::Point)
 
   # Find nearest vertex
   id,dist = nn(kdtree, SVector(Tuple(x)))
+  @show id
+  T = typeof(dist)
 
   # Find all neighbouring cells
   trian = get_triangulation(f)
-  # topo = get_grid_topology(trian)
-  topo = GridTopology(trian)
-  vertex_to_cells = get_faces(topo,0,num_cell_dims(model))
+  @show typeof(trian)
+  node_coordinates = get_node_coordinates(trian)
+  node_coordinates = reshape(node_coordinates, length(node_coordinates))
+  model = trian   # this is wrong
+  # topo = get_grid_topology(model)
+  topo = GridTopology(model)
+  D = num_cell_dims(trian)
+  vertex_to_cells = get_faces(topo,0,D)
   cells = vertex_to_cells[id]
+  @show cells
 
-  # Calculate the distance from the point to all the cells. Without
-  # round-off, and with non-overlapping cells, the distance would be
-  # negative for exactly one cell and positive for all other ones. Due
-  # to round-off, the distance can be slightly negative or slightly
-  # positive for points near cell boundaries, in particular near
-  # vertices. In this case, choose the cell with the smallest
-  # distance, and check that the distance (if positive) is at most at
-  # round-off level.
-  mindist = T(Inf)
-  minc,minλ = 0,zero(SVector{D+1,T})
-  for c in cells
-    # Calculate barycentric coordinates λ for x in cell c
-    ids = trian.cell_node_ids[c]
-    @assert length(ids) == D+1 "simplex has correct number of vertices"
-    ys = zero(SVector{D+1,SVector{D,T}})
-    for (n,id) in enumerate(ids)
-      y = SVector(Tuple(node_coordinates[id]))
-      ys = setindex(ys, y, n)
+  # Find polytope
+  # TODO: this only works for simplices, not for hypercubes
+  # type CartesianGrid has no field reffes
+  @show typeof(trian)
+  reffes = trian.reffes
+  @assert length(reffes) == 1
+  reffe = reffes[1].reffe
+  polytope = reffe.polytope
+  extrusion = polytope.extrusion
+  @assert length(extrusion) == D
+
+  if all(e == TET_AXIS for e in extrusion)
+
+    # Calculate the distance from the point to all the cells. Without
+    # round-off, and with non-overlapping cells, the distance would be
+    # positive for exactly one cell and positive for all other ones.
+    # Due to round-off, the distance can be slightly negative or
+    # slightly positive for points near cell boundaries, in particular
+    # near vertices. In this case, choose the cell with the largest
+    # distance, and check that the distance (if negative) is at most
+    # at round-off level.
+    maxdist = -T(Inf)
+    maxcell = 0
+    for cell in cells
+      @show "for" cell
+      # Calculate barycentric coordinates λ for x in cell c
+      ids = trian.cell_node_ids[cell]
+      @show "for" ids
+      N = length(ids)
+      # @show N D+1
+      @assert N == D+1 "simplex has correct number of vertices"
+      ys = zero(SVector{N,SVector{D,T}})
+      for (n,id) in enumerate(ids)
+        y = SVector(Tuple(node_coordinates[id]))
+        ys = setindex(ys, y, n)
+      end
+      @show "for" ys
+      λ = cartesian2barycentric(ys, SVector{D,T}(Tuple(x)))
+      @show "for" λ
+      # dist is the distance to the closest face. If positive, the point
+      # is inside this cell.
+      dist = minimum(λ)
+      @show "for" dist
+      if dist > maxdist
+        maxcell = cell
+        maxdist = dist
+      end
     end
-    λ = cartesian2barycentric(ys, SVector{D,T}(Tuple(x)))
-    dist = max(-minimum(λ), maximum(λ) - 1)
-    if dist < mindist
-      minc,minλ = c,λ
-      mindist = dist
+    @assert maxcell > 0
+    cell = maxcell
+    dist = maxdist
+  
+    # Ensure the point is inside one of the cells, up to round-off errors
+    # TODO: Add proper run-time check
+    @show cell dist
+    @assert dist ≥ -1000 * eps(T) "Point is not inside a cell"
+
+  elseif all(e == HEX_AXIS for e in extrusion)
+
+    maxdist = -T(Inf)
+    maxcell = 0
+    for cell in cells
+      ids = trian.cell_node_ids[cell]
+      N = length(ids)
+      @assert N == 2^D "hypercube has correct number of vertices"
+      dist = T(Inf)
+      for d in 1:D
+        for f in 1:2
+          face = [i+1 for i in 0:2^D-1 if (i & (1<<(D-d)) == 0) == (f==1)]
+          yc = ids[face[1]][d]
+          @assert all(ids[j][d] == y for j in face)
+          dist = min(dist, f == 1 ? x[d] - y : y - x[d])
+        end
+      end
+      if dist > maxdist
+        maxcell = cell
+        maxdist = dist
+      end
     end
+    @assert maxcell > 0
+    cell = maxcell
+    dist = maxdist
+  
+    # Ensure the point is inside one of the cells, up to round-off errors
+    # TODO: Add proper run-time check
+    @show cell dist
+    @assert dist ≥ -1000 * eps(T) "Point is not inside a cell"
+
+  else
+
+    @notimplemented "Need either tet or hex mesh"
+
   end
-  @assert 1 ≤ minc
-  cell,λ = minc,minλ
-  dist = mindist
-
-  # Ensure the point is inside one of the cells, up to round-off errors
-  # TODO: Add proper run-time check
-  @assert dist ≤ 1000 * eps(T) "Point is not inside a cell"
 
   f = getindex!(c1,cell_f,cell)
   fx = evaluate!(c2,f,x)
