f6ce10b54c5934143796c5a3eb1574cf899382ae
Francesc Verdugo <fverdugo@cimne.upc.edu>
2021-03-29 08:44:14 +0200

    Revert "Marking PeriodicBCTests as broken for julia 1.6"
    This reverts commit 1ba0248d9ce9e5398d0e4e3d8487974f1f1b4fd6.

--- test/GeometryTests/PeriodicBCTests.jl
+++ test/GeometryTests/PeriodicBCTests.jl
@@ -5,7 +5,6 @@ using Gridap.Geometry
 
 using Test
 
-@test_broken begin
 
 ## 2D mesh with periodic BC in x
 model = CartesianDiscreteModel((0,1,0,1),(3,3);isperiodic=(true,false))
@@ -139,6 +138,4 @@ model = CartesianDiscreteModel((0,1,0,1),(3,3);isperiodic=(true,true))
    [4, 7], [4, 6], [4, 5], [5, 8], [5, 6], [6, 9],
    [7, 9], [7, 8], [8, 9]]
 
-true
-end
 end #module
