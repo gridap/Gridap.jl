b1b4207d7acdcf647421c6c4fae54da5d4464eb7
Francesc Verdugo <fverdugo@cimne.upc.edu>
2021-05-05 17:24:15 +0200

    Extending CLagrangianFESpace to support Dirichlet data

--- src/FESpaces/CLagrangianFESpaces.jl
+++ src/FESpaces/CLagrangianFESpaces.jl
@@ -1,28 +1,84 @@
 
 """
-    struct CLagrangianFESpace{S} <: SingleFieldFESpace
+    struct NodeToDofGlue{T}
+      free_dof_to_node::Vector{Int32}
+      free_dof_to_comp::Vector{Int16}
+      dirichlet_dof_to_node::Vector{Int32}
+      dirichlet_dof_to_comp::Vector{Int16}
+      node_and_comp_to_dof::Vector{T}
+    end
+"""
+struct NodeToDofGlue{T}
+  free_dof_to_node::Vector{Int32}
+  free_dof_to_comp::Vector{Int16}
+  dirichlet_dof_to_node::Vector{Int32}
+  dirichlet_dof_to_comp::Vector{Int16}
+  node_and_comp_to_dof::Vector{T}
+end
+
+"""
+    struct CLagrangianFESpace{T} <: SingleFieldFESpace
       grid::Grid
-      dof_to_node::Vector{Int}
-      dof_to_comp::Vector{Int8}
-      node_and_comp_to_dof::Vector{S}
+      glue::NodeToDofGlue{T}
       # + private fields
     end
 """
 struct CLagrangianFESpace{S} <: SingleFieldFESpace
   space::UnconstrainedFESpace
   grid::Grid
-  dof_to_node::Vector{Int}
-  dof_to_comp::Vector{Int8}
-  node_and_comp_to_dof::Vector{S}
+  glue::NodeToDofGlue{S}
+end
 
-  @doc """
-      CLagrangianFESpace(::Type{T},grid::Grid) where T
-  """
-  function CLagrangianFESpace(::Type{T},grid::Grid) where T
-    space, dof_to_node, dof_to_comp, node_and_comp_to_dof = _generate_clagrangian_fespace(zero(T),grid)
-    S = eltype(node_and_comp_to_dof)
-    new{S}(space,grid,dof_to_node,dof_to_comp,node_and_comp_to_dof)
-  end
+"""
+    CLagrangianFESpace(::Type{T},grid::Grid) where T
+"""
+function CLagrangianFESpace(::Type{T},grid::Grid) where T
+  vector_type = Vector{_dof_type(T)}
+  node_to_tag = fill(Int8(UNSET),num_nodes(grid))
+  tag_to_mask = fill(_default_mask(T),0)
+  CLagrangianFESpace(T,grid,vector_type,node_to_tag,tag_to_mask)
+end
+
+"""
+    CLagrangianFESpace(
+    ::Type{T},
+    grid::Grid,
+    vector_type::Type,
+    node_to_tag::AbstractVector,
+    tag_to_mask::AbstractVector) where T
+"""
+function CLagrangianFESpace(
+  ::Type{T},
+  grid::Grid,
+  vector_type::Type,
+  node_to_tag::AbstractVector{<:Integer},
+  tag_to_masks::AbstractVector) where T
+
+  z = zero(T)
+  glue, dirichlet_dof_tag = _generate_node_to_dof_glue_component_major(
+    z,node_to_tag,tag_to_masks)
+  cell_dofs_ids, cell_reffe = _generate_cell_data_clagrangian(z,grid,glue)
+  cell_shapefuns, cell_dof_basis = compute_cell_space(cell_reffe, grid)
+  cell_is_dirichlet = collect(lazy_map(dofs->any(dof->dof<0,dofs),cell_dofs_ids))
+
+  nfree = length(glue.free_dof_to_node)
+  ndirichlet = length(glue.dirichlet_dof_to_node)
+  ntags = length(tag_to_masks)
+  dirichlet_cells = collect(Int32,findall(cell_is_dirichlet))
+
+  space = UnconstrainedFESpace(
+    vector_type,
+    nfree,
+    ndirichlet,
+    cell_dofs_ids,
+    cell_shapefuns,
+    cell_dof_basis,
+    cell_is_dirichlet,
+    dirichlet_dof_tag,
+    dirichlet_cells,
+    ntags)
+
+  CLagrangianFESpace(space,grid,glue)
 end
 
 # FESpace interface
@@ -83,9 +139,134 @@ end
 
 # Helpers
 
-function _generate_clagrangian_fespace(z,grid)
+_default_mask(::Type) = true
+_default_mask(::Type{T}) where T <: MultiValue = ntuple(i->true,Val{length(T)}())
 
-  grid_reffes = get_reffes(grid)
+_dof_type(::Type{T}) where T = T
+_dof_type(::Type{T}) where T<:MultiValue = eltype(T)
+
+function _generate_node_to_dof_glue_component_major(z,node_to_tag,tag_to_mask)
+  @notimplementedif length(z) != 1
+  nfree_dofs = 0
+  ndiri_dofs = 0
+  for tag in node_to_tag
+    if tag == UNSET
+      nfree_dofs += 1
+    else
+      mask = tag_to_mask[tag]
+      if mask
+        ndiri_dofs += 1
+      else
+        nfree_dofs += 1
+      end
+    end
+  end
+  free_dof_to_node = Vector{Int32}(undef,nfree_dofs)
+  free_dof_to_comp = ones(Int16,nfree_dofs)
+  diri_dof_to_node = Vector{Int32}(undef,ndiri_dofs)
+  diri_dof_to_comp = ones(Int16,ndiri_dofs)
+  diri_dof_to_tag = ones(Int8,ndiri_dofs)
+  nnodes = length(node_to_tag)
+  node_and_comp_to_dof = Vector{Int32}(undef,nnodes)
+  nfree_dofs = 0
+  ndiri_dofs = 0
+  for (node,tag) in enumerate(node_to_tag)
+    if tag == UNSET
+      nfree_dofs += 1
+      free_dof_to_node[nfree_dofs] = node
+      node_and_comp_to_dof[node] = nfree_dofs
+    else
+      mask = tag_to_mask[tag]
+      if mask
+        ndiri_dofs += 1
+        diri_dof_to_node[ndiri_dofs] = node
+        diri_dof_to_tag[ndiri_dofs] = tag
+        node_and_comp_to_dof[node] = -ndiri_dofs
+      else
+        nfree_dofs += 1
+        free_dof_to_node[nfree_dofs] = node
+        node_and_comp_to_dof[node] = nfree_dofs
+      end
+    end
+  end
+  glue = NodeToDofGlue(
+   free_dof_to_node,
+   free_dof_to_comp,
+   diri_dof_to_node,
+   diri_dof_to_comp,
+   node_and_comp_to_dof)
+
+  glue, diri_dof_to_tag
+end
+
+function _generate_node_to_dof_glue_component_major(
+  z::MultiValue,node_to_tag,tag_to_masks)
+  nfree_dofs = 0
+  ndiri_dofs = 0
+  ncomps = length(z)
+  @assert length(testitem(tag_to_masks)) == ncomps
+  for (node,tag) in enumerate(node_to_tag)
+    if tag == UNSET
+      nfree_dofs += ncomps
+    else
+      for mask in tag_to_masks[tag]
+        if mask
+          ndiri_dofs += 1
+        else
+          nfree_dofs += 1
+        end
+      end
+    end
+  end
+  free_dof_to_node = Vector{Int32}(undef,nfree_dofs)
+  free_dof_to_comp = ones(Int16,nfree_dofs)
+  diri_dof_to_node = Vector{Int32}(undef,ndiri_dofs)
+  diri_dof_to_comp = ones(Int16,ndiri_dofs)
+  diri_dof_to_tag = ones(Int8,ndiri_dofs)
+  T = change_eltype(z,Int32)
+  nnodes = length(node_to_tag)
+  node_and_comp_to_dof = zeros(T,nnodes)
+  nfree_dofs = 0
+  ndiri_dofs = 0
+  m = zero(Mutable(T))
+  for (node,tag) in enumerate(node_to_tag)
+    if tag == UNSET
+      for comp in 1:ncomps
+        nfree_dofs += 1
+        free_dof_to_node[nfree_dofs] = node
+        free_dof_to_comp[nfree_dofs] = comp
+        m[comp] = nfree_dofs
+      end
+    else
+      for (comp,mask) in tag_to_masks[tag]
+        if mask
+          ndiri_dofs += 1
+          diri_dof_to_node[ndiri_dofs] = node
+          diri_dof_to_comp[ndiri_dofs] = comp
+          diri_dof_to_tag[ndiri_dofs] = tag
+          m[comp] = -ndiri_dofs
+        else
+          nfree_dofs += 1
+          free_dof_to_node[nfree_dofs] = node
+          free_dof_to_comp[nfree_dofs] = comp
+          m[comp] = nfree_dofs
+        end
+      end
+    end
+    node_and_comp_to_dof[node] = m
+  end
+  glue = NodeToDofGlue(
+   free_dof_to_node,
+   free_dof_to_comp,
+   diri_dof_to_node,
+   diri_dof_to_comp,
+   node_and_comp_to_dof)
+
+  glue, diri_dof_to_tag
+end
+
+function _generate_cell_data_clagrangian(z,grid,glue)
+  ctype_to_reffe = get_reffes(grid)
   cell_to_ctype = get_cell_type(grid)
   function fun(reffe)
     p = get_polytope(reffe)
@@ -93,119 +274,61 @@ function _generate_clagrangian_fespace(z,grid)
     LagrangianRefFE(typeof(z),p,orders)
   end
   # using map instead of lazy_map seems to kill the compiler.
-  ctype_to_reffe = collect(lazy_map(fun,grid_reffes))
+  ctype_to_reffe = collect(lazy_map(fun,ctype_to_reffe))
   cell_to_reffe = expand_cell_data(ctype_to_reffe,cell_to_ctype)
+  cell_to_nodes = get_cell_node_ids(grid)
 
-  nnodes = num_nodes(grid)
-  dof_to_node, dof_to_comp, node_and_comp_to_dof = _generate_dof_layout_component_major(z,nnodes)
-
-  cell_node_ids = get_cell_node_ids(grid)
-
-  cell_dofs_ids = _generate_cell_dofs_clagrangian_fespace(
+  cell_to_dofs = _generate_cell_dofs_clagrangian(
     z,
-    cell_node_ids,
+    cell_to_nodes,
     ctype_to_reffe,
     cell_to_ctype,
-    node_and_comp_to_dof)
+    glue.node_and_comp_to_dof)
 
-  cell_map = get_cell_map(grid)
-  cell_shapefuns, cell_dof_basis = compute_cell_space(cell_to_reffe, grid)
-
-  nfree = length(dof_to_node)
-  ndirichlet = 0
-
-  dirichlet_dof_tag = Int8[]
-  dirichlet_cells = Int32[]
-  cell_is_dirichlet = Fill(false,num_cells(grid))
-  ntags = 0
-  vector_type = Vector{_dof_type(z)}
-
-  space = UnconstrainedFESpace(
-    vector_type,
-    nfree,
-    ndirichlet,
-    cell_dofs_ids,
-    cell_shapefuns,
-    cell_dof_basis,
-    cell_is_dirichlet,
-    dirichlet_dof_tag,
-    dirichlet_cells,
-    ntags)
-
-  space, dof_to_node, dof_to_comp, node_and_comp_to_dof
-
+  cell_to_dofs, cell_to_reffe
 end
 
-_dof_type(r::Number) = typeof(r)
-_dof_type(r::MultiValue) = eltype(r)
-
-function _generate_dof_layout_component_major(z,nnodes::Integer)
-  ndofs = nnodes
-  dof_to_comp = ones(Int8,ndofs)
-  dof_to_node = collect(1:nnodes)
-  node_and_comp_to_dof = dof_to_node
-  (dof_to_node, dof_to_comp, node_and_comp_to_dof)
-end
-
-function _generate_dof_layout_component_major(z::MultiValue,nnodes::Integer)
-  ncomps = num_components(z)
-  V = change_eltype(z,Int)
-  ndofs = ncomps*nnodes
-  dof_to_comp = zeros(Int8,ndofs)
-  dof_to_node = zeros(Int,ndofs)
-  node_and_comp_to_dof = zeros(V,nnodes)
-  m = zero(Mutable(V))
-  for node in 1:nnodes
-    o = ncomps*(node-1)
-    for comp in 1:ncomps
-      dof = comp+o
-      dof_to_comp[dof] = comp
-      dof_to_node[dof] = node
-      m[comp] = dof
-    end
-    node_and_comp_to_dof[node] = m
-  end
-  (dof_to_node, dof_to_comp, node_and_comp_to_dof)
-end
-
-function _generate_cell_dofs_clagrangian_fespace(
+function _generate_cell_dofs_clagrangian(
   z,
-  cell_node_ids,
-  reffes,
+  cell_to_nodes,
+  ctype_to_reffe,
   cell_to_ctype,
   node_and_comp_to_dof)
 
-  cell_node_ids
+  cell_to_dofs = lazy_map(
+    Broadcasting(Reindex(node_and_comp_to_dof)),cell_to_nodes)
+  Table(cell_to_dofs)
 end
 
-function _generate_cell_dofs_clagrangian_fespace(
+function _generate_cell_dofs_clagrangian(
   z::MultiValue,
-  cell_node_ids,
-  reffes,
+  cell_to_nodes,
+  ctype_to_reffe,
   cell_to_ctype,
   node_and_comp_to_dof)
 
   ncomps = num_components(z)
 
-  ctype_to_lnode_to_comp_to_ldof = map(get_node_and_comp_to_dof,reffes)
-  ctype_to_num_ldofs = map(num_dofs,reffes)
+  ctype_to_lnode_to_comp_to_ldof = map(get_node_and_comp_to_dof,ctype_to_reffe)
+  ctype_to_num_ldofs = map(num_dofs,ctype_to_reffe)
 
-  cell_dofs = _allocate_cell_dofs_clagrangian_fespace(ctype_to_num_ldofs,cell_to_ctype)
+  cell_to_dofs = _allocate_cell_dofs_clagrangian(
+    ctype_to_num_ldofs,cell_to_ctype)
 
-  cache = array_cache(cell_node_ids)
-  _fill_cell_dofs_clagrangian_fespace!(
-    cell_dofs,
+  cache = array_cache(cell_to_nodes)
+  _fill_cell_dofs_clagrangian!(
+    cell_to_dofs,
     cache,
-    cell_node_ids,
+    cell_to_nodes,
     ctype_to_lnode_to_comp_to_ldof,
     cell_to_ctype,
     node_and_comp_to_dof,
     ncomps)
 
-  cell_dofs
+  cell_to_dofs
 end
 
-function _allocate_cell_dofs_clagrangian_fespace(ctype_to_num_ldofs,cell_to_ctype)
+function _allocate_cell_dofs_clagrangian(ctype_to_num_ldofs,cell_to_ctype)
   ncells = length(cell_to_ctype)
   ptrs = zeros(Int32,ncells+1)
   for cell in 1:ncells
@@ -219,29 +342,28 @@ function _allocate_cell_dofs_clagrangian_fespace(ctype_to_num_ldofs,cell_to_ctyp
   Table(data,ptrs)
 end
 
-function _fill_cell_dofs_clagrangian_fespace!(
-  cell_dofs,
+function _fill_cell_dofs_clagrangian!(
+  cell_to_dofs,
   cache,
-  cell_node_ids,
+  cell_to_nodes,
   ctype_to_lnode_and_comp_to_ldof,
   cell_to_ctype,
   node_and_comp_to_dof,
   ncomps)
 
   ncells = length(cell_to_ctype)
-
   for cell in 1:ncells
-    nodes = getindex!(cache,cell_node_ids,cell)
+    nodes = getindex!(cache,cell_to_nodes,cell)
     ctype = cell_to_ctype[cell]
     lnode_and_comp_to_ldof = ctype_to_lnode_and_comp_to_ldof[ctype]
-    p = cell_dofs.ptrs[cell]-1
+    p = cell_to_dofs.ptrs[cell]-1
     for (lnode, node) in enumerate(nodes)
       for comp in 1:ncomps
         ldof = lnode_and_comp_to_ldof[lnode][comp]
         dof = node_and_comp_to_dof[node][comp]
-        cell_dofs.data[p+ldof] = dof
+        cell_to_dofs.data[p+ldof] = dof
       end
     end
   end
-
 end
+
--- test/FESpacesTests/CLagrangianFESpacesTests.jl
+++ test/FESpacesTests/CLagrangianFESpacesTests.jl
@@ -19,10 +19,10 @@ matdata = ([],[],[])
 vecdata = ([],[])
 test_single_field_fe_space(V,matvecdata,matdata,vecdata)
 
-@test get_cell_dof_ids(V) === get_cell_node_ids(grid)
-@test V.dof_to_node == collect(1:num_nodes(grid))
-@test V.dof_to_comp == ones(Int,num_nodes(grid))
-@test V.node_and_comp_to_dof == V.dof_to_node
+@test get_cell_dof_ids(V) == collect1d(get_cell_node_ids(grid))
+@test V.glue.free_dof_to_node == collect(1:num_nodes(grid))
+@test V.glue.free_dof_to_comp == ones(Int,num_nodes(grid))
+@test V.glue.node_and_comp_to_dof == V.glue.free_dof_to_node
 
 u(x) = x[1]+x[2]
 uh = interpolate(u,V)
@@ -34,9 +34,9 @@ V = CLagrangianFESpace(VectorValue{2,Float64},grid)
 test_single_field_fe_space(V,matvecdata,matdata,vecdata)
 
 @test get_cell_dof_ids(V) == [[1,3,7,9,2,4,8,10], [3,5,9,11,4,6,10,12], [7,9,13,15,8,10,14,16], [9,11,15,17,10,12,16,18]]
-@test V.dof_to_node == [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9]
-@test V.dof_to_comp == [1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2]
-@test V.node_and_comp_to_dof == VectorValue{2,Int}[(1,2),(3,4),(5,6),(7,8),(9,10),(11,12),(13,14),(15,16),(17,18)]
+@test V.glue.free_dof_to_node == [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9]
+@test V.glue.free_dof_to_comp == [1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2]
+@test V.glue.node_and_comp_to_dof == VectorValue{2,Int}[(1,2),(3,4),(5,6),(7,8),(9,10),(11,12),(13,14),(15,16),(17,18)]
 
 u(x) = VectorValue(x[1]+x[2],x[2])
 uh = interpolate(u,V)
