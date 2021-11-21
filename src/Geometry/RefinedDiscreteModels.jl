7b10630905064d8efeaf697dff6e5e251b4a350b
RAPPAPORT Ari <ari.rappaport@inria.fr>
2021-11-21 19:13:03 +0100

    Able to append to the cell_coords array

--- src/Geometry/RefinedDiscreteModels.jl
+++ src/Geometry/RefinedDiscreteModels.jl
@@ -1,13 +1,20 @@
 # setp 1
 function newest_vertex_bisection(grid::Grid,cell_mask::AbstractVector{<:Bool})
-  # todo
+    #get_faces(top
+    cell_coords = get_cell_coordinates(grid)
+    @show typeof(cell_coords)
+    #@show new_val = VectorValue{2, Float64}(1.5, 1.5, 1.5)
+    @show new_val = [VectorValue(1.5, 1.5, 1.5)]
+    cell_coords = lazy_append(cell_coords, new_val)
+    @show cell_coords 
+  # tod
   #ref_grid
 end
 
 # step 2
 function newest_vertex_bisection(model::DiscreteModel,cell_mask::AbstractVector{<:Bool})
-  #grid  = get_grid(model)
-  #ref_grid = newest_vertex_bisection(grid,cell_mask)
+  grid  = get_grid(model)
+  ref_grid = newest_vertex_bisection(grid,cell_mask)
   #ref_topo = GridTopology(grid)
   #labels = get_face_labelling(model)
   #ref_labels = # Compute them from the original labels (This is perhaps the most tedious part)
--- test/GeometryTests/RefinedDiscreteModelsTests.jl
+++ test/GeometryTests/RefinedDiscreteModelsTests.jl
@@ -11,9 +11,10 @@ using Gridap.Io
 domain = (0,1,0,1)
 partition = (4,4)
 model = CartesianDiscreteModel(domain,partition)
-@show cell_map = get_cell_map(get_triangulation(model))
+model = simplexify(model)
+cell_map = get_cell_map(get_triangulation(model))
 @show num_cells = length(cell_map)
-@show cell_mask = fill(true, num_cells)
+cell_mask = fill(true, num_cells)
 model_ref = newest_vertex_bisection(model, cell_mask)
 @test model_ref isa DiscreteModel
 
