4ec3825b7863a6dd5e1a71a9175495a99de3f213
RAPPAPORT Ari <ari.rappaport@inria.fr>
2021-11-27 14:41:17 +0100

    Returning a new UnstructuredGrid

--- src/Geometry/RefinedDiscreteModels.jl
+++ src/Geometry/RefinedDiscreteModels.jl
@@ -1,17 +1,23 @@
 function get_midpoint(x::AbstractVector, y::AbstractVector)
-    (x + y) / 2.0
+    (x + y) ./ 2.0
 end
 
 
 # setp 1
 function newest_vertex_bisection(grid::Grid,cell_mask::AbstractVector{<:Bool})
     #get_faces(top
-    cell_coords = get_cell_coordinates(grid)
-    @show typeof(cell_coords)
+    #@show cell_coords = get_cell_coordinates(grid)
+    @show node_coords = get_node_coordinates(grid)
+    @show cell_node_ids = get_cell_node_ids(grid)
+    @show reffes = get_reffes(grid)
+    @show cell_types = get_cell_type(grid)
+    UnstructuredGrid(node_coords, cell_node_ids, reffes, cell_types)
+    # TODO: Modify node__coords and cell_node_ids
     #@show new_val = VectorValue{2, Float64}(1.5, 1.5, 1.5)
     #new_val = LazyArray([VectorValue(1.5, 1.5), VectorValue(1.2, 2), VectorValue(3.2, 3.1)])
-    @show new_val = lazy_map(get_midpoint, cell_coords[1:2], 0*cell_coords[1:2])
-    cell_coords = lazy_append(cell_coords, new_val)
+    #@show new_val = lazy_map(get_midpoint, cell_coords[1:2], 0*cell_coords[1:2])
+    #@show new_val = lazy_map(get_midpoint, cell_coords[1:2], 0*cell_coords[1:2])
+    #cell_coords = lazy_append(cell_coords, new_val)
   # tod
   #ref_grid
 end
