c65f96dbaf1c52ff8e7713f2d18363caf192d26d
RAPPAPORT Ari <ari.rappaport@inria.fr>
2021-11-27 17:03:13 +0100

    Add build_dual_edge

--- src/Geometry/RefinedDiscreteModels.jl
+++ src/Geometry/RefinedDiscreteModels.jl
@@ -1,3 +1,5 @@
+using Gridap.Arrays
+
 function get_midpoint(x::AbstractVector, y::AbstractVector)
     (x + y) ./ 2.0
 end
@@ -7,7 +9,12 @@ function build_edges(elem::Matrix{T}) where {T <: Integer}
     unique(sort!(edge, dims=2), dims=1)
 end
 
-function test_construction(edge::Matrix{S}, top::GridTopology, d::T) where {S, T <: Integer}
+function build_dual_edge(elem::Matrix{Ti}, N::T) where {Ti, T <: Integer}
+    NT = size(elem, 1)
+    [elem[:,[1,2,3]],elem[:,[2,3,1]], [1:NT,1:NT,1:NT],N,N]
+end
+
+function test_against_top(edge::Matrix{Ti}, top::GridTopology, d::T) where {Ti, T <: Integer}
     edge_vec = [edge[i,:] for i in 1:size(edge,1)]
     edge_top = get_faces(top, d, 0)
     @assert issetequal(edge_vec, edge_top)
@@ -16,11 +23,13 @@ end
 """
 node_coords == node, cell_node_ids == elem in Long Chen's notation
 """
-function newest_vertex_bisection(top::GridTopology, node_coords::Vector, cell_node_ids::Arrays.Table)
-    @show elem = vcat(cell_node_ids'...)
-    test_construction(elem, top, 2)
+function newest_vertex_bisection(top::GridTopology, node_coords::Vector, cell_node_ids::Table)
+    N = size(node_coords, 1)
+    elem = vcat(cell_node_ids'...)
+    test_against_top(elem, top, 2)
     edge = build_edges(elem)
-    test_construction(edge, top, 1)
+    @show dual_edge = build_dual_edge(elem, N)
+    test_against_top(edge, top, 1)
     node_coords, cell_node_ids
 end
 
