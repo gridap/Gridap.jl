40b5d1378102a5f0e99d69fd2bfeb50dd5a3ff8e
RAPPAPORT Ari <ari.rappaport@inria.fr>
2021-11-27 16:23:51 +0100

    Create edge data structure

--- src/Geometry/RefinedDiscreteModels.jl
+++ src/Geometry/RefinedDiscreteModels.jl
@@ -2,17 +2,31 @@ function get_midpoint(x::AbstractVector, y::AbstractVector)
     (x + y) ./ 2.0
 end
 
+"""
+node_coords == node, cell_node_ids == elem in Long Chen's notation
+"""
+function newest_vertex_bisection(node_coords::Vector, cell_node_ids::Arrays.Table)
+    @show elem = vcat(cell_node_ids'...)
+    edge = [elem[:,[1,2]]; elem[:,[1,3]]; elem[:,[2,3]]]
+    edge = unique(sort!(edge, dims=2), dims=1)
+    #edge = sort!(edge, dims=2)
+    @show length(edge)
+    #edge = unique(sort(edge,2), "rows")
+    node_coords, cell_node_ids
+end
 
 # setp 1
 function newest_vertex_bisection(grid::Grid,cell_mask::AbstractVector{<:Bool})
     #get_faces(top
     #@show cell_coords = get_cell_coordinates(grid)
-    @show node_coords = get_node_coordinates(grid)
-    @show cell_node_ids = get_cell_node_ids(grid)
-    @show reffes = get_reffes(grid)
-    @show cell_types = get_cell_type(grid)
-    UnstructuredGrid(node_coords, cell_node_ids, reffes, cell_types)
+    node_coords = get_node_coordinates(grid)
+    cell_node_ids = get_cell_node_ids(grid)
     # TODO: Modify node__coords and cell_node_ids
+    typeof(node_coords)
+    node_coords, cell_node_ids = newest_vertex_bisection(node_coords, cell_node_ids)
+    reffes = get_reffes(grid)
+    cell_types = get_cell_type(grid)
+    UnstructuredGrid(node_coords, cell_node_ids, reffes, cell_types)
     #@show new_val = VectorValue{2, Float64}(1.5, 1.5, 1.5)
     #new_val = LazyArray([VectorValue(1.5, 1.5), VectorValue(1.2, 2), VectorValue(3.2, 3.1)])
     #@show new_val = lazy_map(get_midpoint, cell_coords[1:2], 0*cell_coords[1:2])
