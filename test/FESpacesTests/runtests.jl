97a9dab7fa8bbf65b6ca362cf33515b575242634
amartin <alberto.martin@monash.edu>
2022-12-11 10:43:05 +1100

    LinearizedFESpacesTests

--- test/FESpacesTests/runtests.jl
+++ test/FESpacesTests/runtests.jl
@@ -1,6 +1,6 @@
 module FESpacesTests
-
+using Test
 include("runtests_1.jl")
 include("runtests_2.jl")
-
+@testset "LinearizedFESpacesTests" begin include("LinearizedFESpacesTests.jl") end
 end # module
