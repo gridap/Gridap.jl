92b9c5547e9cf98e043065aa011fecd55e5a6b50
JordiManyer <jordi.manyer@monash.edu>
2023-06-16 11:17:27 +1000

    Added matrix-vector assembly for Block assemblers

--- src/MultiField/BlockSparseMatrixAssemblers.jl
+++ src/MultiField/BlockSparseMatrixAssemblers.jl
@@ -81,10 +81,34 @@ function select_block_vecdata(vecdata,j::Integer)
    map(data->_select_block_data(data,j),vecdata[2]))
 end
 
+function select_block_matvecdata(matvecdata,i::Integer,j::Integer)
+  (map(data->lazy_map(_select_block_matvecdata{i,j}(),data),matvecdata[1]),
+   map(data->_select_block_data(data,i),matvecdata[2]),
+   map(data->_select_block_data(data,j),matvecdata[3]))
+end
+
 function _select_block_data(data,idx::Integer...)
   n = length(data)
   _idx = map(i->Fill(i,n),idx)
   return lazy_map(getindex,data,_idx...)
+end
+
+struct _select_block_matvecdata{I,J} <: Map end
+
+function Arrays.evaluate!(cache,::_select_block_matvecdata{I,J},data) where {I,J}
+  matdata, vecdata = data
+  return matdata[I,J]
+end
+
+function Arrays.evaluate!(cache,::_select_block_matvecdata{I,I},data) where {I}
+  matdata, vecdata = data
+  return matdata[I,I], vecdata[I]
+end
+
+function combine_matdata(data1,data2)
+  w1,r1,c1 = data1
+  w2,r2,c2 = data2
+  return (vcat(w1,w2),vcat(r1,r2),vcat(c1,c2))
 end
 
 """
@@ -108,9 +132,9 @@ end
 function FESpaces.allocate_vector(a::BlockSparseMatrixAssembler,vecdata)
   v = allocate_block_vector(a)
   block_assemblers = a.block_assemblers
-  for j in 1:blocksize(v,1)
-    _a = block_assemblers[j,1]
-    _vecdata = select_block_vecdata(vecdata,j)
+  for i in 1:blocksize(v,1)
+    _a = block_assemblers[i,1]
+    _vecdata = select_block_vecdata(vecdata,i)
     v[Block(i)] = allocate_vector(_a,_vecdata)
   end
   return v
@@ -118,7 +142,7 @@ end
 
 function FESpaces.assemble_vector_add!(b::BlockVector,a::BlockSparseMatrixAssembler,vecdata)
   block_assemblers = a.block_assemblers
-  for j in 1:blocksize(v,1)
+  for j in 1:blocksize(b,1)
     _a = block_assemblers[j,1]
     _vecdata = select_block_vecdata(vecdata,j)
     bj = b[Block(j)]
@@ -163,8 +187,8 @@ end
 
 function FESpaces.assemble_matrix_add!(mat::BlockMatrix,a::BlockSparseMatrixAssembler,matdata)
   block_assemblers = a.block_assemblers
-  for i in 1:blocksize(m,1)
-    for j in 1:blocksize(m,2)
+  for i in 1:blocksize(mat,1)
+    for j in 1:blocksize(mat,2)
       _a = block_assemblers[i,j]
       _matdata = select_block_matdata(matdata,i,j)
       mat_ij = mat[Block(i,j)]
@@ -175,21 +199,75 @@ end
 
 # Matrix and vector Assembly
 
-"""
 function FESpaces.allocate_matrix_and_vector(a::BlockSparseMatrixAssembler,data)
-  
+  matvecdata, matdata, vecdata = data
+  m = allocate_block_matrix(a)
+  v = allocate_block_vector(a)
+  block_assemblers = a.block_assemblers
+  for i in 1:blocksize(m,1)
+    for j in 1:blocksize(m,2)
+      _a = block_assemblers[i,j]
+      _matvecdata = select_block_matvecdata(matvecdata,i,j)
+      _matdata = select_block_matdata(matdata,i,j)
+      if i == j # Diagonal blocks
+        _vecdata = select_block_vecdata(vecdata,j)
+        m[Block(i,j)], v[Block(i)] = allocate_matrix_and_vector(_a,(_matvecdata,_matdata,_vecdata))
+      else      # Off-diagonal blocks
+        __matdata = combine_matdata(_matvecdata,_matdata) 
+        m[Block(i,j)] = allocate_matrix(_a,__matdata)
+      end
+    end
+  end
+  return m, v
 end
 
-function FESpaces.assemble_matrix_and_vector!(A,b,a::BlockSparseMatrixAssembler, data)
-
+function FESpaces.assemble_matrix_and_vector!(A::BlockMatrix,b::BlockVector,a::BlockSparseMatrixAssembler,data)
+  for (i,j) in blockaxes(A)
+    LinearAlgebra.fillstored!(A[i,j],zero(eltype(A)))
+  end
+  fill!(b,zero(eltype(b)))
+  assemble_matrix_and_vector_add!(A,b,a,data)
 end
 
-function FESpaces.assemble_matrix_and_vector_add!(A,b,a::BlockSparseMatrixAssembler,data)
-
+function FESpaces.assemble_matrix_and_vector_add!(A::BlockMatrix,b::BlockVector,a::BlockSparseMatrixAssembler,data)
+  matvecdata, matdata, vecdata = data
+  block_assemblers = a.block_assemblers
+  for i in 1:blocksize(A,1)
+    for j in 1:blocksize(A,2)
+      _a = block_assemblers[i,j]
+      _matvecdata = select_block_matvecdata(matvecdata,i,j)
+      _matdata = select_block_matdata(matdata,i,j)
+      if i == j # Diagonal blocks
+        _vecdata = select_block_vecdata(vecdata,j)
+        _A, _b = A[Block(i,j)], b[Block(i)]
+        assemble_matrix_and_vector_add!(_A,_b,_a,(_matvecdata,_matdata,_vecdata))
+      else      # Off-diagonal blocks
+        __matdata = combine_matdata(_matvecdata,_matdata)
+        _A = A[Block(i,j)]
+        assemble_matrix_add!(_A,_a,__matdata)
+      end
+    end
+  end
 end
 
 function FESpaces.assemble_matrix_and_vector(a::BlockSparseMatrixAssembler,data)
-
+  matvecdata, matdata, vecdata = data
+  m = allocate_block_matrix(a)
+  v = allocate_block_vector(a)
+  block_assemblers = a.block_assemblers
+  for i in 1:blocksize(m,1)
+    for j in 1:blocksize(m,2)
+      _a = block_assemblers[i,j]
+      _matvecdata = select_block_matvecdata(matvecdata,i,j)
+      _matdata = select_block_matdata(matdata,i,j)
+      if i == j # Diagonal blocks
+        _vecdata = select_block_vecdata(vecdata,j)
+        m[Block(i,j)], v[Block(i)] = assemble_matrix_and_vector(_a,(_matvecdata,_matdata,_vecdata))
+      else      # Off-diagonal blocks
+        __matdata = combine_matdata(_matvecdata,_matdata) 
+        m[Block(i,j)] = assemble_matrix(_a,__matdata)
+      end
+    end
+  end
+  return m, v
 end
-"""
-
