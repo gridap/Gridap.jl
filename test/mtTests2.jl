59cfb8c0f54e3ab37f3279ec3e56675dd7810dc6
JordiManyer <jordi.manyer@monash.edu>
2023-07-03 10:29:32 +1000

    Working concept for edge triangulations tied to boundary triangulations

--- test/mtTests2.jl
+++ test/mtTests2.jl
@@ -1,46 +1,36 @@
 using Gridap
 using Gridap.Geometry
-using FillArrays
 
-function EdgeTriangulation(model::DiscreteModel,tags)
-  D = Geometry.num_cell_dims(model)
-  labeling = get_face_labeling(model)
-  face_to_mask = get_face_mask(labeling,tags,D-2)
-  face_to_bgface = findall(face_to_mask)
-  return EdgeTriangulation(model,face_to_bgface)
-end
-
-function EdgeTriangulation(
-  model::DiscreteModel,
-  face_to_bgface::AbstractVector{<:Integer})
-
-  D = Geometry.num_cell_dims(model)
-  topo = get_grid_topology(model)
-  bgface_grid = Grid(Geometry.ReferenceFE{D-2},model)
-  bgface_to_lcell = Fill(1,Geometry.num_faces(model,D-2))
-
-  face_grid = view(bgface_grid,face_to_bgface)
-  cell_grid = get_grid(model)
-  glue = Geometry.FaceToCellGlue(topo,cell_grid,face_grid,face_to_bgface,bgface_to_lcell)
-  trian = BodyFittedTriangulation(model,face_grid,face_to_bgface)
-
-  BoundaryTriangulation(trian,glue)
-end
-
-model = CartesianDiscreteModel((0,1,0,1,0,1),(2,2,3))
+D = 3
+domain = repeat([0.0,1.0],D)
+model = CartesianDiscreteModel(domain,(2,2,3))
 
 face_tags = ["tag_23","tag_26","tag_24","tag_25"]
 Γ  = BoundaryTriangulation(model,tags=face_tags)
-nF = num_cells(Γ)
+_Λ  = SkeletonTriangulation(Γ)
+glue = get_glue(_Λ,Val(D-1)) # glue from the edges to the attached faces
 
 edge_tags = ["tag_17","tag_18","tag_20","tag_19"]
-Λ  = EdgeTriangulation(model,edge_tags)
-nE = num_cells(Λ)
+labeling = get_face_labeling(model)
+edge_to_mask = get_face_mask(labeling,edge_tags,D-2)
+edge_to_bgedge = findall(edge_to_mask) # ids of the edges we want
+
+topo = get_grid_topology(model)
+edge_to_face_map = Geometry.get_faces(topo,D-2,D-1) # edge to face connectivity
+
+# Find local numbering of the edges we want
+face_pairs = map(p -> [p...],zip(glue.plus.tface_to_mface,glue.minus.tface_to_mface))
+local_edge_ids = map(edge_to_bgedge) do edge
+  edge_faces = edge_to_face_map[edge]
+  return findfirst(faces -> faces == edge_faces, face_pairs)
+end
+Λ = view(_Λ,local_edge_ids)
 
 reffe = ReferenceFE(lagrangian,Float64,1)
 V = FESpace(Γ,reffe)
 
-v = zero(V)
-pts = get_cell_points(Λ)
-v(pts)
+dΛ = Measure(Λ,2)
+v = interpolate(x -> sum(x),V)
 
+l(v) = ∫(mean(v))*dΛ
+b = assemble_vector(l,V)
