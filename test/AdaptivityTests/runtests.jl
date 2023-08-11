d344d0faed9d61db6d34a2e6bac6591248822e64
JordiManyer <jordi.manyer@monash.edu>
2023-08-11 15:47:55 +1000

    Minor modifications

--- test/AdaptivityTests/runtests.jl
+++ test/AdaptivityTests/runtests.jl
@@ -2,26 +2,20 @@ module AdaptivityTests
 
 using Test
 
-@testset "AdaptedGeometry" begin
+@testset "Refinement" begin
   include("RefinementRulesTests.jl")
   include("AdaptedGeometryTests.jl")
   include("FaceLabelingTests.jl")
-end
-
-@testset "Refinement" begin
   include("CartesianRefinementTests.jl")
   include("ComplexChangeDomainTests.jl")
   include("EdgeBasedRefinementTests.jl")
   include("FineToCoarseFieldsTests.jl")
   include("RefinementRuleBoundaryTests.jl")
+  include("MultifieldRefinementTests.jl")
 end
 
 @testset "CompositeQuadratures" begin
   include("CompositeQuadratureTests.jl")
-end
-
-@testset "MultiFields" begin
-  include("MultifieldRefinementTests.jl")
 end
 
 end # module
