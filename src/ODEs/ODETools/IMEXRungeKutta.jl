8a1a03183d0b43948e41b92d8f88de8d5f167649
Oriol Colomes <oriol.colomes@gmail.com>
2023-11-15 22:27:11 +0100

    IMEX-RK to linear case

--- src/ODEs/ODETools/IMEXRungeKutta.jl
+++ src/ODEs/ODETools/IMEXRungeKutta.jl
@@ -49,20 +49,20 @@ function solve_step!(uf::AbstractVector,
   if cache === nothing
     ode_cache = allocate_cache(op)
     vi = similar(u0)
-    ui = Vector{typeof(u0)}(undef,0)
-    im_rhs = similar(u0)
-    ex_rhs = similar(u0)
+    fi = Vector{typeof(u0)}(undef,0)
+    gi = Vector{typeof(u0)}(undef,0)
     for i in 1:s
-      push!(ui,similar(u0))
+      push!(fi,similar(u0))
+      push!(gi,similar(u0))
     end
     nls_stage_cache = nothing
     nls_update_cache = nothing
   else
-    ode_cache, vi, ui, im_rhs, ex_rhs, nls_stage_cache, nls_update_cache = cache
+    ode_cache, vi, fi, gi, nls_stage_cache, nls_update_cache = cache
   end
 
   # Create RKNL stage operator
-  nlop_stage = IMEXRungeKuttaStageNonlinearOperator(op,t0,dt,u0,ode_cache,vi,ui,im_rhs,ex_rhs,0,aᵢ,aₑ)
+  nlop_stage = IMEXRungeKuttaStageNonlinearOperator(op,t0,dt,u0,ode_cache,vi,fi,gi,0,aᵢ,aₑ)
 
   # Compute intermediate stages
   for i in 1:s
@@ -70,7 +70,7 @@ function solve_step!(uf::AbstractVector,
     # Update time
     ti = t0 + c[i]*dt
     ode_cache = update_cache!(ode_cache,op,ti)
-    update!(nlop_stage,ti,ui,i)
+    update!(nlop_stage,ti,fi,gi,i)
 
     if(aᵢ[i,i]==0)
       # Skip stage solve if a_ii=0 => u_i=u_0, f_i = f_0, gi = g_0
@@ -80,8 +80,9 @@ function solve_step!(uf::AbstractVector,
       nls_stage_cache = solve!(uf,solver.nls_stage,nlop_stage,nls_stage_cache)
     end
 
-    # Update stage unknown
-    @. nlop_stage.ui[i] = uf
+    # Update RHS at stage i using solution at u_i
+    rhs!(nlop_stage, uf)
+    explicit_rhs!(nlop_stage, uf)
 
   end
 
@@ -93,7 +94,7 @@ function solve_step!(uf::AbstractVector,
 
     # Create RKNL final update operator
     ode_cache = update_cache!(ode_cache,op,tf)
-    nlop_update = IMEXRungeKuttaUpdateNonlinearOperator(op,tf,dt,u0,ode_cache,vi,ui,im_rhs,ex_rhs,s,bᵢ,bₑ)
+    nlop_update = IMEXRungeKuttaUpdateNonlinearOperator(op,tf,dt,u0,ode_cache,vi,fi,gi,s,bᵢ,bₑ)
 
     # solve at final update
     nls_update_cache = solve!(uf,solver.nls_update,nlop_update,nls_update_cache)
@@ -101,7 +102,7 @@ function solve_step!(uf::AbstractVector,
   end
 
   # Update final cache
-  cache = (ode_cache, vi, ui, im_rhs, ex_rhs, nls_stage_cache, nls_update_cache)
+  cache = (ode_cache, vi, fi, gi, nls_stage_cache, nls_update_cache)
 
   return (uf, tf, cache)
 
@@ -113,7 +114,7 @@ IMEXRungeKuttaStageNonlinearOperator <: NonlinearOperator
 Nonlinear operator for the implicit-explicit Runge-Kutta stage.
   At a given stage `i` it represents the nonlinear operator A(t,u_i,(u_i-u_n)/dt) such that
 ```math
-A(t,u_i,(u_i-u_n)/dt) = M(u_i,t)(u_i-u_n)/Δt - f(∑aᵢ[i,j] * u_j,t_i) - g(∑aₑ[i,j] * u_j,t_i) = 0
+A(t,u_i,(u_i-u_n)/dt) = M(u_i,t)(u_i-u_n)/Δt - ∑aᵢ[i,j] * f(u_j,t_j) - ∑aₑ[i,j] * g(u_j,t_j) = 0
 ```
 """
 mutable struct IMEXRungeKuttaStageNonlinearOperator <: RungeKuttaNonlinearOperator
@@ -123,9 +124,8 @@ mutable struct IMEXRungeKuttaStageNonlinearOperator <: RungeKuttaNonlinearOperat
   u0::AbstractVector
   ode_cache
   vi::AbstractVector
-  ui::Vector{AbstractVector}
-  im_rhs::AbstractVector
-  ex_rhs::AbstractVector
+  fi::Vector{AbstractVector}
+  gi::Vector{AbstractVector}
   i::Int
   aᵢ::Matrix{Float64}
   aₑ::Matrix{Float64}
@@ -147,13 +147,15 @@ mutable struct IMEXRungeKuttaUpdateNonlinearOperator <: RungeKuttaNonlinearOpera
   u0::AbstractVector
   ode_cache
   vi::AbstractVector
-  ui::Vector{AbstractVector}
-  im_rhs::AbstractVector
-  ex_rhs::AbstractVector
+  fi::Vector{AbstractVector}
+  gi::Vector{AbstractVector}
   s::Int
   bᵢ::Vector{Float64}
   bₑ::Vector{Float64}
 end
+
+IMEXRungeKuttaNonlinearOperator = Union{IMEXRungeKuttaStageNonlinearOperator,
+  IMEXRungeKuttaUpdateNonlinearOperator}
 
 """
 residual!(b,op::IMEXRungeKuttaStageNonlinearOperator,x)
@@ -161,7 +163,7 @@ residual!(b,op::IMEXRungeKuttaStageNonlinearOperator,x)
 Compute the residual of the IMEXR Runge-Kutta nonlinear operator `op` at `x` and
 store it in `b` for a given stage `i`.
 ```math
-b = A(t,x,(x-x₀)/dt) = M(ui,ti)∂ui/∂t - f(∑aᵢ[i,j] * xj,ti) - g(∑_{j<i} aₑ_ij * xj,ti)
+b = A(t,x,(x-x₀)/dt) = ∂ui/∂t - ∑aᵢ[i,j] * f(xj,tj)
 ```
 
 Uses the vector b as auxiliar variable to store the residual of the left-hand side of
@@ -169,16 +171,17 @@ the i-th stage ODE operator, then adds the corresponding contribution from right
 at all earlier stages.
 ```math
 b = M(ui,ti)∂u/∂t
-b - f(∑_{j<=i} aᵢ_ij * uj,ti) - g(∑_{j<i} aₑ_ij * uj,ti) = 0
+b - ∑_{j<=i} aᵢ_ij * f(uj,tj) - ∑_{j<i} aₑ_ij * g(uj,tj) = 0
 ```
 """
 function residual!(b::AbstractVector,
   op::IMEXRungeKuttaStageNonlinearOperator,
   x::AbstractVector)
   rhs!(op,x)
-  explicit_rhs!(op,x)
   lhs!(b,op,x)
-  @. b = b - op.im_rhs - op.ex_rhs
+  for j in 1:op.i
+    @. b = b - op.aᵢ[op.i,j] * op.fi[j] - op.aₑ[op.i,j] * op.gi[j]
+  end
   b
 end
 
@@ -188,24 +191,24 @@ residual!(b,op::IMEXRungeKuttaUpdateNonlinearOperator,x)
 Computes the residual of the IMEX Runge-Kutta nonlinear operator `op` at `x` and
 for the final update.
 ```math
-b = A(t,x,(x-x₀)/dt) = M(xf,tf)∂uf/∂t - f(∑bᵢ[i] * xi,tf) - g(∑bₑ[i] * xi,tf)
+b = A(t,x,(x-x₀)/dt) = ∂ui/∂t - ∑bᵢ[i] * f(xj,tj) - ∑bₑ[i] * g(xj,tj)
 ```
 
 Uses the vector b as auxiliar variable to store the residual of the left-hand side of
 the final update ODE operator, then adds the corresponding contribution from right-hand sides
 at all stages.
 ```math
-b = M(uf,tf)∂u/∂t
-b - f(∑_{i<=s} bᵢ[i] * ui,tf) - g(∑_{i<=s} bₑ[i] * ui,tf) = 0
+b = M(ui,ti)∂u/∂t
+b - ∑_{i<=s} bᵢ[i] * f(ui,ti) - ∑_{i<s} bₑ[i] * g(ui,ti) = 0
 ```
 """
 function residual!(b::AbstractVector,
   op::IMEXRungeKuttaUpdateNonlinearOperator,
   x::AbstractVector)
-  rhs!(op,x)
-  explicit_rhs!(op,x)
   lhs!(b,op,x)
-  @. b = b - op.im_rhs - op.ex_rhs
+  for i in 1:op.s
+    @. b = b - op.bᵢ[i] * op.fi[i] - op.bₑ[i] * op.gi[i]
+  end
   b
 end
 
@@ -251,8 +254,7 @@ function jacobian!(J::AbstractMatrix,
   jacobian!(J,op.odeop,op.ti,(uf,vf),2,1.0/(op.dt),op.ode_cache)
 end
 
-function rhs!(op::Union{IMEXRungeKuttaStageNonlinearOperator,IMEXRungeKuttaUpdateNonlinearOperator},
-    x::AbstractVector)
+function rhs!(op::IMEXRungeKuttaNonlinearOperator, x::AbstractVector)
   u = x
   v = op.vi
   @. v = (x-op.u0)/(op.dt)
@@ -260,15 +262,24 @@ function rhs!(op::Union{IMEXRungeKuttaStageNonlinearOperator,IMEXRungeKuttaUpdat
   rhs!(f[op.i],op.odeop,op.ti,(u,v),op.ode_cache)
 end
 
-function explicit_rhs!(op::RungeKuttaNonlinearOperator, x::AbstractVector)
+function explicit_rhs!(op::IMEXRungeKuttaNonlinearOperator, x::AbstractVector)
   u = x
   v = op.vi
   @. v = (x-op.u0)/(op.dt)
-  explicit_rhs!(op.ex_rhs,op.odeop,op.ti,(u,v),op.ode_cache)
+  g = op.gi
+  explicit_rhs!(g[op.i],op.odeop,op.ti,(u,v),op.ode_cache)
 end
 
-function update!(op::IMEXRungeKuttaStageNonlinearOperator,ti::Float64,ui::AbstractVector,i::Int)
+function lhs!(b::AbstractVector, op::IMEXRungeKuttaNonlinearOperator, x::AbstractVector)
+  u = x
+  v = op.vi
+  @. v = (x-op.u0)/(op.dt)
+  lhs!(b,op.odeop,op.ti,(u,v),op.ode_cache)
+end
+
+function update!(op::RungeKuttaNonlinearOperator,ti::Float64,fi::AbstractVector,gi::AbstractVector,i::Int)
   op.ti = ti
-  op.ui = ui
+  op.fi = fi
+  op.gi = gi
   op.i = i
 end
