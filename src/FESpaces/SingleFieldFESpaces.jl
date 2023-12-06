a649dc982a80188cb14f6039ef52a7be6dc1153a
JordiManyer <jordi.manyer@monash.edu>
2023-12-07 10:53:48 +1100

    Updated zero_free_values

--- src/FESpaces/SingleFieldFESpaces.jl
+++ src/FESpaces/SingleFieldFESpaces.jl
@@ -16,7 +16,9 @@ num_dirichlet_dofs(f::SingleFieldFESpace) = length(get_dirichlet_dof_ids(f))
 """
 function zero_dirichlet_values(f::SingleFieldFESpace)
   V = get_vector_type(f)
-  allocate_vector(V,num_dirichlet_dofs(f))
+  dir_values = allocate_vector(V,num_dirichlet_dofs(f))
+  fill!(dir_values,zero(eltype(V)))
+  return dir_values
 end
 
 """
