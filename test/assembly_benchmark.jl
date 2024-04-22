eb92e8b0c93d9e7ad6039cba4e5006be61ca8190
JordiManyer <jordi.manyer@monash.edu>
2024-04-22 11:53:01 +1000

    Added better memeory usage when reusing COO

--- test/assembly_benchmark.jl
+++ test/assembly_benchmark.jl
@@ -65,14 +65,58 @@ function assemble_coo!(mat,a,matdata,cache)
   sparse_matrix!(mat,m2.V,K,reset=true)
 end
 
+###########
+mutable struct CachedInserterCOO{Tv,Ti,Ti2}
+  nnz :: Int
+  mat :: SparseMatrixCSC{Tv,Ti}
+  K :: Vector{Ti2}
+
+  function CachedInserterCOO(mat::SparseMatrixCSC{Tv,Ti}, K::Vector{Ti2}) where {Tv,Ti,Ti2}
+    new{Tv,Ti,Ti2}(0,mat,K)
+  end
+end
+
+Algebra.LoopStyle(::Type{<:CachedInserterCOO}) = Loop()
+
+@noinline function Algebra.add_entry!(::typeof(+),a::CachedInserterCOO,v,i,j)
+  a.nnz = a.nnz + 1
+  k = a.K[a.nnz]
+  a.mat.nzval[k] += v
+  nothing
+end
+
+function assemble_coo_opt(a,matdata;reuse=false)
+  m1 = Algebra.CounterCOO(get_matrix_builder(a),(get_rows(a),get_cols(a)))
+  symbolic_loop_matrix!(m1,a,matdata)
+  m2 = nz_allocation(m1)
+  numeric_loop_matrix!(m2,a,matdata)
+  m3 = create_from_nz(m2)
+  if reuse
+    K = precompute_nzindex(m3,m2)
+    return m3, K
+  end
+  m3
+end
+
+function assemble_coo_opt!(mat,a,matdata,K)
+  LinearAlgebra.fillstored!(mat,zero(eltype(mat)))
+  m = CachedInserterCOO(mat,K)
+  numeric_loop_matrix!(m,a,matdata)
+  mat
+end
+
 model = CartesianDiscreteModel((0,1,0,1),(100,100))
 
-reffe = ReferenceFE(lagrangian, Float64, 1)
+order = 1
+reffe = ReferenceFE(lagrangian, Float64, order)
 V = TestFESpace(model, reffe)
 
-Ω = Triangulation(model)
-dΩ = Measure(Ω, 2)
-a(u,v) = ∫(∇(u)⋅∇(v))dΩ
+qdegree = 2*order
+Ω  = Triangulation(model)
+dΩ = Measure(Ω, qdegree)
+Γ  = Boundary(model)
+dΓ = Measure(Γ, qdegree)
+a(u,v) = ∫(∇(u)⋅∇(v))dΩ + ∫(u*v)dΓ - ∫((∇⋅u) ⋅ (∇⋅v))dΩ
 
 u = get_trial_fe_basis(V)
 v = get_fe_basis(V)
@@ -81,10 +125,15 @@ matdata = collect_cell_matrix(V,V,a(u,v))
 assem = SparseMatrixAssembler(V,V)
 
 A1 = assemble_matrix(assem,matdata)
-A2, cache = assemble_coo(assem,matdata;reuse=true)
-assemble_coo!(A2,assem,matdata,cache)
+A2, cache2 = assemble_coo(assem,matdata;reuse=true);
+assemble_coo!(A2,assem,matdata,cache2)
+A3, cache3 = assemble_coo_opt(assem,matdata;reuse=true);
+assemble_coo_opt!(A3,assem,matdata,cache3)
+A1 ≈ A2 ≈ A3
 
 @benchmark assemble_matrix($assem,$matdata)
 @benchmark assemble_coo($assem,$matdata)
+@benchmark assemble_coo_opt($assem,$matdata)
 @benchmark assemble_matrix!($A1,$assem,$matdata)
-@benchmark assemble_coo!($A2,$assem,$matdata,$cache)
+@benchmark assemble_coo!($A2,$assem,$matdata,$cache2)
+@benchmark assemble_coo_opt!($A3,$assem,$matdata,$cache3)
