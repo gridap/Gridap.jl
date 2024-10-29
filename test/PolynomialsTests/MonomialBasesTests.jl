680d17e5c2eaedcf004bdbe3e75be156b7939146
Antoine Marteau <antoine.marteau@protonmail.com>
2024-10-29 15:51:56 +1100

    add test coverage for sym tensors MonomialBases

--- test/PolynomialsTests/MonomialBasesTests.jl
+++ test/PolynomialsTests/MonomialBasesTests.jl
@@ -120,7 +120,7 @@ g = G[
   (0.0, 1.0, 0.0, 0.0), (0.0, 0.0, 0.0, 1.0),
   (3.0, 2.0, 0.0, 0.0), (0.0, 0.0, 3.0, 2.0),
   (0.0, 6.0, 0.0, 0.0), (0.0, 0.0, 0.0, 6.0),
-  (9.0, 12.0, 0.0, 0.0), (0.0, 0.0, 9.0, 12.0)]
+  (9.0, 12.0, 0.0, 0.0),(0.0, 0.0, 9.0, 12.0)]
 
 bx = repeat(permutedims(v),np)
 ∇bx = repeat(permutedims(g),np)
@@ -166,11 +166,80 @@ bx = repeat(permutedims(v),np)
 test_field_array(b,x,bx,grad=∇bx)
 test_field_array(b,x[1],bx[1,:],grad=∇bx[1,:])
 
+# SymTensor-valued P space
+
+order = 1
+V = SymTensorValue{2,Float64}
+G = gradient_type(V,xi)
+filter = (e,o) -> sum(e) <= o
+b = MonomialBasis{2}(V,order,filter)
+
+v = V[(1.0, 0.0, 0.0), (0.0, 1.0, 0.0), (0.0, 0.0, 1.0),
+      (2.0, 0.0, 0.0), (0.0, 2.0, 0.0), (0.0, 0.0, 2.0),
+      (3.0, 0.0, 0.0), (0.0, 3.0, 0.0), (0.0, 0.0, 3.0)]
+
+g = G[(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0),
+      (0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0),
+      (0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0),
+      (1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0),
+      (0.0, 0.0, 1.0, 0.0, 1.0, 0.0, 0.0, 0.0),
+      (0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0),
+      (0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0),
+      (0.0, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0, 0.0),
+      (0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0)]
+
+bx = repeat(permutedims(v),np)
+∇bx = repeat(permutedims(g),np)
+test_field_array(b,x,bx,grad=∇bx)
+test_field_array(b,x[1],bx[1,:],grad=∇bx[1,:])
+
+# SymTracelessTensor-valued P space
+
+order = 1
+V = SymTracelessTensorValue{2,Float64}
+G = gradient_type(V,xi)
+filter = (e,o) -> sum(e) <= o
+b = MonomialBasis{2}(V,order,filter)
+
+v = V[(1.0, 0.0), (0.0, 1.0),
+      (2.0, 0.0), (0.0, 2.0),
+      (3.0, 0.0), (0.0, 3.0)]
+
+g = G[(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0),
+      (0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0),
+      (1.0, 0.0, 0.0, 0.0, 0.0, 0.0, -1., 0.0),
+      (0.0, 0.0, 1.0, 0.0, 1.0, 0.0, 0.0, 0.0),
+      (0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, -1.),
+      (0.0, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0, 0.0)]
+
+bx = repeat(permutedims(v),np)
+∇bx = repeat(permutedims(g),np)
+test_field_array(b,x,bx,grad=∇bx)
+test_field_array(b,x[1],bx[1,:],grad=∇bx[1,:])
+
+
 order = 1
 b = MonomialBasis{1}(Float64,order)
 @test evaluate(b,Point{1,Float64}[(0,),(1,)]) == [1.0 0.0; 1.0 1.0]
 
 b = MonomialBasis{0}(VectorValue{2,Float64},order)
 @test evaluate(b,Point{0,Float64}[(),()]) == VectorValue{2,Float64}[(1.0, 0.0) (0.0, 1.0); (1.0, 0.0) (0.0, 1.0)]
+
+b = MonomialBasis{0}(TensorValue{2,2,Float64},order)
+@test evaluate(b,Point{0,Float64}[(),()]) == TensorValue{2,2,Float64}[
+  (1.0, 0.0, 0.0, 0.0) (0.0, 1.0, 0.0, 0.0) (0.0, 0.0, 1.0, 0.0) (0.0, 0.0, 0.0, 1.0);
+  (1.0, 0.0, 0.0, 0.0) (0.0, 1.0, 0.0, 0.0) (0.0, 0.0, 1.0, 0.0) (0.0, 0.0, 0.0, 1.0)
+]
+
+b = MonomialBasis{0}(SymTensorValue{2,Float64},order)
+@test evaluate(b,Point{0,Float64}[(),()]) == SymTensorValue{2,Float64}[
+  (1.0, 0.0, 0.0) (0.0, 1.0, 0.0) (0.0, 0.0, 1.0);
+  (1.0, 0.0, 0.0) (0.0, 1.0, 0.0) (0.0, 0.0, 1.0)
+]
+
+b = MonomialBasis{0}(SymTracelessTensorValue{2,Float64},order)
+@test evaluate(b,Point{0,Float64}[(),()]) == SymTracelessTensorValue{2,Float64}[
+  (1.0, 0.0) (0.0, 1.0); (1.0, 0.0) (0.0, 1.0)
+]
 
 end # module
