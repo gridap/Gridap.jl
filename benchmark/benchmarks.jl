60fab730b9425f4735ce783a60d5bb79b35ae258
JordiManyer <jordi.manyer@monash.edu>
2024-11-05 14:27:04 +1100

    Made benchmarks extendable

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -1,29 +1,14 @@
 using BenchmarkTools
+using PkgBenchmark
 using Gridap
 
-include("drivers.jl")
+macro include_bm(SUITE,name)
+  quote
+    include("bm/$($name).jl")
+    SUITE["$($name)"] = $(Symbol(name)).SUITE
+  end
+end
 
 const SUITE = BenchmarkGroup()
 
-for (D,ncells) in [(2,20),(3,8)]
-  for order in [1,2,3]
-    basis_cases = [
-      ("lagrangian",lagrangian,Float64,order),
-      ("vector_lagragian",lagrangian,VectorValue{D,Float64},order),
-      ("raviart_thomas",raviart_thomas,Float64,order-1),
-    ]
-    for (basis_name,basis,T,degree) in basis_cases
-      biform_cases = [
-        ("mass",mass,2*order),
-        ("laplacian",laplacian,2*(order-1)),
-      ]
-      for (biform_name,biform,qdegree) in biform_cases
-        reffe = ReferenceFE(basis, T, degree)
-        name = "assembly_$(D)D_$(basis_name)_$(biform_name)_$(order)"
-        SUITE[name] = @benchmarkable bm_matrix_assembly(
-          $(D),$(ncells),$(reffe),$(qdegree),$(biform)
-        )
-      end
-    end
-  end
-end
+@include_bm SUITE "bm_assembly"
