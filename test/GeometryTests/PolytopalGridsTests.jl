d695e266810ffce92b6314f6763f76581f1abe2b
JordiManyer <jordi.manyer@monash.edu>
2025-01-06 22:32:43 +1100

    DG poisson working for polytopal meshes

--- test/GeometryTests/PolytopalGridsTests.jl
+++ test/GeometryTests/PolytopalGridsTests.jl
@@ -23,13 +23,13 @@ u_exact(x) = x[1]^order + x[2]^order
 
 dΩ = Measure(Ω,2*order)
 dΓ = Measure(Γ,2*order)
+dΛ = Measure(Λ,2*order)
 
 sum(∫(1)dΩ)
 
 V = FESpaces.PolytopalFESpace(Ω,order)
 
 # L2 projection
-
 mass_lhs(u,v) = ∫(u⋅v)dΩ
 mass_rhs(v) = ∫(v*u_exact)dΩ
 
@@ -40,6 +40,18 @@ eh = uh - u_exact
 sum(∫(eh⋅eh)dΩ)
 
 # DG Poisson
-f(x) = Δ(u_exact)(x)
-lap_lhs(u,v) = ∫(∇(u)⋅∇(v))dΩ
-lap_rhs(v) = ∫(v*f)dΩ
+β = 100.0
+f(x) = -Δ(u_exact)(x)
+nΛ = get_normal_vector(Λ)
+βΛ = CellField(β ./ get_array(∫(1)dΛ),Λ)
+nΓ = get_normal_vector(Γ)
+βΓ = CellField(β ./ get_array(∫(1)dΓ),Γ)
+
+lap_lhs(u,v) = ∫(∇(u)⋅∇(v))dΩ + ∫(βΛ*jump(u*nΛ)⋅jump(v*nΛ) - mean(∇(u))⋅jump(v*nΛ) - mean(∇(v))⋅jump(u*nΛ))dΛ + ∫(βΓ*u*v - (∇(u)⋅nΓ)*v - (∇(v)⋅nΓ)*u)dΓ
+lap_rhs(v) = ∫(v*f)dΩ + ∫(βΓ*u_exact*v - (∇(v)⋅nΓ)*u_exact)dΓ
+
+op = AffineFEOperator(lap_lhs,lap_rhs,V,V)
+uh = solve(op)
+
+eh = uh - u_exact
+sum(∫(eh⋅eh)dΩ)
