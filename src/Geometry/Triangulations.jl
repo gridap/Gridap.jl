388325f561f8edf63e7a08bb624d9ed320d1e2b3
JordiManyer <jordi.manyer@monash.edu>
2025-02-25 18:00:51 +1100

    Bugfix: Issue 1085

--- src/Geometry/Triangulations.jl
+++ src/Geometry/Triangulations.jl
@@ -306,11 +306,17 @@ function extend(a::LazyArray{<:Fill{typeof(linear_combination)}},b::PosNegPartit
   lazy_map(linear_combination,d1,d2)
 end
 
-#function extend(a::LazyArray{<:Fill},b::PosNegPartition)
-#  k = a.maps.value
-#  args = map(i->extend(i,b),a.args)
-#  lazy_map(k,args...)
-#end
+function extend(a::LazyArray{<:Fill{Broadcasting{Operation{typeof(dot)}}}},b::PosNegPartition)
+  d1 = extend(a.args[1],b)
+  d2 = extend(a.args[2],b)
+  lazy_map(Broadcasting(Operation(dot)),d1,d2)
+end
+
+# function extend(a::LazyArray{<:Fill},b::PosNegPartition)
+#   k = a.maps.value
+#   args = map(i->extend(i,b),a.args)
+#   lazy_map(k,args...)
+# end
 
 """
 """
