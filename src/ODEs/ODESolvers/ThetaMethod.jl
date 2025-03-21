4116f7ec0fb57b3fd76abf098ddd5f43357b33ca
Shreyas <shreyasprashanth2002@gmail.com>
2025-03-21 15:02:18 +0100

    Corrections

--- src/ODEs/ODESolvers/ThetaMethod.jl
+++ src/ODEs/ODESolvers/ThetaMethod.jl
@@ -162,7 +162,7 @@ function ode_march!(
     J, r, reuse, sysslvrcache
   )
 
-  # Update the sysslr if required. By default it does nothing.
+  # Update the sysslvr if required. By default it does nothing.
   update_sysslvr!(sysslvr, ws, usx)
   
   sysslvrcache = solve!(x, sysslvr, stageop, sysslvrcache)
