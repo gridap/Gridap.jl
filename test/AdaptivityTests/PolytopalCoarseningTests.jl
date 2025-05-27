e64a8231095084ea0af0906ba31d60d5c421ebdf
Jordi Manyer <jordi.manyer@monash.edu>
2025-05-27 16:47:11 +1000

    Added polytopal coarsening tests

--- test/AdaptivityTests/PolytopalCoarseningTests.jl
+++ test/AdaptivityTests/PolytopalCoarseningTests.jl
@@ -1,4 +1,6 @@
+module PolytopalCoarseningTests
 
+using Test
 using Gridap
 using Gridap.ReferenceFEs, Gridap.Adaptivity, Gridap.Geometry, Gridap.Arrays
 
@@ -9,6 +11,9 @@ function select_facet(p,lf,cperms)
   pid = cperms[lf + get_offset(p,D-1)]
   return f[fperms[pid]]
 end
+
+outdir = "tmp/"
+mkpath(outdir)
 
 D = 2
 n = 2
@@ -33,10 +38,10 @@ f1, f2 = select_facet(p1,lf1,cell_perms[c1]), select_facet(p2,lf2,cell_perms[c2]
 p = ReferenceFEs.merge_polytopes(p1, p2, f1, f2)
 q = ReferenceFEs.polytope_from_faces(D,get_vertex_coordinates(p),get_faces(p,D-1,0))
 
-writevtk(p1,"tmp/p1";append=false)
-writevtk(p2,"tmp/p2";append=false)
-writevtk(p,"tmp/p";append=false)
-writevtk(q,"tmp/q";append=false)
+writevtk(p1,joinpath(outdir,"p1");append=false)
+writevtk(p2,joinpath(outdir,"p2");append=false)
+writevtk(p,joinpath(outdir,"p");append=false)
+writevtk(q,joinpath(outdir,"q");append=false)
 
 ############################################################################################
 
@@ -45,7 +50,7 @@ n = 4
 nc = (n,fill(1,D-1)...)
 domain = (0,n,repeat([0,1],D-1)...)
 model = Geometry.voronoi(simplexify(CartesianDiscreteModel(domain,nc)))
-writevtk(Triangulation(model),"tmp/model";append=false)
+writevtk(Triangulation(model),joinpath(outdir,"model");append=false)
 
 patch_cells = Table([
   [1],[2,3,6,7],[4,8],[5,9,10]
@@ -54,7 +59,7 @@ topo = get_grid_topology(model)
 ptopo = Geometry.PatchTopology(topo,patch_cells)
 
 cmodel = Adaptivity.coarsen(model,ptopo)
-writevtk(Triangulation(cmodel),"tmp/cmodel";append=false)
+writevtk(Triangulation(cmodel),joinpath(outdir,"cmodel");append=false)
 
 ############################################################################################
 
@@ -62,7 +67,7 @@ D = 3
 nc = (3,3,3)
 domain = (0,1,0,1,0,1)
 model = Geometry.PolytopalDiscreteModel(CartesianDiscreteModel(domain,nc))
-writevtk(Triangulation(ReferenceFE{1},model),"tmp/model";append=false)
+writevtk(Triangulation(ReferenceFE{1},model),joinpath(outdir,"model");append=false)
 
 patch_cells = Table([
   [1],[2,3,4,5,6,7,8,9],[10,11,13,14,19,20,22,23],[12,15,16,17,18],[21,24],[25,26],[27]
@@ -71,8 +76,10 @@ topo = get_grid_topology(model)
 ptopo = Geometry.PatchTopology(topo,patch_cells)
 
 cmodel = Adaptivity.coarsen(model,ptopo)
-writevtk(Triangulation(ReferenceFE{1},cmodel),"tmp/cmodel";append=false)
+writevtk(Triangulation(ReferenceFE{1},cmodel),joinpath(outdir,"cmodel");append=false)
 
 for (i,p) in enumerate(get_polytopes(cmodel))
-  writevtk(p,"tmp/p_$i";append=false)
+  writevtk(p,joinpath(outdir,"p_$i");append=false)
+end
+
 end
