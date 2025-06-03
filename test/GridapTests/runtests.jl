7bcd3d34909dc98d7fa03e781a95f3b05535fcac
Jordi Manyer <jordi.manyer@monash.edu>
2025-06-04 08:35:46 +1000

    Added Poisson DG test for polytopal 3d meshes

--- test/GridapTests/runtests.jl
+++ test/GridapTests/runtests.jl
@@ -2,35 +2,24 @@ module GridapTests
 
 using Test
 
-@time @testset "Poisson" begin include("PoissonTests.jl") end
+@testset "Conforming Methods" begin
+  @time @testset "PLaplacian" begin include("PLaplacianTests.jl") end
+  @time @testset "Poisson1D" begin include("Poisson1DTests.jl") end
+  @time @testset "PoissonLagrangeMultiplier" begin include("PoissonLagrangeMultiplierTests.jl") end
+  @time @testset "Poisson" begin include("PoissonTests.jl") end
+  @time @testset "Biharmonic" begin include("BiharmonicTests.jl") end
+  @time @testset "Darcy" begin include("DarcyTests.jl") end
+  @time @testset "PeriodicDarcy" begin include("PeriodicDarcyTests.jl") end
+  @time @testset "HCurl" begin include("HCurlTests.jl") end
+  @time @testset "StokesTaylorHood" begin include("StokesTaylorHoodTests.jl") end
+  @time @testset "StokesNitsche" begin include("StokesNitscheTests.jl") end
+end
 
-@time @testset "PoissonDG" begin include("PoissonDGTests.jl") end
-
-@time @testset "Poisson1D" begin include("Poisson1DTests.jl") end
-
-@time @testset "PLaplacian" begin include("PLaplacianTests.jl") end
-
-@time @testset "StokesTaylorHood" begin include("StokesTaylorHoodTests.jl") end
-
-@time @testset "StokesDG" begin include("StokesDGTests.jl") end
-
-@time @testset "StokesNitsche" begin include("StokesNitscheTests.jl") end
-
-@time @testset "Darcy" begin include("DarcyTests.jl") end
-
-@time @testset "PeriodicDarcy" begin include("PeriodicDarcyTests.jl") end
-
-@time @testset "HCurl" begin include("HCurlTests.jl") end
-
-@time @testset "SurfaceCoupling" begin include("SurfaceCouplingTests.jl") end
-
-@time @testset "IsotropicDamage" begin include("IsotropicDamageTests.jl") end
-
-@time @testset "Biharmonic" begin include("BiharmonicTests.jl") end
-
-@time @testset "PoissonLagrangeMultiplier" begin include("PoissonLagrangeMultiplierTests.jl") end
-
-@time @testset "EmptyDomains" begin include("EmptyDomainsTests.jl") end
+@testset "DG Methods" begin
+  @time @testset "Poisson - DG" begin include("PoissonDGTests.jl") end
+  @time @testset "Poisson - DG polytopal" begin include("PoissonDGPolytopalTests.jl") end
+  @time @testset "Stokes - DG" begin include("StokesDGTests.jl") end
+end
 
 @testset "Hybrid Methods" begin
   @time @testset "Poisson - HDG" begin include("HDGTests.jl") end
@@ -40,6 +29,10 @@ using Test
   @time @testset "Poisson - HHO polytopal" begin include("HHOPolytopalTests.jl") end
   @time @testset "Poisson - HHO polytopal (mixed order)" begin include("HHOMixedPolytopalTests.jl") end
 end
+
+@time @testset "SurfaceCoupling" begin include("SurfaceCouplingTests.jl") end
+
+@time @testset "IsotropicDamage" begin include("IsotropicDamageTests.jl") end
 
 @testset "Issue reproducers" begin
   @time @testset "Issue614" begin include("issues/issue_614.jl") end
@@ -51,5 +44,7 @@ end
   @time @testset "Issue778" begin include("issues/issue_778.jl") end
   @time @testset "Issue869" begin include("issues/issue_869.jl") end
 end
+
+@time @testset "EmptyDomains" begin include("EmptyDomainsTests.jl") end
 
 end # module
