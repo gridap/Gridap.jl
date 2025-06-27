df5a09719c7756ae783c810cef95af684f5da8dc
amboschman <a.m.boschman@gmail.com>
2025-06-27 11:13:20 +1000

    Added scaled_meas as an operation

--- src/Fields/FieldsInterfaces.jl
+++ src/Fields/FieldsInterfaces.jl
@@ -363,7 +363,7 @@ evaluate!(cache,op::Broadcasting{<:Operation},x::Field...) = OperationField(op.f
 
 # Define some well known operations
 
-for op in (:+,:-,:*,:/,:⋅,:⊙,:⊗,:inv,:det,:meas,:pinvJt,:tr,:grad2curl,:symmetric_part,:transpose)
+for op in (:+,:-,:*,:/,:⋅,:⊙,:⊗,:inv,:det,:meas,:scaled_meas,:pinvJt,:tr,:grad2curl,:symmetric_part,:transpose)
   @eval ($op)(a::Field...) = Operation($op)(a...)
 end
 
