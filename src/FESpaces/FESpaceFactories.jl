fd9c3fb3a523105fec519dbc94b5a989d4d3214d
Antoine Marteau <antoine.marteau@protonmail.com>
2025-08-01 11:09:25 +1000

    Allow FESpace constructor to use FEEC RefFE descriptor

--- src/FESpaces/FESpaceFactories.jl
+++ src/FESpaces/FESpaceFactories.jl
@@ -108,7 +108,7 @@ function FESpace(
 end
 
 function FESpace(model::DiscreteModel,
-                 reffe::Tuple{<:ReferenceFEName,Any,Any}; kwargs...)
+                 reffe::Tuple{<:Union{ReferenceFEName,Symbol},Any,Any}; kwargs...)
   reffe_name, reffe_args,reffe_kwargs = reffe
   cell_reffe = ReferenceFE(model,reffe_name,reffe_args...;reffe_kwargs...)
   FESpace(model,cell_reffe;kwargs...)
