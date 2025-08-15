6955bcca8a8d36c7e7a5e8c97d5d4624108c1f08
Jordi Manyer <jordi.manyer@monash.edu>
2025-08-15 17:47:17 +1000

    Added extension for TikzPictures.jl

---
+++ test/VisualizationTests/TikzPictures.jl
@@ -0,0 +1,17 @@
+module TikzPicturesTests
+
+using TikzPictures
+using Gridap
+using Gridap.Geometry: DiscreteModelMock
+
+d = mktempdir()
+
+model = DiscreteModelMock()
+tp = TikzPicture(model)
+TikzPictures.save(PDF(joinpath(d,"tikz_model_2d")), tp)
+
+model = CartesianDiscreteModel((0,1,0,1,0,1),(2,2,2))
+tp = TikzPicture(model)
+TikzPictures.save(PDF(joinpath(d,"tikz_model_3d")), tp)
+
+end
--- test/VisualizationTests/runtests.jl
+++ test/VisualizationTests/runtests.jl
@@ -3,5 +3,6 @@ module VisualizationTests
 using Test
 
 @testset "Vtk" begin include("VtkTests.jl") end
+@testset "TikzPictures" begin include("TikzPictures.jl") end
 
 end # module
