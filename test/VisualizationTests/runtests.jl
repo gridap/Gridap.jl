3dfb69f613eaed36ff34a164b0509db1445a8c42
Jordi Manyer <jordi.manyer@monash.edu>
2025-08-16 10:37:50 +1000

    Deactivate tests for some architectures

--- test/VisualizationTests/runtests.jl
+++ test/VisualizationTests/runtests.jl
@@ -3,6 +3,9 @@ module VisualizationTests
 using Test
 
 @testset "Vtk" begin include("VtkTests.jl") end
-@testset "TikzPictures" begin include("TikzPictures.jl") end
+
+if !(Sys.ARCH === :aarch64 || Sys.ARCH === :x86_64) # Library issues on ARM and x86_64
+  @testset "TikzPictures" begin include("TikzPictures.jl") end
+end
 
 end # module
