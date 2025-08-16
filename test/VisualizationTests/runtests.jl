28a9667eefe89df92e70bb6885540d2dd863ecb0
Jordi Manyer <jordi.manyer@monash.edu>
2025-08-16 11:39:30 +1000

    Minor

--- test/VisualizationTests/runtests.jl
+++ test/VisualizationTests/runtests.jl
@@ -4,6 +4,7 @@ using Test
 
 @testset "Vtk" begin include("VtkTests.jl") end
 
+@info "ARCH: $(Sys.ARCH)"
 if !(Sys.ARCH === :aarch64 || Sys.ARCH === :x86_64) # Library issues on ARM and x86_64
   @testset "TikzPictures" begin include("TikzPictures.jl") end
 end
