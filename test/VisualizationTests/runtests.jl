3b9b71416ccb50b07a93fc4c67d1996c38ce114c
Jordi Manyer <jordi.manyer@monash.edu>
2025-09-19 21:09:21 +1000

    Split extension tests into their own CI, to be able to better control their environment

--- test/VisualizationTests/runtests.jl
+++ test/VisualizationTests/runtests.jl
@@ -4,8 +4,4 @@ using Test
 
 @testset "Vtk" begin include("VtkTests.jl") end
 
-if !(Sys.ARCH == :aarch64 || Sys.ARCH == :i686) # Library issues on ARM and x86_64
-  @testset "TikzPictures" begin include("TikzPictures.jl") end
-end
-
 end # module
