54a5c4e3a3010666a2d67d98e9f20a51c470f790
Antoine Marteau <antoine.marteau@protonmail.com>
2025-10-23 16:59:00 +1100

    finished MTW reference bases

--- src/ReferenceFEs/MTWRefFEs.jl
+++ src/ReferenceFEs/MTWRefFEs.jl
@@ -22,10 +22,10 @@ function MardalTaiWintherRefFE(::Type{T},p::Polytope{D}) where {T,D}
   @check D in (2,3) "MardalTaiWinther Reference FE only defined in dimension 2 and 3."
 
   prebasis = _mtw_basis(Val(D),T)
-  fb_n = FEEC_poly_basis(Val(D-1),Float64,1,0,:P⁻)
+  fb_n = FEEC_poly_basis(Val(D-1),T,1,0,:P⁻)
   # 2D: {1-x,x}       # in SEGMENT cartesian coords, so ≡ barycentric {λ1, λ2}
   # 3D: {1-x-y, x, y} # in TRI     cartesian coords, so ≡ barycentric {λ1, λ2, λ3}
-  fb_t = FEEC_poly_basis(Val(D-1),Float64,1,1,:P⁻)
+  fb_t = FEEC_poly_basis(Val(D-1),T,1,1,:P⁻)
   # 2D: {1}
   # 3D: {(1-y, x), (y, 1-x), (-y, x)}
 
@@ -70,7 +70,7 @@ function _mtw_basis(::Val{2},T)
     0 0 0 0 1 0  0 0 0 0 2 -2 0 0 0 -1 0  0 0  0; # (x² + 2xy, -2xy -y²)
     0 0 0 0 2 0 -1 0 0 0 0 -4 0 3 0  0 3  0 0 -1; # (-x³ + 2x² + 3xy², 3x²y - 4xy -y³)
     0 0 0 0 1 0  0 0 0 0 0 -2 2 0 0  0 3 -2 0 -1] # (2x²y + x² + 3xy², -2xy² - 2xy -y³)
-#∅            X    X              X         X   ] could optimise by filtering these terms from `monoms`
+#∅            X    X              X         X   ] could optimise by filtering these terms from `monoms` using a CompWiseTensorPolyBasis.
   linear_combination(permutedims(change), monoms)
 end
 
@@ -100,14 +100,23 @@ function _mtw_basis(::Val{3},::Type{T}) where T
     # (-x²z -2xyz -xz² +xz,  y²z +2xyz +yz² -yz,  0)
     [ [(-1, (2, 0, 1)), (-2, (1, 1, 1)), (-1, (1, 0, 2)), (1, (1, 0, 1)) ], [(1, (0, 2, 1)), (2, (1, 1, 1)), (1, (0, 1, 2)), (-1, (0, 1, 1)) ], [] ],
     # (0,  -x³y -x²y² -2x²yz +x²y,  x³z +2x²yz +x²z² -x²z)
+    [ [], [(-1, (3, 1, 0)), (-1, (2, 2, 0)), (-2, (2, 1, 1)), (1, (2, 1, 0))], [(1, (3, 0, 1)), (2, (2, 1, 1)), (1, (2, 0, 2)), (-1, (2, 0, 1)) ] ],
     # (x³y +x²y² +2x²yz -x²y,  0,  -3x²yz -2xy²z -2xyz² +2xyz)
+    [ [(1, (3, 1, 0)), (1, (2, 2, 0)), (2, (2, 1, 1)), (-1, (2, 1, 0))], [], [(-3, (2, 1, 1)), (-2, (1, 2, 1)), (-2, (1, 1, 2)), (2, (1, 1, 1)) ] ],
     # (-x³z -2x²yz -x²z² +x²z,  3x²yz +2xy²z +2xyz² -2xyz,  0)
+    [ [(-1, (3, 0, 1)), (-2, (2, 1, 1)), (-1, (2, 0, 2)), (1, (2, 0, 1)) ], [(3, (2, 1, 1)), (2, (1, 2, 1)), (2, (1, 1, 2)), (-2, (1, 1, 1)) ], [] ],
     # (0,  -x²y² -xy³ -2xy²z +xy²,  2x²yz +3xy²z +2xyz² -2xyz)
+    [ [], [(-1, (2, 2, 0)), (-1, (1, 3, 0)), (-2, (1, 2, 1)), (1, (1, 2, 0))], [(2, (2, 1, 1)), (3, (1, 2, 1)), (2, (1, 1, 2)), (-2, (1, 1, 1)) ] ],
     # (x²y² +xy³ +2xy²z -xy²,  0,  -2xy²z -y³z -y²z² +y²z)
+    [ [(1, (2, 2, 0)), (1, (1, 3, 0)), (2, (1, 2, 1)), (-1, (1, 2, 0))], [], [(-2, (1, 2, 1)), (-1, (0, 3, 1)), (-1, (0, 2, 2)), (1, (0, 2, 1)) ] ],
     # (-2x²yz -3xy²z -2xyz² +2xyz, 2xy²z +y³z +y²z² -y²z,  0)
+    [ [(-2, (2, 1, 1)), (-3, (1, 2, 1)), (-2, (1, 1, 2)), (2, (1, 1, 1)) ], [(2, (1, 2, 1)), (1, (0, 3, 1)), (1, (0, 2, 2)), (-1, (0, 2, 1)) ], [] ],
     # (0,  -2x²yz -2xy²z -3xyz² +2xyz,  x²z² +xz³ +2xyz² -xz²)
+    [ [], [(-2, (2, 1, 1)), (-2, (1, 2, 1)), (-3, (1, 1, 2)), (2, (1, 1, 1)) ], [(1, (2, 0, 2)), (1, (1, 0, 3)), (2, (1, 1, 2)), (-1, (1, 0, 2)) ] ],
     # (2x²yz +2xy²z +3xyz² -2xyz,  0,  -y²z² -yz³ -2xyz² +yz²)
+    [ [(2, (2, 1, 1)), (2, (1, 2, 1)), (3, (1, 1, 2)), (-2, (1, 1, 1)) ], [], [(-1, (0, 2, 2)), (-1, (0, 1, 3)), (-2, (1, 1, 2)), (1, (0, 1, 2)) ] ],
     # (-x²z² -xz³ -2xyz² +xz²,  y²z² +yz³ +2xyz² -yz²,  0)
+    [ [(-1, (2, 0, 2)), (-1, (1, 0, 3)), (-2, (1, 1, 2)), (1, (1, 0, 2)) ], [(1, (0, 2, 2)), (1, (0, 1, 3)), (2, (1, 1, 2)), (-1, (0, 1, 2)) ], [] ],
   ]
 
   comp_terms = [ CartesianIndex{3}[] for _ in 1:3 ]
@@ -128,7 +137,8 @@ function _mtw_basis(::Val{3},::Type{T}) where T
   change = zeros(T, (num_pb_polys, num_polys))
   comps_num_term = length.(comp_terms)
 
-  # need to loop again because we don't know the final number of terms per comp initially
+  # need to loop again because we don't know the final number of terms per comp
+  # initially, so the size of change was unknown
   for (poly, comp_to_coefsAndExponents) in enumerate(poly_to_comp_to_coefsAndExponents)
     for (l,coefsAndExponents) in enumerate(comp_to_coefsAndExponents)
       offset = sum(comps_num_term[1:l-1])
@@ -143,62 +153,3 @@ function _mtw_basis(::Val{3},::Type{T}) where T
   linear_combination(change, poly_prebasis)
 end
 
-## pre-pre basis from DefElements (https://defelement.org/elements/examples/tetrahedron-mardal-tai-winther-1.html)
-#function _mtw_basis(::Val{3},T)
-#  monoms = MonomialBasis(Val(3),VectorValue{3,T},4,Polynomials._p_filter)
-#  change = T[
-##e₁ 1      x      x²     x³     y      xy     x²y    y²     xy²    y³     z      xz     x²z    yz     xyz    y²z    z²     xz²    yz²    z³     #
-##e₂   1      x      x²     x³     y      xy     x²y    y²     xy²    y³     z      xz     x²z    yz     xyz    y²z    z²     xz²    yz²    z³   #
-##e₃     1      x      x²     x³     y      xy     x²y    y²     xy²    y³     z      xz     x²z    yz     xyz    y²z    z²     xz²    yz²    z³ #
-#    # (1, 0, 0) (0, 1, 0) (0, 0, 1)
-#    1 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0;
-#    0 1 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0;
-#    0 0 1  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0;
-#    # (x, 0, 0) (0, x, 0) (0, 0, x)
-#    0 0 0  1 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0;
-#    0 0 0  0 1 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0;
-#    0 0 0  0 0 1  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0;
-#    # (y, 0, 0) (0, y, 0) (0, 0, y)
-#    0 0 0  0 0 0  0 0 0  0 0 0  1 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0;
-#    0 0 0  0 0 0  0 0 0  0 0 0  0 1 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0;
-#    0 0 0  0 0 0  0 0 0  0 0 0  0 0 1  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0;
-#    # (z, 0, 0) (0, z, 0) (0, 0, z)
-#    0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  1 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0;
-#    0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 1 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0;
-#    0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 1  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0;
-#
-#    # (0, -x²y -xy² -2xyz +xy,  x²z +2xyz +xz² -xz)
-#    0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0;
-#    #-(-x²y -xy² -2xyz +xy,  0,  y²z +2xyz +yz² -yz)
-#    0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0;
-#    # (-x²z -2xyz -xz² +xz,  y²z +2xyz +yz² -yz,  0)
-#    0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0;
-#
-#    # (0,  -x³y -x²y² -2x²yz +x²y,  x³z +2x²yz +x²z² -x²z)
-#    0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0;
-#    #(x³y +x²y² +2x²yz -x²y,  0,  -3x²yz -2xy²z -2xyz² +2xyz)
-#    0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0;
-#    #(-x³z -2x²yz -x²z² +x²z,  3x²yz +2xy²z +2xyz² -2xyz,  0)
-#    0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0;
-#
-#    # (0,  -x²y² -xy³ -2xy²z +xy²,  2x²yz +3xy²z +2xyz² -2xyz)
-#    0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0;
-#    # (x²y² +xy³ +2xy²z -xy²,  0,  -2xy²z -y³z -y²z² +y²z)
-#    0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0;
-#    # (-2x²yz -3xy²z -2xyz² +2xyz, 2xy²z +y³z +y²z² -y²z,  0)
-#    0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0;
-#
-##e₁ 1      x      x²     x³     y      xy     x²y    y²     xy²    y³     z      xz     x²z    yz     xyz    y²z    z²     xz²    yz²    z³     #
-##e₂   1      x      x²     x³     y      xy     x²y    y²     xy²    y³     z      xz     x²z    yz     xyz    y²z    z²     xz²    yz²    z³   #
-##e₃     1      x      x²     x³     y      xy     x²y    y²     xy²    y³     z      xz     x²z    yz     xyz    y²z    z²     xz²    yz²    z³ #
-#    # (0,  -2x²yz -2xy²z -3xyz² +2xyz,  x²z² +xz³ +2xyz² -xz²)
-#    0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0;
-#    # (2x²yz +2xy²z +3xyz² -2xyz,  0,  -y²z² -yz³ -2xyz² +yz²)
-#    0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0;
-#    # (-x²z² -xz³ -2xyz² +xz²,  y²z² +yz³ +2xyz² -yz²,  0)
-#    0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0  0 0 0;
-##∅                                                                                                                                            ;
-#  ]
-#  linear_combination(permutedims(change), monoms)
-#end
-#
