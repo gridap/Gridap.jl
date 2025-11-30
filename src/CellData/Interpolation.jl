c77b44363c48d71700cbcfb42c656b266ffdb644
Oriol Colomes <oriol.colomes@gmail.com>
2025-11-30 20:41:28 +0100

    Reverted modifications in interpolation.jl and changed type of array from vector to a matrix in `linear_combination` function

--- src/CellData/Interpolation.jl
+++ src/CellData/Interpolation.jl
@@ -32,8 +32,7 @@ function return_cache(a::Interpolable,x::Point)
 
   cell_f = get_array(f)
   cell_f_cache = array_cache(cell_f)
-  active_cell_f = findall(lazy_map(i->length(i)>0, cell_f))
-  cf = testitem(cell_f[active_cell_f])
+  cf = testitem(cell_f)
   f_cache = return_cache(cf,x)
   cache2 = cell_f_cache, f_cache, cell_f, f
 
