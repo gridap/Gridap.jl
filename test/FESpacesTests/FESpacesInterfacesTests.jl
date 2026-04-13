bd284eda43c6bd64480a83b65bfc7dce675aa5ce
Ariana Johnson <ajohnson3916@sdsu.edu>
2026-04-13 12:27:28 -0700

    Add FEFunction overload for get_dirichlet_dof_values

--- test/FESpacesTests/FESpacesInterfacesTests.jl
+++ test/FESpacesTests/FESpacesInterfacesTests.jl
@@ -24,6 +24,13 @@ vh = FEFunction(V,rand(num_free_dofs(V)))
 @test isa(vh,FEFunction)
 test_fe_function(vh)
 
+    @testset "get_dirichlet_dof_values FEFunction overload" begin
+  dv_space = get_dirichlet_dof_values(V)
+  dv_func  = get_dirichlet_dof_values(vh)
+
+  @test dv_func == dv_space
+end
+
 
 dv = get_fe_basis(V)
 du = get_trial_fe_basis(V)
