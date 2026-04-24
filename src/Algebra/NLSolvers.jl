2960958a2cf7de4219b69e41b5c168acf4f1cc01
Ady0333 <adityashinde1525@gmail.com>
2026-04-24 08:12:04 +0530

    Fix NewtonRaphsonSolver convergence check and max_nliters error
    Signed-off-by: Ady0333 <adityashinde1525@gmail.com>

--- src/Algebra/NLSolvers.jl
+++ src/Algebra/NLSolvers.jl
@@ -64,7 +64,7 @@ function _solve_nr!(x,A,b,dx,ns,nls,op)
     if isconv; return; end
 
     if nliter == nls.max_nliters
-      @unreachable
+      error("NewtonRaphsonSolver failed to converge in $(nls.max_nliters) iterations. Final residual: $(maximum(abs,b))")
     end
 
     # Assemble jacobian (fast in-place version)
@@ -78,7 +78,7 @@ end
 
 function _check_convergence(nls,b)
   m0 = maximum(abs,b)
-  return (false, m0)
+  return (m0 < nls.tol, m0)
 end
 
 function _check_convergence(nls,b,m0)
