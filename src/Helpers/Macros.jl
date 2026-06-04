9ebbe97b8053906a6207a2460d406788c57eaaff
Antoine Marteau <antoine.marteau@protonmail.com>
2026-06-04 18:10:36 +1000

    improved @check_inferred macro

--- src/Helpers/Macros.jl
+++ src/Helpers/Macros.jl
@@ -74,20 +74,55 @@ Macro used unsure that `expression` is inferrable (passes [`Test.@inferred`](@re
 The check gets deactivated when running Gridap in performance mode.
 """
 macro check_inferred(ex)
+  ex.head != :call && error("@check_inferred requires a call expression, got $(string(ex))")
   ex_str = string(ex)
+  args_symbs = map(ex.args[2:end]) do arg_ex
+    string(arg_ex)
+  end
+  args_ex = map(ex.args[2:end]) do arg_ex
+    esc(arg_ex)
+  end
+
   @static if execution_mode == "debug"
     quote
       try
         $(esc( :(Helpers.Test.@inferred $(ex)) ))
       catch e
-        print("""
-        @check_inferred failed, the following call needs to be inferrable:
-          $($ex_str)
-        """)
-        rethrow(e)
-      end
-    end
+        if e isa InferrabilityException
+          # This ensures that we track the innermost inferrability failure of the tree
+          rethrow(e)
+
+        elseif e isa ErrorException && contains(e.msg , "does not match inferred return type")
+          args_symbs = $args_symbs
+          args_types = map(string∘typeof, tuple($(args_ex...)))
+          args_vals = tuple($(args_ex...))
+          ids = 1:length(args_symbs)
+          args_str = map(ids, args_symbs, args_types, args_vals) do i, symb, T, val
+            "    $symb::$T = $val\n"
+          end
+
+          msg = """
+          InferrabilityException: The following call is not type-inferrable:
+              $($ex_str)
+
+          Call arguments:
+          $(join(args_str, "\n"))
+          """
+          rethrow(InferrabilityException(msg))
+
+        else
+          rethrow()
+        end
+      end # catch
+    end # quote
   else
     esc(ex)
   end
 end
+
+struct InferrabilityException <: Exception
+  msg
+end
+
+Base.showerror(io::IO, e::InferrabilityException) = print(io, e.msg)
+
