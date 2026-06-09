270904a0e140c503955d9c5e235fae1bcaa79a71
Antoine Marteau <antoine.marteau@protonmail.com>
2026-06-09 12:00:52 +1000

    test @check_inferred

--- test/HelpersTests/MacrosTests.jl
+++ test/HelpersTests/MacrosTests.jl
@@ -1,3 +1,19 @@
 module MacrosTests
 
+using Test
+using Gridap.Helpers
+
+y = 0
+f(x) = y+x
+@test_throws Helpers.InferrabilityException @check_inferred f(0)
+
+let y=y
+g(x) = y+x
+@test_nowarn @check_inferred g(0)
+end
+
+const z = 0
+h(x) = z+x
+@test_nowarn @check_inferred h(0)
+
 end # module
