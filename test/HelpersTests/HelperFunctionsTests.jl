b71b6bff821dff29487aa226f333cfec65985067
Antoine Marteau <antoine.marteau@protonmail.com>
2026-06-17 14:39:06 +1000

    add findfirstvalue helper

--- test/HelpersTests/HelperFunctionsTests.jl
+++ test/HelpersTests/HelperFunctionsTests.jl
@@ -3,11 +3,26 @@ module HelperFunctionsTests
 using Test
 using Gridap.Helpers
 
+# tfill
+
 for D = 0:5
   @test tfill(2,Val(D)) == tuple(fill(2,D)...)
 end
 
+# get_val_parameter
+
 @test 1 == get_val_parameter(Val{1}())
+
+# findfirstvalue
+
+v = rand(1:20, 10)
+for i in 1:20
+  equals_i = ==(i)
+  i_or_nothing = i ∈ v ? i : nothing
+  @test findfirstvalue(equals_i, v) == i_or_nothing
+end
+
+# public_names_in_md
 
 module MockModule
   const C1 = nothing
