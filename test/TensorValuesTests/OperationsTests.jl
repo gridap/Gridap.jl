67e974a8ffa750b7d1b0e45454d36608d8c5153e
Antoine Marteau <antoine.marteau@protonmail.com>
2026-08-14 10:12:34 +1000

    minor

--- test/TensorValuesTests/OperationsTests.jl
+++ test/TensorValuesTests/OperationsTests.jl
@@ -1383,10 +1383,10 @@ a = ThirdOrderTensorValue{1,3,2}(1,2,1,4,2,2)
 b = a .* a
 @test b == ThirdOrderTensorValue{1,3,2}(1,4,1,16,4,4)
 
-b = TensorValue{1,3,2,Float64}(1,2,3,4,5,6)
-@test typeof(a .* b) == TensorValue{2,3,Float64,6}
+b = ThirdOrderTensorValue{1,3,2,Float64}(1,2,3,4,5,6)
+@test typeof(a .* b) == ThirdOrderTensorValue{1,3,2,Float64,6}
 
-c = TensorValue{3,2,Int}(1,2,3,4,5,6)
+c = ThirdOrderTensorValue{1,2,3,Int}(1,2,3,4,5,6)
 @test_throws ErrorException a .* c
 
 # Operation involving empty tensors (check type promotion)
